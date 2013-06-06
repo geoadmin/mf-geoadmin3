84732d492f468dced1174a5ea4b02b85f151b2bf
Éric Lemoine <eric.lemoine@gmail.com>
2013-06-06 11:43:16 +0200

    Add a "map" controller

---
+++ app/map/MapController.js
@@ -0,0 +1,24 @@
+mapModule.controller('MapController', ['$scope', function($scope) {
+
+  var map = new ol.Map({
+    layers: [
+      new ol.layer.TileLayer({
+        source: new ol.source.Stamen({
+          layer: 'watercolor'
+        })
+      }),
+      new ol.layer.TileLayer({
+        source: new ol.source.Stamen({
+          layer: 'terrain-labels'
+        })
+      })
+    ],
+    view: new ol.View2D({
+      center: [0, 0],
+      zoom: 2
+    })
+  });
+
+  $scope.map = map;
+
+}]);
