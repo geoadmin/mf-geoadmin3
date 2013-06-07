b32eecc8c720f636422f024962490c4264a63f2e
Éric Lemoine <eric.lemoine@gmail.com>
2013-06-07 12:24:30 +0200

    Add and use a directive for bg layer selector

---
+++ app/backgroundlayers/BackgroundLayersDirective.js
@@ -0,0 +1,62 @@
+(function() {
+  var mapModule = angular.module('app.backgroundlayers');
+
+  mapModule.directive('appBackgroundLayerSelector',
+      ['$parse', '$http', function($parse, $http) {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
+        map: '=appBackgroundLayerSelectorMap',
+        wmtsUrl: '=appBackgroundLayerSelectorWmtsUrl',
+        wmtsLayers: '=appBackgroundLayerSelectorWmtsLayers'
+      },
+      template:
+          '<select ng-model="currentLayer"' +
+              'ng-options="l.value as l.label for l in wmtsLayers">' +
+          '</select>',
+      link: function(scope, element, attrs) {
+        var map = $parse(attrs.appBackgroundLayerSelectorMap)(scope);
+        var wmtsUrl = $parse(attrs.appBackgroundLayerSelectorWmtsUrl)(scope);
+        var wmtsLayers = $parse(attrs.appBackgroundLayerSelectorWmtsLayers)(scope);
+
+        scope.currentLayer = wmtsLayers[0].value;
+
+        var wmtsLayerObjects = [];
+        var http = $http.get('http://wmts.geo.admin.ch/1.0.0/WMTSCapabilities.xml');
+        http.success(function(data, status, header, config) {
+          var parser = new ol.parser.ogc.WMTSCapabilities();
+          var capabilities = parser.read(data);
+          var i, ii = scope.wmtsLayers.length;
+          var wmtsSourceOptions, wmtsSource, wmtsLayer;
+          for (i = 0; i < ii; ++i) {
+            wmtsSourceOptions = ol.source.WMTS.optionsFromCapabilities(
+                capabilities, scope.wmtsLayers[i].value);
+            wmtsSource = new ol.source.WMTS(wmtsSourceOptions);
+            wmtsLayer = new ol.layer.TileLayer({source: wmtsSource});
+            wmtsLayerObjects.push(wmtsLayer);
+            if (scope.currentLayer === scope.wmtsLayers[i].value) {
+              map.getLayers().setAt(0, wmtsLayer);
+            }
+          }
+        });
+
+        scope.$watch('currentLayer', function(newVal, oldVal) {
+          if (oldVal !== newVal) {
+            var i, ii = scope.wmtsLayers.length;
+            for (i = 0; i < ii; ++i) {
+              if (scope.wmtsLayers[i].value === newVal) {
+                break;
+              }
+            }
+            if (i < scope.wmtsLayers.length) {
+              map.getLayers().setAt(0, wmtsLayerObjects[i]);
+            }
+          }
+        });
+      }
+    };
+  }]);
+})();
+
+
