d5e3e3010bff2731068e8e2bd4190ecf594d78c1
Éric Lemoine <eric.lemoine@gmail.com>
2013-06-13 14:37:09 +0200

    Extend the map directive to read states from URL
    Current, "X", "Y" and "zoom" are supported.

--- app/map/MapDirective.js
+++ app/map/MapDirective.js
@@ -1,11 +1,28 @@
 (function() {
   var mapModule = angular.module('app.map');
 
-  mapModule.directive('appMap', ['$parse', function($parse) {
+  mapModule.directive('appMap',
+      ['$parse', '$location', function($parse, $location) {
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
         var map = $parse(attrs.appMap)(scope);
+        var view = map.getView();
+
+        // set view states based on URL query string
+        var queryParams = $location.search();
+        if (queryParams.X !== undefined && queryParams.Y !== undefined) {
+          view.setCenter([+queryParams.X, +queryParams.Y]);
+        }
+        if (queryParams.zoom !== undefined) {
+          var projectionExtent = view.getProjection().getExtent();
+          var size = Math.max(
+              ol.extent.getHeight(projectionExtent),
+              ol.extent.getWidth(projectionExtent));
+          var resolution = size / (256 * Math.pow(2, queryParams.zoom));
+          view.setResolution(resolution);
+        }
+
         map.setTarget(element[0]);
       }
     };
