91a6a28733d12c1c04b9d1e16e7f8cde685eb6c2
Marc Monnerat <procrastinatio@gmail.com>
2013-06-14 14:33:53 +0200

    fixing js style according to gjslint. no functional changes

--- app/backgroundlayerselector/BackgroundLayerSelectorController.js
+++ app/backgroundlayerselector/BackgroundLayerSelectorController.js
@@ -6,13 +6,13 @@
   module.controller('GaBackgroundLayerSelectorController',
       ['$scope', function($scope) {
 
-    $scope.wmtsUrl = 'http://wmts.geo.admin.ch/1.0.0/WMTSCapabilities.xml';
-    $scope.wmtsLayers = [
-      {label: 'Color Map', value: 'ch.swisstopo.pixelkarte-farbe'},
-      {label: 'Aerial Imagery', value: 'ch.swisstopo.swissimage'},
-      {label: 'Grey Map', value: 'ch.swisstopo.pixelkarte-grau'}
-    ];
+        $scope.wmtsUrl = 'http://wmts.geo.admin.ch/1.0.0/WMTSCapabilities.xml';
+        $scope.wmtsLayers = [
+          {label: 'Color Map', value: 'ch.swisstopo.pixelkarte-farbe'},
+          {label: 'Aerial Imagery', value: 'ch.swisstopo.swissimage'},
+          {label: 'Grey Map', value: 'ch.swisstopo.pixelkarte-grau'}
+        ];
 
-  }]);
+      }]);
 
 })();
