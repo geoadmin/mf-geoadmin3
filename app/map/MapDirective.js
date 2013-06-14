da1c1f3cff74042d04870be57b827cf67751a4e2
Marc Monnerat <procrastinatio@gmail.com>
2013-06-14 14:33:53 +0200

    fixing js style according to gjslint. no functional changes

--- app/map/MapDirective.js
+++ app/map/MapDirective.js
@@ -5,30 +5,30 @@
 
   module.directive('gaMap',
       ['$parse', '$location', function($parse, $location) {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        var map = $parse(attrs.gaMap)(scope);
-        var view = map.getView();
+        return {
+          restrict: 'A',
+          link: function(scope, element, attrs) {
+            var map = $parse(attrs.gaMap)(scope);
+            var view = map.getView();
 
-        // set view states based on URL query string
-        var queryParams = $location.search();
-        if (queryParams.Y !== undefined && queryParams.X !== undefined) {
-          view.setCenter([+queryParams.Y, +queryParams.X]);
-        }
-        if (queryParams.zoom !== undefined) {
-          var projectionExtent = view.getProjection().getExtent();
-          var size = Math.max(
-              ol.extent.getHeight(projectionExtent),
-              ol.extent.getWidth(projectionExtent));
-          var resolution = size / (256 * Math.pow(2, queryParams.zoom));
-          view.setResolution(resolution);
-        }
+            // set view states based on URL query string
+            var queryParams = $location.search();
+            if (queryParams.Y !== undefined && queryParams.X !== undefined) {
+              view.setCenter([+queryParams.Y, +queryParams.X]);
+            }
+            if (queryParams.zoom !== undefined) {
+              var projectionExtent = view.getProjection().getExtent();
+              var size = Math.max(
+               ol.extent.getHeight(projectionExtent),
+               ol.extent.getWidth(projectionExtent));
+              var resolution = size / (256 * Math.pow(2, queryParams.zoom));
+              view.setResolution(resolution);
+            }
 
-        map.setTarget(element[0]);
-      }
-    };
-  }]);
+            map.setTarget(element[0]);
+          }
+        };
+      }]);
 })();
 
 
