7c6178635340e4c9a855120810e41af80ce52f3d
Frederic Junod <frederic.junod@camptocamp.com>
2013-06-14 14:14:46 +0200

    Build print form from capabilities

--- app/print/PrintController.js
+++ app/print/PrintController.js
@@ -6,11 +6,25 @@
   module.controller('GaPrintController',
       ['$scope', '$http', function($scope, $http) {
 
-        var http = $http.get('info.json');
-        http.success(function(data, status, header, config) {
-          $scope.capabilities = data;
-        });
+    var http = $http.get('info.json');
+    http.success(function(data, status, header, config) {
+      $scope.capabilities = data;
 
-      }]);
+      // default values:
+      $scope.layout = data.layouts[0];
+    });
+
+
+    $scope.submit = function() {
+      var spec = {
+        layout: this.layout.name
+      };
+      var http = $http.post(this.capabilities.createURL, spec);
+      http.success(function() {
+        
+      });
+    };
+
+  }]);
 
 })();
