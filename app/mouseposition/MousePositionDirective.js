f48b3a648d34c0808881a0e041b8aa61a06cbd95
Éric Lemoine <eric.lemoine@gmail.com>
2013-06-18 09:06:55 +0200

    Add a mouse position directive
    This directive wraps an ol3 mouse position control.

---
+++ app/mouseposition/MousePositionDirective.js
@@ -0,0 +1,36 @@
+(function() {
+  goog.provide('ga_mouseposition_directive');
+
+  var module = angular.module('ga_mouseposition_directive', []);
+
+  module.directive('gaMousePosition',
+      ['$parse', function($parse) {
+        return {
+          restrict: 'A',
+          link: function(scope, element, attrs) {
+            var map = $parse(attrs.gaMousePositionMap)(scope);
+            var mousePositionProjectionFunction =
+                $parse(attrs.gaMousePositionProjection);
+            var mousePositionProjection =
+                mousePositionProjectionFunction(scope);
+
+            var control = new ol.control.MousePosition({
+              coordinateFormat: ol.coordinate.createStringXY(2),
+              projection: mousePositionProjection,
+              target: element[0],
+              undefinedHTML: '&nbsp;'
+            });
+            control.setMap(map);
+
+            scope.$watch(mousePositionProjectionFunction,
+                function(oldVal, newVal) {
+                  if (newVal != oldVal) {
+                    //control.setProjection(newVal);
+                  }
+                });
+          }
+        };
+      }]);
+})();
+
+
