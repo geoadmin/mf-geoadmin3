d9a24df223bfe67e7b888a13c845980f816d4512
Frederic Junod <frederic.junod@camptocamp.com>
2013-06-18 14:05:06 +0200

    JS lint app/print/PrintController.js

--- app/print/PrintController.js
+++ app/print/PrintController.js
@@ -6,38 +6,38 @@
   module.controller('GaPrintController',
       ['$scope', '$http', function($scope, $http) {
 
-    var http = $http.get('info.json');  // FIXME
-    http.success(function(data, status, header, config) {
-      $scope.capabilities = data;
+        var http = $http.get('info.json');  // FIXME
+        http.success(function(data, status, header, config) {
+          $scope.capabilities = data;
 
-      // default values:
-      $scope.layout = data.layouts[0];
-      $scope.dpi = data.dpis[0];
+          // default values:
+          $scope.layout = data.layouts[0];
+          $scope.dpi = data.dpis[0];
 
-      $scope.scale = data.scales[0];  // FIXME
-    });
+          $scope.scale = data.scales[0];  // FIXME
+        });
 
-    $scope.submit = function() {
-      // http://mapfish.org/doc/print/protocol.html#print-pdf
-      var view = this.map.getView();
-      var proj = view.getProjection();
-      var spec = {
-        layout: this.layout.name,
-        srs: proj.getCode(),
-        units: proj.getUnits(),
-        layers: [{
-        }],
-        pages: [{
-          center: view.getCenter(),
-          scale: this.scale.value,
-          dpi: this.dpi.value
-        }]
-      };
-      // var http = $http.post(this.capabilities.createURL, spec);
-      // http.success(function() {
-      // });
-    };
+        $scope.submit = function() {
+          // // http://mapfish.org/doc/print/protocol.html#print-pdf
+          // var view = this.map.getView();
+          // var proj = view.getProjection();
+          // var spec = {
+          //   layout: this.layout.name,
+          //   srs: proj.getCode(),
+          //   units: proj.getUnits(),
+          //   layers: [{
+          //   }],
+          //   pages: [{
+          //     center: view.getCenter(),
+          //     scale: this.scale.value,
+          //     dpi: this.dpi.value
+          //   }]
+          // };
+          // var http = $http.post(this.capabilities.createURL, spec);
+          // http.success(function() {
+          // });
+        };
 
-  }]);
+      }]);
 
 })();
