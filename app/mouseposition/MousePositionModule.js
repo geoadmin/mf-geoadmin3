1d666a693d00f2d9e87ef62db9c865d313a29cf9
Éric Lemoine <eric.lemoine@gmail.com>
2013-06-18 09:10:37 +0200

    Remove the coordXY filter

--- app/mouseposition/MousePositionModule.js
+++ app/mouseposition/MousePositionModule.js
@@ -2,11 +2,9 @@
   goog.provide('ga_mouseposition');
 
   goog.require('ga_mouseposition_controller');
-  goog.require('ga_mouseposition_filter');
   goog.require('ga_mouseposition_directive');
 
   angular.module('ga_mouseposition', [
-    'ga_mouseposition_filter',
     'ga_mouseposition_controller',
     'ga_mouseposition_directive'
   ]);
