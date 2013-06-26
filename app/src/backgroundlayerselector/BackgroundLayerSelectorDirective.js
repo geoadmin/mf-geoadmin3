6cf2dea960430cb2134d6dbd06b9ccf500b4f37c
Éric Lemoine <eric.lemoine@gmail.com>
2013-06-26 08:13:01 +0200

    Background layer directive uses permalink service

--- app/src/backgroundlayerselector/BackgroundLayerSelectorDirective.js
+++ app/src/backgroundlayerselector/BackgroundLayerSelectorDirective.js
@@ -1,10 +1,13 @@
 (function() {
   goog.provide('ga_backgroundlayerselector_directive');
+  goog.require('ga_permalink');
 
-  var module = angular.module('ga_backgroundlayerselector_directive', []);
+  var module = angular.module('ga_backgroundlayerselector_directive', [
+    'ga_permalink'
+  ]);
 
   module.directive('gaBackgroundLayerSelector',
-      ['$parse', '$http', '$location', function($parse, $http, $location) {
+      ['$parse', '$http', 'gaPermalink', function($parse, $http, gaPermalink) {
         return {
           restrict: 'A',
           replace: true,
@@ -14,18 +17,31 @@
             wmtsLayers: '=gaBackgroundLayerSelectorWmtsLayers'
           },
           template:
-           '<select ng-model="currentLayer"' +
-              'ng-options="l.value as l.label for l in wmtsLayers">' +
-           '</select>',
+              '<select ng-model="currentLayer"' +
+                  'ng-options="l.value as l.label for l in wmtsLayers">' +
+              '</select>',
           link: function(scope, element, attrs) {
             var map = $parse(attrs.gaBackgroundLayerSelectorMap)(scope);
             var wmtsUrl = $parse(attrs.gaBackgroundLayerSelectorWmtsUrl)(scope);
             var wmtsLayers =
                 $parse(attrs.gaBackgroundLayerSelectorWmtsLayers)(scope);
 
-            var queryParams = $location.search();
+            var queryParams = gaPermalink.getParams();
             scope.currentLayer = (queryParams.bgLayer !== undefined) ?
              queryParams.bgLayer : wmtsLayers[0].value;
+
+            var setCurrentLayer = function(layerName) {
+              var i, ii = scope.wmtsLayers.length;
+              for (i = 0; i < ii; ++i) {
+                if (scope.wmtsLayers[i].value === layerName) {
+                  break;
+                }
+              }
+              if (i < scope.wmtsLayers.length) {
+                map.getLayers().setAt(0, wmtsLayerObjects[i]);
+                gaPermalink.updateParams({bgLayer: layerName});
+              }
+            };
 
             var wmtsLayerObjects = [];
             var http = $http.get(wmtsUrl);
@@ -37,29 +53,16 @@
               for (i = 0; i < ii; ++i) {
                 wmtsSourceOptions = ol.source.WMTS.optionsFromCapabilities(
                  capabilities, scope.wmtsLayers[i].value);
-                wmtsSourceOptions.attributions = [
-                  new ol.Attribution('&copy; Data: swisstopo')
-                ];
                 wmtsSource = new ol.source.WMTS(wmtsSourceOptions);
                 wmtsLayer = new ol.layer.TileLayer({source: wmtsSource});
                 wmtsLayerObjects.push(wmtsLayer);
-                if (scope.currentLayer === scope.wmtsLayers[i].value) {
-                  map.getLayers().setAt(0, wmtsLayer);
-                }
               }
+              setCurrentLayer(scope.currentLayer);
             });
 
             scope.$watch('currentLayer', function(newVal, oldVal) {
               if (oldVal !== newVal) {
-                var i, ii = scope.wmtsLayers.length;
-                for (i = 0; i < ii; ++i) {
-                  if (scope.wmtsLayers[i].value === newVal) {
-                    break;
-                  }
-                }
-                if (i < scope.wmtsLayers.length) {
-                  map.getLayers().setAt(0, wmtsLayerObjects[i]);
-                }
+                setCurrentLayer(newVal);
               }
             });
           }
