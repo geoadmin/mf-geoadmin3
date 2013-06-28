c2306f1f45117c4cdaaf5e036de1ad667180e8e0
Frederic Junod <frederic.junod@camptocamp.com>
2013-06-28 09:23:46 +0200

    Use  to group scope assignment

--- app/src/contextmenu/ContextMenuDirective.js
+++ app/src/contextmenu/ContextMenuDirective.js
@@ -3,9 +3,11 @@
 
   var module = angular.module('ga_contextmenu_directive', []);
 
-  module.directive('gaContextMenu', ['$http', function($http) {
-    var height = 'http://api.geo.admin.ch/height?cb=JSON_CALLBACK';
-    var lv03tolv95 = 'http://tc-geodesy.bgdi.admin.ch/reframe/lv03tolv95?cb=JSON_CALLBACK';
+  module.directive('gaContextMenu', ['$http', '$q', function($http, $q) {
+    var heightURL =
+        'http://api.geo.admin.ch/height?cb=JSON_CALLBACK';
+    var lv03tolv95URL =
+        'http://tc-geodesy.bgdi.admin.ch/reframe/lv03tolv95?cb=JSON_CALLBACK';
 
     return {
       restrict: 'A',
@@ -14,40 +16,44 @@
       },
       link: function(scope, element, attrs) {
         scope.map.on('contextmenu', function(event) {
-          scope.$apply(function() {
-            event.preventDefault();
-            var epsg21781 = event.getCoordinate();
-            var epsg4326 = ol.proj.transform(epsg21781, 'EPSG:21781', 'EPSG:4326');
-            scope.epsg21781 = ol.coordinate.toStringXY(epsg21781, 0);
-            scope.epsg4326 = ol.coordinate.toStringXY(epsg4326, 5);
+          event.preventDefault();
+          var epsg21781 = event.getCoordinate();
+          var epsg4326 = ol.proj.transform(epsg21781,
+              'EPSG:21781', 'EPSG:4326');
 
-            $http.jsonp(height, {
+          $q.all([
+            $http.jsonp(heightURL, {
               params: {
                 easting: epsg21781[0],
                 northing: epsg21781[1],
                 elevation_model: 'COMB'
               }
-            }).success(function(data) {
-              scope.altitude = data.height;
-            });
-            $http.jsonp(lv03tolv95, {
+            }),
+            $http.jsonp(lv03tolv95URL, {
               params: {
                 easting: epsg21781[0],
                 northing: epsg21781[1]
               }
-            }).success(function(data) {
-              scope.epsg2056 = ol.coordinate.toStringXY(data.coordinates, 2);
-            });
+            })
+          ]).then(function(results) {
+            scope.epsg21781 = ol.coordinate.toStringXY(epsg21781, 0);
+            scope.epsg4326 = ol.coordinate.toStringXY(epsg4326, 5);
 
-            var pixel = event.getPixel();
-            element.css('left', pixel[0] + 'px');
-            element.css('top', pixel[1] + 'px');
-            element.css('display', 'block');
-
-            scope.map.once('down', function() {
-              element.css('display', 'none');
-            });
+            scope.altitude = results[0].data.height;
+            scope.epsg2056 =
+                ol.coordinate.toStringXY(results[1].data.coordinates, 2);
           });
+
+          var pixel = event.getPixel();
+          element.css('left', pixel[0] + 'px');
+          element.css('top', pixel[1] + 'px');
+          element.css('display', 'block');
+
+          scope.map.once('down', function() {
+            element.css('display', 'none');
+          });
+
+          scope.$apply();
         });
       }
     };
