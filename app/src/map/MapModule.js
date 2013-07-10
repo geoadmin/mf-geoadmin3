e3eec198b6e331d527b295c1f48016a06a88ef01
Éric Lemoine <eric.lemoine@gmail.com>
2013-07-10 11:24:54 +0200

    Add a layers service
    This service loads the layers metadata from the layers.json file, and provides a getLayerById function to get a an ol.layer.TileLayer object from a layer id.

--- app/src/map/MapModule.js
+++ app/src/map/MapModule.js
@@ -3,10 +3,12 @@
 
   goog.require('ga_map_controller');
   goog.require('ga_map_directive');
+  goog.require('ga_map_service');
 
 
   angular.module('ga_map', [
+    'ga_map_controller',
     'ga_map_directive',
-    'ga_map_controller'
+    'ga_map_service'
   ]);
 })();
