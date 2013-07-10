300249fad93a648254c662fa9ee19f2859913782
Olivier Terral <olivier.terral@swisstopo.ch>
2013-07-10 15:56:36 +0200

    Add DND

--- app/src/importkml/ImportKmlController.js
+++ app/src/importkml/ImportKmlController.js
@@ -3,24 +3,22 @@
 
   var module = angular.module('ga_importkml_controller', []);
 
-  module.controller('GaImportKmlController', ['$scope', function($scope) {
+  module.controller('GaImportKmlController', ['$scope', gaGlobalOptions, function($scope) {
     
-    $scope.upload = {
-        userMessage: "No user message"
-    };
-  
     $scope.file = null;
-    $scope.fileName = "File name";
-    
-    $scope.setFile= function(inputFile) {
+    $scope.fileUrl = null;
+    $scope.fileContent = null;
+    $scope.options = {
+        serviceUrl: gaGlobalOptions.serviceUrl;
+    }
+    /*$scope.setFile= function(inputFile) {
         
          $scope.$apply(function($scope) {
              $scope.file = inputFile.files[0];
              $scope.fileName = $scope.file.name;
              $scope.upload.userMessage = "Upload succeed (Controller way)";
         });
-    };
+    };*/
+
   }]);
-
 })();
-
--- app/src/importkml/ImportKmlDirective.js
+++ app/src/importkml/ImportKmlDirective.js
@@ -1,42 +1,132 @@
 (function() {
-    goog.provide('ga_importkml_directive');
-    
-    var module = angular.module('ga_importkml_directive', []);
- 
-    module.directive('gaImportKml',
-        ['$http', '$log',
-        function($http, $log) {
+  goog.provide('ga_importkml_directive');
+  
+  var module = angular.module('ga_importkml_directive', []);
+
+  module.directive('gaImportKml',
+    ['$http', '$log',
+    function($http, $log) {            
+      return {
+        retsrict: 'A',
+        templateUrl: 'src/importkml/partials/form.html', 
+        scope: {
+            map: "=gaImportKmlMap",
+            options: "=geImportKmlOptions"
+        },
+        link: function(scope, element, attrs, controller) {
             
-            return {
-                retsrict: 'A',
-                templateUrl: 'src/importkml/partials/form.html', 
+          // Trigger the hidden input[type=file] onclick event
+          element.find('button').click(function() {
+            element.find('input[type="file"]').click();
+          });
+
+          // Register input[type=file] onchange event 
+          element.find("input[type=file]").bind('change', function(event) {
            
-                link: function(scope, element, attrs, controller
-                ) {
-                   $(element).find("input[type=file]").bind('change', function(event){
-                       var files = event.target.files;
-                       var file = files[0];
+            if (event.target.files) {               
+              // Use HTML5 fileAPI 
+              var files = event.target.files;
+              handleFileList(files);
+            
+            } else {
+                // Use old behavior, download file using proxy
+           }
+          });
+          
 
-                        
-                        // Doesn't work
-                       /*scope.$apply(function() 
-                       {
-                            scope.file = file;
-                            scope.fileName = file.name;
-                       });*/
+          // Register drag'n'drop events on map div
+          var dropzone = $(scope.map.viewport_); //element.find("textarea");
+          dropzone.bind('dragenter', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            $log.log("dragenter");
+          });
+          dropzone.bind('dragleave', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            $log.log("dragleave");
+          });
+          dropzone.bind('dragover', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            $log.log("dragover");
+          });
+          dropzone.bind('drop', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            $log.log("drop");
 
-                       // Work
-                       var controllerScope =  angular.element(event.target).scope();
-                       controllerScope.file = file;
-                       controllerScope.fileName = file.name;
-                       controllerScope.$apply();
+            // A file, an <a> html tag or a plain text url can be dropped
+            var files = evt.originalEvent.dataTransfer.files
 
-                       // example of kml in Swiss territory
-                       // http://60plus.csem.ch/v02/Balades/gpx_xml_kml/20091223_Vers_le_Lac_des_Tailleres.
-                       $log.log(scope);
-                       $log.log(file);
-                   });
-                }
-            }; 
-         }]);
+            if (files && files.length > 0) {
+              // A file has been dropped
+              handleFileList(files);
+            
+            } else if (evt.originalEvent.dataTransfer.types) {
+              // No files so may be it's HTML link or a URL which has been
+              // dropped
+              var text = evt.originalEvent.dataTransfer.getData(evt.originalEvent.dataTransfer.types[0]);
+               scope.$apply(function() {
+                  scope.fileContent = text; 
+               });
+
+            } else {
+              // No FileAPI
+              // Use old behavior, download file using proxy
+            }
+
+                 
+          });
+
+          // Handle a FileList (from input[type=file] or DnD), works only with FileAPI
+          function handleFileList(files) {
+            var file = files[0];
+            scope.file = file;
+            scope.fileUrl = "http://local/" + file.name;  
+            
+            // Read the file
+ 	        var reader = new FileReader();
+	        //reader.onprogress = handleReaderProgress;
+	        reader.onload = handleReaderLoadEnd;
+            reader.readAsText(file);
+          }
+                
+          // Callback when FileReader has finished          
+          function handleReaderLoadEnd(evt) {
+               scope.$apply(function() {
+                  scope.fileContent = evt.target.result;
+               });
+               displayFileContent(); 
+          }
+           
+          // Display the KML file content on the map
+          function displayFileContent() {
+                  // Add a layer 
+                  var epsg4326 = ol.proj.get('EPSG:4326');
+                  var swissProjection = ol.proj.get('EPSG:21781');
+
+  
+                  var vector = new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                      projection: epsg4326
+                    })
+                  });
+                  scope.map.addLayer(vector);
+                
+                  // Parse the KML file
+                  var kml = new ol.parser.KML({
+                    maxDepth: 1, 
+                    dimension: 2, 
+                    extractStyles: true, 
+                    extractAttributes: true
+                  });
+
+                  vector.parseFeatures(scope.fileContent, kml, swissProjection);
+                   
+          }
+        }
+      }; 
+    }]
+  );
 })();
