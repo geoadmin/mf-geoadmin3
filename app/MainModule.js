9f51e2fb5696bff9286010de1f65dfc841bedf33
Éric Lemoine <eric.lemoine@gmail.com>
2013-07-12 15:43:15 +0200

    Move print controller to ctrl dir

--- app/MainModule.js
+++ app/MainModule.js
@@ -7,13 +7,13 @@
   goog.require('ga_map');
   goog.require('ga_mouseposition');
   goog.require('ga_permalinkpanel');
-  goog.require('ga_print');
   goog.require('ga_scaleline');
   goog.require('ga_translation');
 
   goog.require('ga_map_controller');
   goog.require('ga_mouseposition_controller');
   goog.require('ga_permalinkpanel_controller');
+  goog.require('ga_print_controller');
 
   var module = angular.module('ga', [
     'ga_attribution',
@@ -22,12 +22,12 @@
     'ga_map',
     'ga_mouseposition',
     'ga_permalinkpanel',
-    'ga_print',
     'ga_scaleline',
     'ga_translation',
     'ga_map_controller',
     'ga_mouseposition_controller',
-    'ga_permalinkpanel_controller'
+    'ga_permalinkpanel_controller',
+    'ga_print_controller'
   ]);
 
   module.config(['$translateProvider', function($translateProvider) {
