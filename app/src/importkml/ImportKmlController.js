aa4754a3a303b71eb3e8e0a782d906fa6874d18b
Olivier Terral <olivier.terral@swisstopo.ch>
2013-07-12 16:47:42 +0200

    Add progress bar, tabs and improve user experience

--- app/src/importkml/ImportKmlController.js
+++ app/src/importkml/ImportKmlController.js
@@ -8,22 +8,25 @@
     
       $scope.file = null;  
       $scope.fileUrl = null;  
-      $scope.fileContent = null; // Use for debug purpose
+      $scope.fileContent = null; 
       $scope.userMessage = "";
+      $scope.progress = 0;
       $scope.options = {
-        serviceUrl: gaGlobalOptions.serviceUrl
+        serviceUrl: gaGlobalOptions.serviceUrl,
+        proxyUrl: "ogcproxy?url="
       };
 
       // Handle fileURL
-      $scope.handleFileUrl = function() {
-        //scope.fileUrl = url;
+      $scope.handleFileUrl = function() {       
         var url = $scope.fileUrl;
         
-        if (url && url.length > 0) {
-          var proxyUrl = $scope.options.serviceUrl + "/ogcproxy?url=" + encodeURIComponent(url);
-          $scope.userMessage = "Uploading file ...";
+        if (url && url.length > 0) { // Angular test the validation of the input too
           
-          $http({method: 'GET', url: proxyUrl})
+          var proxyUrl =  $scope.options.proxyUrl + encodeURIComponent(url);
+          $scope.userMessage = "Uploading file ...";
+          $scope.progress = 0.1;
+
+          $http.get(proxyUrl)
             .success(function(data, status, headers, config) {
               $scope.userMessage = "Upload succeed !!!";
               $scope.fileContent = data;                
@@ -31,14 +34,63 @@
             })
             .error(function(data, status, headers, config) {
               $scope.userMessage = "Upload file failed !!! Verify that your file exists"; 
+              $scope.progress = 0;
               $log.error("Display KML failed");               
             });
         }
       };
+     
 
+      // Handle a FileList (from input[type=file] or DnD), works only with FileAPI
+      $scope.handleFileList = function(files) {
+
+        var file = files[0];
+        
+         // Test the validity of the file
+        if (!/\.kml$/g.test(file.name)) { 
+          alert("The file you are trying to load is not a KML file");
+          return ;
+        }  
+
+        if (file.size > 20000000) {
+         alert("The file you are trying to load is more than 20 MB");
+         return; 
+        }
+               
+        $scope.file = file;       
+        $scope.userMessage = "Reading file ...";
+        $scope.progress = 0.1;    
+        
+        // Read the file
+ 	    var reader = new FileReader();
+	    reader.onprogress = $scope.handleReaderProgress;
+	    reader.onload = $scope.handleReaderLoadEnd;
+        reader.readAsText(file);
+      };
+                
+      // Callback when FileReader is processing
+      $scope.handleReaderProgress = function(evt) {
+        if (evt.lengthComputable) {
+          $scope.$apply(function() {
+            $scope.progress = (evt.loaded / evt.total) * 80 ;
+          });
+        }
+      }
+
+      // Callback when FileReader has finished          
+      $scope.handleReaderLoadEnd = function(evt) {
+        $scope.$apply(function() {
+          $scope.userMessage = "Read file succeed !!!";
+          $scope.fileContent = evt.target.result;
+          $scope.displayFileContent();
+        });
+      }; 
+     
+     
       // Display the KML file content on the map
       $scope.displayFileContent = function() {
-        $scope.userMessage = "Starting parsing file ...";
+        $scope.userMessage = "Start parsing file ...";
+        $scope.progress = 80;
 
         // Create vector layer 
         var epsg4326 = ol.proj.get('EPSG:4326');
@@ -59,31 +111,43 @@
           extractStyles: true, 
           extractAttributes: true
         });
+        
+        try {
+          vector.parseFeatures($scope.fileContent, kml, swissProjection);  
+          $scope.userMessage = "File parsed. Your datas are now display on the map.";
+          $scope.progress += 20;
 
-        vector.parseFeatures($scope.fileContent, kml, swissProjection);                  
-        $scope.userMessage = "File parsed. Your datas are now display on the map.";
+        } catch(e) {
+          $scope.userMessage = "Parsing failed for the following reason: " + e.message;
+          $scope.progress = 0;
+          $scope.map.removeLayer(kml);
+        }
+
       };
+
+      $scope.clearUserOutput = function() {
+        $scope.userMessage = "";
+        $scope.progress = 0;
+      };
+
+      $scope.cancel = function() {
+        $scope.userMessage = "Operation canceled";
+        $scope.progress = 0;
+        
+        if ($scope.fileReader) {
+          $scope.fileReader.abort();
+        }
+ 
+        // Cancel $http, how?
+        // $http.cancel();
+      }
       
-      // Handle a FileList (from input[type=file] or DnD), works only with FileAPI
-      $scope.handleFileList = function(files) {
-         $scope.userMessage = "Reading file ...";
-          
-         var file = files[0];
-         $scope.file = file;         
-           
-         // Read the file
- 	     var reader = new FileReader();
-	     //reader.onprogress = handleReaderProgress;
-	     reader.onload = $scope.handleReaderLoadEnd;
-         reader.readAsText(file);
-      };
-                
-      // Callback when FileReader has finished          
-      $scope.handleReaderLoadEnd = function(evt) {
-         $scope.userMessage = "Read file succeed !!!";
-         $scope.fileContent = evt.target.result;
-         $scope.displayFileContent();
-         $scope.$apply();
-      };           
+      $scope.reset = function() {
+        $scope.cancel();
+        $scope.clearUserOutput();
+        $scope.file = null;
+        $scope.fileUrl = null;
+        $scope.fileContent = null;
+      }
   }]);
 })();
--- app/src/importkml/ImportKmlDirective.js
+++ app/src/importkml/ImportKmlDirective.js
@@ -21,65 +21,80 @@
                      
           // Register input[type=file] onchange event 
           element.find("input[type=file]").bind('change', function(evt) {
-           
-            if (evt.target.files) {               
+                                  
+            if (evt.target.files && evt.target.files.length > 0) {               
               // Use HTML5 fileAPI
-              scope.handleFileList(evt.target.files);
-              scope.$apply();
+              scope.$apply(function() {
+                scope.handleFileList(evt.target.files);
+              });
 
             } else {
-                // No FileAPI
-                // Use old behavior, use the form validation download file using proxy
+              // No FileAPI
+              // Submit the form with an iframe hack
+              // var form = $(element).parent('form');
+              // form.submit();
+              // log.log(evt.target.value);               
            }
           });
           
 
-          // Register drag'n'drop events on map div
-          var dropzone = $(scope.map.viewport_); //element.find("textarea");
-          dropzone.bind('dragenter', function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            $log.log("dragenter");
-            this.style.border = "5px solid red";
-          });
-          dropzone.bind('dragleave', function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            $log.log("dragleave");
-            this.style.border = "none";
-          });
-          dropzone.bind('dragover', function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            $log.log("dragover");
-          });
-          dropzone.bind('drop', function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            $log.log("drop");
-            this.style.border = "none";
+          // Register drag'n'drop events on <body>
+          var dropZone = angular.element("<div class='import-kml-drop-zone'><div>DROP ME HERE</div></div>");
 
+          var dragEnterZone = angular.element(document.body); //element.find("textarea");
+          dragEnterZone.append(dropZone);
+          dragEnterZone.bind('dragenter', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            //$log.log("dragenter");
+            dropZone.css("display", "table");
+          });
+
+          dropZone.bind('dragleave', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            //$log.log("dragleave");
+            this.style.display = "none";
+          });
+
+          dropZone.bind('dragover', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            //$log.log("dragover");
+          });
+
+          dropZone.bind('drop', function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            //$log.log("drop");
+            this.style.display = "none";
+            
             // A file, an <a> html tag or a plain text url can be dropped
             var files = evt.originalEvent.dataTransfer.files
 
             if (files && files.length > 0) {
-              // A file has been dropped
-              scope.handleFileList(files);
-              scope.$apply();              
+               scope.$apply(function() {
+                scope.handleFileList(evt.files);
+              });             
             
             } else if (evt.originalEvent.dataTransfer.types) {
               // No files so may be it's HTML link or a URL which has been
               // dropped
               var text = evt.originalEvent.dataTransfer.getData(evt.originalEvent.dataTransfer.types[0]);
-              scope.fileUrl = text;
-              scope.handleFileUrl();
-              scope.$apply()
+              scope.$apply(function() {
+                scope.fileUrl = text;
+                scope.handleFileUrl();
+              });
               
             } else {
               // No FileAPI
               // Use old behavior, download file using proxy
             }                 
           });       
+        },
+
+        addDropZonElt: function(parentElt) {
+            
         }
       }; 
     }]
