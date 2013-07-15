17f97467b8709914c4081ff09aca199d54711250
Éric Lemoine <eric.lemoine@gmail.com>
2013-07-15 12:32:03 +0200

    Default matrix set is 21781_26

--- src/components/map/MapService.js
+++ src/components/map/MapService.js
@@ -6,7 +6,7 @@
   module.provider('gaTileGrid', function() {
     var resolutions = [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000,
       1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2,
-      1.5, 1, 0.5, 0.25];
+      1.5, 1, 0.5];
     var matrixIds = $.map(resolutions, function(r, i) { return i + ''; });
     var origin = [420000, 350000];
 
