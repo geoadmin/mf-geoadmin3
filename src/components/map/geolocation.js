0c131c66ab3a1287ab5b14d30e21e6d9c647df2f
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-07-17 11:35:23 +0200

    Use ol geolocalisation

--- src/components/map/geolocation.js
+++ src/components/map/geolocation.js
@@ -23,31 +23,27 @@ ga_map_geolocation = function(opt_options) {
       goog.events.EventType.TOUCHEND
     ], function(e) {
       e.preventDefault();
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var map = self.map_;
-        var view = map.getView().getView2D();
+      var map = self.map_;
+      var view = map.getView().getView2D();
+      var dest = self.geolocation.getPosition();
 
-        var dest = ol.proj.transform([position.coords.longitude,
-          position.coords.latitude], 'EPSG:4326', 'EPSG:21781');
-
-        var source = view.getCenter();
-        var dist = Math.sqrt(Math.pow(source[0] - dest[0], 2),
-            Math.pow(source[1] - dest[1], 2));
-        var duration = Math.sqrt(500 + dist / view.getResolution() * 1000);
-        var start = +new Date();
-        var pan = ol.animation.pan({
-                    duration: duration,
-                    source: source,
-                    start: start
-        });
-        var bounce = ol.animation.bounce({
-          duration: duration,
-          resolution: Math.max(view.getResolution(), dist / 1000),
-          start: start
-        });
-        map.addPreRenderFunctions([pan, bounce]);
-        view.setCenter(dest);
+      var source = view.getCenter();
+      var dist = Math.sqrt(Math.pow(source[0] - dest[0], 2),
+          Math.pow(source[1] - dest[1], 2));
+      var duration = Math.sqrt(500 + dist / view.getResolution() * 1000);
+      var start = +new Date();
+      var pan = ol.animation.pan({
+                  duration: duration,
+                  source: source,
+                  start: start
       });
+      var bounce = ol.animation.bounce({
+        duration: duration,
+        resolution: Math.max(view.getResolution(), dist / 1000),
+        start: start
+      });
+      map.addPreRenderFunctions([pan, bounce]);
+      view.setCenter(dest);
     });
   }
   goog.base(this, {
@@ -56,3 +52,18 @@ ga_map_geolocation = function(opt_options) {
   });
 };
 goog.inherits(ga_map_geolocation, ol.control.Control);
+
+
+/** 
+ * Overload setMap to use the view projection
+ * @param {ol.Map} map Map.
+ */
+ga_map_geolocation.prototype.setMap = function(map) { 
+    ol.control.Control.prototype.setMap.call(this, map);
+    if (map) {
+        this.geolocation = new ol.Geolocation({
+          tracking: true
+        });
+        this.geolocation.bindTo('projection', map.getView());
+    }
+};
