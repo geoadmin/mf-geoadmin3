d5fa6a93ed7a6d25fa5ff00b34e3e4d8f30d6b42
Gilbert Jeiziner <gjn@space.ch>
2013-07-18 14:25:56 +0200

    Adding more tests, still in recursive hell

--- src/components/catalogtree/CatalogtreeDirective.js
+++ src/components/catalogtree/CatalogtreeDirective.js
@@ -16,29 +16,49 @@
           function($compile, $log, $templateCache) {
         return {
           restrict: 'A',
+          /* commenting the 2 lines below do not change behaviour */
+          template: '',
+          replace: true,
           link: function(scope, element, attrs) {
             //FIXME: It's recommended not to write to scope in
             //directives...can we do without that?
-            scope.$watch('val', function(val, oldVal) {
-              if (val) {
-                scope.node_type = (val.children !== undefined) ?
-                         'node' : 'leaf';
-                scope.selected_open = (val.selected_open != undefined) ?
-                         val.selected_open : true;
+            console.log('calling link...');
 
-                var newElement = angular.element(includes[scope.node_type]);
-                $compile(newElement)(scope);
-                element.replaceWith(newElement);
-              }
+            if (scope.watched == 'True') {
+              scope.$watch('val', function(val, oldVal) {
+                replaceElement(scope, element, val, oldVal, true);
+              });
+            } else if (scope.watched != 'True') {
+              replaceElement(scope, element, scope.val, undefined, false);
             }
-            );
             //FIXME: don't write to scope in directive.
             //Need to find another solution for the ng-click binding
             scope.getLegend = getLegend;
+
+            function replaceElement(sc, el, val, oldVal, watcher) {
+               if (val) {
+                //console.log(val.label, el.length, el.parents().length,
+                //el.siblings().length, watcher);
+                sc.node_type = (val.children !== undefined) ?
+                         'node' : 'leaf';
+                sc.selected_open = (val.selected_open != undefined) ?
+                         val.selected_open : true;
+
+                var newElement = angular.element(includes[sc.node_type]);
+                //console.log(newElement[0].outerHTML);
+                $compile(newElement)(sc);
+                //console.log('Replacing ', el[0].outerHTML, ' with ',
+                //newElement[0].outerHTML);
+                el.replaceWith(newElement);
+                //console.log(el[0].outerHTML);
+                //console.log(newElement[0].outerHTML);
+              }
+            }
           },
           scope: {
             val: '=',
-            parentData: '='
+            parentData: '=',
+            watched: '@'
           }
         };
       }]
