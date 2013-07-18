3b2abfb26705e09997f7bab55ad733a3627cc669
cedricmoullet <cedric.moullet@gmail.com>
2013-07-18 08:34:06 +0200

    Infrastructure to submit form. It will be needed to create a feedback controller in mf-chsdi3

--- src/js/FeedbackController.js
+++ src/js/FeedbackController.js
@@ -4,11 +4,33 @@
   var module = angular.module('ga_feedback_controller', []);
 
   module.controller('GaFeedbackController',
-      ['$scope', '$http', 'gaPermalink', function($scope, $http, gaPermalink) {
+      ['$scope', '$http', 'gaPermalink', 'gaGlobalOptions', function($scope, $http, gaPermalink, gaGlobalOptions) {
+
+        var method = "POST";
+        var url = gaGlobalOptions.baseUrlPath + "/feedback";
 
         $scope.permalinkValue = gaPermalink.getHref();
 
         $scope.submit = function() {
+          var formData = {
+            'email': $scope.email,
+            'feedback': $scope.feedback,
+            'ua': navigator.userAgent,
+            'permalink': $scope.permalinkValue,
+            'typeOfRequest': 'feedback'
+          };
+          console.log(formData);
+          $http({
+            method: method,
+            url: url,
+            data: formData
+          }).
+          success(function(response){
+                alert("ok");
+          }).
+          error(function(response){
+                alert("not ok");
+          })
 
         };
 
