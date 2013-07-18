c949483ec1074498bd2562421e5923fad8892ea6
cedricmoullet <cedric.moullet@gmail.com>
2013-07-18 21:47:06 +0200

    Broadcast two events after the feedback submission

--- src/components/feedback/FeedbackDirective.js
+++ src/components/feedback/FeedbackDirective.js
@@ -4,12 +4,42 @@
   var module = angular.module('ga_feedback_directive', []);
 
   module.directive('gaFeedback',
-      [
-        function() {
+      ['$http', '$rootScope', 'gaPermalink', 'gaGlobalOptions',
+        function($http, $rootScope, gaPermalink, gaGlobalOptions) {
           return {
             restrict: 'A',
             replace: true,
-            templateUrl: 'components/feedback/partials/feedback.html'
+            scope: {
+              map: '=gaFeedbackOptions'
+            },
+            templateUrl: 'components/feedback/partials/feedback.html',
+            link: function(scope, element, attrs) {
+              var method = 'POST';
+              var url = gaGlobalOptions.baseUrlPath + '/feedback';
+
+              scope.permalinkValue = gaPermalink.getHref();
+
+              scope.submit = function() {
+                var formData = {
+                  'email': scope.email,
+                  'feedback': scope.feedback,
+                  'ua': navigator.userAgent,
+                  'permalink': scope.permalinkValue,
+                  'typeOfRequest': 'feedback'
+                };
+
+                $http({
+                  method: method,
+                  url: url,
+                  data: formData
+                }).success(function(response) {
+                  $rootScope.$broadcast('gaFeedbackSuccess', response);
+                }).error(function(response) {
+                  $rootScope.$broadcast('gaFeedbackError', response);
+                });
+
+              };
+            }
           };
         }]);
 
--- src/js/FeedbackController.js
+++ src/js/FeedbackController.js
@@ -4,35 +4,16 @@
   var module = angular.module('ga_feedback_controller', []);
 
   module.controller('GaFeedbackController',
-      ['$scope', '$http', 'gaPermalink', 'gaGlobalOptions', function($scope, $http, gaPermalink, gaGlobalOptions) {
+      ['$scope', function($scope) {
+        $scope.$on('gaFeedbackSuccess', function(event, response) {
+          angular.element(
+              document.getElementById('feedback')).modal('hide');
+        });
 
-        var method = "POST";
-        var url = gaGlobalOptions.baseUrlPath + "/feedback";
-
-        $scope.permalinkValue = gaPermalink.getHref();
-
-        $scope.submit = function() {
-          var formData = {
-            'email': $scope.email,
-            'feedback': $scope.feedback,
-            'ua': navigator.userAgent,
-            'permalink': $scope.permalinkValue,
-            'typeOfRequest': 'feedback'
-          };
-          console.log(formData);
-          $http({
-            method: method,
-            url: url,
-            data: formData
-          }).
-          success(function(response){
-                alert("ok");
-          }).
-          error(function(response){
-                alert("not ok");
-          })
-
-        };
+        $scope.$on('gaFeedbackError', function(event, response) {
+          angular.element(
+              document.getElementById('feedback')).modal('hide');
+        });
 
       }]);
 
