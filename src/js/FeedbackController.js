26cbee30623f9adff0ffe12cddf7e676e7b72a21
cedricmoullet <cedric.moullet@gmail.com>
2013-07-18 22:22:13 +0200

    Tentative to make dialogs. Feedback welcome...

--- src/js/FeedbackController.js
+++ src/js/FeedbackController.js
@@ -8,11 +8,15 @@
         $scope.$on('gaFeedbackSuccess', function(event, response) {
           angular.element(
               document.getElementById('feedback')).modal('hide');
+          angular.element(
+              document.getElementById('feedback-success')).modal('show');
         });
 
         $scope.$on('gaFeedbackError', function(event, response) {
           angular.element(
               document.getElementById('feedback')).modal('hide');
+          angular.element(
+              document.getElementById('feedback-error')).modal('show');
         });
 
       }]);
