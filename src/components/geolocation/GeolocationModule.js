6e6526ae0954c76c8b32db4973c087a835b3c365
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-07-22 16:54:11 +0200

    Apply elemoine review3

--- src/components/geolocation/GeolocationModule.js
+++ src/components/geolocation/GeolocationModule.js
@@ -2,9 +2,8 @@
   goog.provide('ga_geolocation');
 
   goog.require('ga_geolocation_directive');
-  goog.require('ga_map_directive');
 
-  angular.module('ga_map', [
+  angular.module('ga_geolocation', [
     'ga_geolocation_directive'
   ]);
 })();
