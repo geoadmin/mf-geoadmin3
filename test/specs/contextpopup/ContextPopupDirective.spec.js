be1bc4a0492c81489c0cb908762ccafcf1ade615
Antoine Abt <antoine@abt.im>
2013-08-01 12:09:07 +0200

    fix test to take mobile delay into account

--- test/specs/contextpopup/ContextPopupDirective.spec.js
+++ test/specs/contextpopup/ContextPopupDirective.spec.js
@@ -94,15 +94,17 @@ describe('ga_contextpopup_directive', function() {
         $timeout.flush();
         $httpBackend.flush();
 
-        var popover = element.find('.popover');
-        expect(popover.css('display')).to.be('block');
+        $timeout(function() {
+            var popover = element.find('.popover');
+            expect(popover.css('display')).to.be('block');
 
-        var tables = element.find('div.popover-content table');
-        var tds = $(tables[0]).find('td');
+            var tables = element.find('div.popover-content table');
+            var tds = $(tables[0]).find('td');
 
-        expect($(tds[1]).text()).to.be('661473.0, 188192.0');
-        expect($(tds[3]).text()).to.be('2725984.40, 1180787.40');
-        expect($(tds[7]).text()).to.be('1233 [m]');
+            expect($(tds[1]).text()).to.be('661473.0, 188192.0');
+            expect($(tds[3]).text()).to.be('2725984.40, 1180787.40');
+            expect($(tds[7]).text()).to.be('1233 [m]');
+        }, 250);
       });
 
       it('touchend prevents handler from being called', function() {
