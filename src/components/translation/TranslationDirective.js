0f5a9372edb2ebb0343293f07514eebd35b976c2
Éric Lemoine <eric.lemoine@gmail.com>
2013-08-12 15:20:12 +0200

    Fix indent

--- src/components/translation/TranslationDirective.js
+++ src/components/translation/TranslationDirective.js
@@ -9,30 +9,30 @@
   module.directive('gaTranslationSelector',
       ['$translate', '$window', 'gaPermalink',
         function($translate, $window, gaPermalink) {
-        return {
-          restrict: 'A',
-          replace: true,
-           scope: {
-             options: '=gaTranslationSelectorOptions'
-           },
-           template:
-               '<select ng-model="lang" ' +
-                   'ng-options="l.value as l.label for l in ' +
-                       'options.langs" class="input-small">' +
-               '</select>',
-          link: function(scope, element, attrs) {
-            scope.$watch('lang', function(value) {
-              $translate.uses(value).then(angular.noop, function(lang) {
-                // failed to load lang from server, fallback to default code.
-                scope.lang = scope.options.fallbackCode;
+          return {
+            restrict: 'A',
+            replace: true,
+             scope: {
+               options: '=gaTranslationSelectorOptions'
+             },
+             template:
+                 '<select ng-model="lang" ' +
+                     'ng-options="l.value as l.label for l in ' +
+                         'options.langs" class="input-small">' +
+                 '</select>',
+            link: function(scope, element, attrs) {
+              scope.$watch('lang', function(value) {
+                $translate.uses(value).then(angular.noop, function(lang) {
+                  // failed to load lang from server, fallback to default code.
+                  scope.lang = scope.options.fallbackCode;
+                });
+                gaPermalink.updateParams({lang: value});
               });
-              gaPermalink.updateParams({lang: value});
-            });
 
-            scope.lang = gaPermalink.getParams().lang ||
-                ($window.navigator.userLanguage ||
-                 $window.navigator.language).split('-')[0];
-          }
-        };
+              scope.lang = gaPermalink.getParams().lang ||
+                  ($window.navigator.userLanguage ||
+                   $window.navigator.language).split('-')[0];
+            }
+          };
       }]);
 })();
