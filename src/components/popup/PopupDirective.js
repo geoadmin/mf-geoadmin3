6be793509628cdbfe7d5cb5d9241e96fa2a47075
Olivier Terral <olivier.terral@swisstopo.ch>
2013-08-14 11:04:11 +0200

    Fix indent ImportKml and Popup directives

--- src/components/popup/PopupDirective.js
+++ src/components/popup/PopupDirective.js
@@ -5,59 +5,59 @@
 
   module.directive('gaPopup',
       [function() {
-          return {
-            restrict: 'A',
-            transclude: true,
-            scope: {
-              toggle: '=gaPopup',
-              optionsFunc: '&gaPopupOptions' // Options from directive
-            },
-            template:
-                  '<h4 class="popover-title ga-popup-title">' +
-                    '<span translate>{{options.title}}</span>' +
-                    '<button type="button" class="close" ng-click="close()">' +
-                    'x</button>' +
-                  '</h4>' +
-                  '<div class="popover-content ga-popup-content" ' +
-                       'ng-transclude>' +
-                  '</div>',
+         return {
+           restrict: 'A',
+           transclude: true,
+           scope: {
+             toggle: '=gaPopup',
+             optionsFunc: '&gaPopupOptions' // Options from directive
+           },
+           template:
+             '<h4 class="popover-title ga-popup-title">' +
+               '<span translate>{{options.title}}</span>' +
+               '<button type="button" class="close" ng-click="close()">' +
+               'x</button>' +
+             '</h4>' +
+             '<div class="popover-content ga-popup-content" ' +
+                 'ng-transclude>' +
+             '</div>',
 
-            link: function(scope, element, attrs) {
+           link: function(scope, element, attrs) {
 
-              // Get the popup options
-              scope.options = scope.optionsFunc();
+             // Get the popup options
+             scope.options = scope.optionsFunc();
 
-              if (!scope.options) {
-                scope.options = {
-                  title: ''
-                };
-              }
+             if (!scope.options) {
+               scope.options = {
+                 title: ''
+               };
+             }
 
-              // Add close popup function
-              scope.close = scope.options.close ||
-                            (function() {element.toggle();});
+             // Add close popup function
+             scope.close = scope.options.close ||
+                           (function() {element.toggle();});
 
-              // Move the popup to the correct position
-              element.addClass('popover ga-popup');
-              element.css({
-                left: scope.options.x ||
-                      $(document.body).width() / 2 - element.width() / 2,
+             // Move the popup to the correct position
+             element.addClass('popover ga-popup');
+             element.css({
+               left: scope.options.x ||
+                     $(document.body).width() / 2 - element.width() / 2,
 
-                top: scope.options.y || 150
-              });
+               top: scope.options.y || 150
+             });
 
-              // Watch the shown property
-              scope.$watch(
-                  'toggle',
-                  function(newVal, oldVal) {
-                    if (newVal != oldVal) {
-                      element.toggle();
-                    }
-                  }
+             // Watch the shown property
+             scope.$watch(
+                 'toggle',
+                 function(newVal, oldVal) {
+                   if (newVal != oldVal) {
+                     element.toggle();
+                   }
+                 }
               );
-            }
+           }
           };
-        }]);
+       }
+      ]
+  );
 })();
-
-
