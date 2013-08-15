d90c1a9a024af1aa18af163437b67f145e34f410
loicgasser <loicgasser4@gmail.com>
2013-08-15 16:45:46 +0200

    Never use autoload True in models definition, agreed with Marcel

--- chsdi/models/vector/bafu.py
+++ chsdi/models/vector/bafu.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import Column, Text, Integer
+from sqlalchemy.types import Numeric
 from geoalchemy import GeometryColumn, Geometry
 
 from chsdi.models import  *
@@ -12,8 +13,7 @@ Base = bases['bafu']
 class BLN(Base, Vector):
     # view in a schema
     __tablename__ = 'bln'
-    __table_args__ = ({'schema': 'bundinv', 'autoload': True})
-    __template__ = 'tooltips/bln.mako'
+    __table_args__ = ({'schema': 'bundinv', 'autoload': False})
     __esriId__ = 1000
     __bodId__ = 'ch.bafu.bundesinventare-bln'
     __displayFieldName__ = 'bln_name'
@@ -21,14 +21,16 @@ class BLN(Base, Vector):
     __template__ = 'templates/htmlpopup/bln.mako'
     id = Column('gid', Integer, primary_key=True)
     the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
+    bln_name = Column('bln_name', Text)
+    bln_obj = Column('bln_obj', Integer)
+    bln_fl = Column('bln_fl', Numeric)
 
 register('ch.bafu.bundesinventare-bln', BLN)
 
 class JB(Base, Vector):
     # view in a schema
     __tablename__ = 'jb'
-    __table_args__ = ({'schema': 'bundinv', 'autoload': True})
-    __template__ = 'tooltips/jb.mako'
+    __table_args__ = ({'schema': 'bundinv', 'autoload': False})
     __esriId__ = 2000
     __bodId__ = 'ch.bafu.bundesinventare-jagdbanngebiete'
     __displayFieldName__ = 'jb_name'
@@ -36,5 +38,10 @@ class JB(Base, Vector):
     __template__ = 'templates/htmlpopup/jb.mako'
     id = Column('gid', Integer, primary_key=True)
     the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
-
+    jb_name = Column('jb_name', Text)
+    jb_obj = Column('jb_obj', Integer)
+    jb_kat = Column('jb_kat', Text)
+    jb_fl = Column('jb_fl', Numeric)
+    jb_gf = Column('jb_gf', Numeric)
+ 
 register('ch.bafu.bundesinventare-jagdbanngebiete', JB)
--- chsdi/models/vector/uvek.py
+++ chsdi/models/vector/uvek.py
@@ -13,7 +13,7 @@ Base = bases['uvek']
 # IVS NAT and REG use the same template
 class IVS_NAT(Base, Vector):
     __tablename__ = 'ivs_nat'
-    __table_args__ = ({'schema': 'astra', 'autoload': True})
+    __table_args__ = ({'schema': 'astra', 'autoload': False})
     __template__ = 'templates/htmlpopup/ivs_nat.mako'
     __esriId__ = 3000
     __bodId__ = 'ch.astra.ivs-nat'
@@ -21,6 +21,16 @@ class IVS_NAT(Base, Vector):
     __queryable_attributes__ = ['ivs_slaname','ivs_nummer','ivs_signatur']
     id = Column('nat_id', Integer, primary_key=True)
     the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
+    ivs_slaname = Column('ivs_slaname', Text)
+    ivs_nummer = Column('ivs_nummer', Text)
+    ivs_signatur_fr = Column('ivs_signatur_fr', Text)
+    ivs_signatur_it = Column('ivs_signatur_it', Text)
+    ivs_signatur_de = Column('ivs_signatur_de', Text)
+    ivs_kanton = Column('ivs_kanton', Text)
+    ivs_sladatehist = Column('ivs_sladatehist', Integer)
+    ivs_sladatemorph = Column('ivs_sladatemorph', Integer)
+    ivs_slabedeutung = Column('ivs_slabedeutung', Integer)
+    ivs_sortsla = Column('ivs_sortsla', Text)
 
 register('ch.astra.ivs-nat', IVS_NAT)
 register('ch.astra.ivs-nat-verlaeufe', IVS_NAT)
@@ -28,14 +38,24 @@ register('ch.astra.ivs-nat-verlaeufe', IVS_NAT)
 
 class IVS_REG_LOC(Base, Vector):
     __tablename__ = 'ivs_reg_loc'
-    __table_args__ = ({'schema': 'astra', 'autoload': True})
-    __template__ = 'tooltips/ivs_reg_loc.mako'
+    __table_args__ = ({'schema': 'astra', 'autoload': False})
+    __template__ = 'templates/htmlpopup/ivs_nat.mako'
     __esriId__ = 4000
     __bodId__ = 'ch.astra.ivs-reg_loc'
     __displayFieldName__ = 'ivs_slaname'
     __queryable_attributes__ = ['ivs_slaname','ivs_nummer','ivs_signatur']
     id = Column('reg_loc_id', Integer, primary_key=True)
     the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
+    ivs_slaname = Column('ivs_slaname', Text)
+    ivs_nummer = Column('ivs_nummer', Text)
+    ivs_signatur_fr = Column('ivs_signatur_fr', Text)
+    ivs_signatur_it = Column('ivs_signatur_it', Text)
+    ivs_signatur_de = Column('ivs_signatur_de', Text)
+    ivs_kanton = Column('ivs_kanton', Text)
+    ivs_sladatehist = Column('ivs_sladatehist', Integer)
+    ivs_sladatemorph = Column('ivs_sladatemorph', Integer)
+    ivs_slabedeutung = Column('ivs_slabedeutung', Integer)
+    ivs_sortsla = Column('ivs_sortsla', Text)
 
 register('ch.astra.ivs-reg_loc', IVS_REG_LOC)
 
