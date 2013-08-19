350846a0a309151270e8ad06f3aedf68a9b8a762
Olivier Terral <olivier.terral@swisstopo.ch>
2013-08-19 15:59:14 +0200

    Fix js style

--- src/components/importkml/ImportKmlDirective.js
+++ src/components/importkml/ImportKmlDirective.js
@@ -284,7 +284,7 @@
 
                   if (!evt.originalEvent.dataTransfer.types) {
                     return;
-                  } else { 
+                  } else {
                     dropZone.css('display', 'table');
                   }
                 });
