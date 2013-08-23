6cdb122436e7d9fba72bc775847862f10163532e
Gilbert Jeiziner <gjn@space.ch>
2013-08-23 11:52:52 +0200

    Adapt comments

--- src/components/popup/PopupService.js
+++ src/components/popup/PopupService.js
@@ -9,7 +9,7 @@
 
       var Popup = function(options, scope) {
 
-        //Create the popup element with its content to the HTML page
+        // Create the popup element with its content to the HTML page
         var element = angular.element(
           '<div ga-popup ' +
                'ga-popup-options="options" ' +
@@ -18,18 +18,18 @@
           '</div>'
         );
 
-        //pass some popup functions for clients to be used in content
+        // Pass some popup functions for clients to be used in content
         var popup = this;
         options.open = function() {popup.open();};
         options.close = function() {popup.close();};
         options.destroy = function() {popup.destroy();};
 
-        //ceate scope, compile and link
+        // Create scope, compile and link
         this.scope = (scope || $rootScope).$new();
         this.scope.options = options;
         this.element = $compile(element)(this.scope);
 
-        //atach popup to body element
+        // Attach popup to body element
         $(document.body).append(this.element);
       };
 
