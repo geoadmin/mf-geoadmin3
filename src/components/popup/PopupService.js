759dc04c6507837caba3904da1fd723e1c9189fd
Gilbert Jeiziner <gjn@space.ch>
2013-08-28 09:45:33 +0200

    Adding option class to popup service

--- src/components/popup/PopupService.js
+++ src/components/popup/PopupService.js
@@ -13,10 +13,15 @@
 
       var Popup = function(options, scope) {
 
+        var classString = '';
+        if (options.popupClass) {
+          classString = 'class="' + options.popupClass + '" ';
+        }
         // Create the popup element with its content to the HTML page
         var element = angular.element(
           '<div ga-popup ' +
                'ga-popup-options="options" ' +
+               classString +
                'ga-draggable=".ga-popup-title">' +
                options.content +
           '</div>'
