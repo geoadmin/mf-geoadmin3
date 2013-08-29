47db04f4e9d315fcc3fbd7589580b23c751bf20a
loicgasser <loicgasser4@gmail.com>
2013-08-29 17:32:11 +0200

    Fix subscribers accordint to PEP 8

--- chsdi/subscribers.py
+++ chsdi/subscribers.py
@@ -3,6 +3,7 @@
 from pyramid.i18n import get_localizer, TranslationStringFactory
 from chsdi.lib import helpers
 
+
 def add_renderer_globals(event):
     request = event.get('request')
     if request:
@@ -12,11 +13,13 @@ def add_renderer_globals(event):
 
 tsf = TranslationStringFactory('chsdi')
 
+
 def add_localizer(event):
     request = event.request
     request._LOCALE_ = helpers.locale_negotiator(request)
     localizer = get_localizer(request)
     request.lang = localizer.locale_name
+
     def auto_translate(string):
         return localizer.translate(tsf(string))
     request.localizer = localizer
