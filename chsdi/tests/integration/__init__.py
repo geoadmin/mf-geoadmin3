977edddd793ed73c2c97aaf2bedcd4b02aa26af5
loicgasser <loicgasser4@gmail.com>
2013-08-29 17:49:14 +0200

    Fix init test according to PEP 8

--- chsdi/tests/integration/__init__.py
+++ chsdi/tests/integration/__init__.py
@@ -2,12 +2,13 @@
 
 from unittest import TestCase
 
+
 class TestsBase(TestCase):
 
     def setUp(self):
         from paste.deploy import loadapp
         import os
-        current = os.path.dirname( __file__ )
+        current = os.path.dirname(__file__)
         app = loadapp('config:' + current.replace('chsdi/tests/integration', 'development.ini'))
         from webtest import TestApp
         self.testapp = TestApp(app)
