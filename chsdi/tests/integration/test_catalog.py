6366116f63ef82be60759e928c8b4cee2c6a82b9
loicgasser <loicgasser4@gmail.com>
2013-08-29 17:46:38 +0200

    Fix tests on catalog according to PEP 8

--- chsdi/tests/integration/test_catalog.py
+++ chsdi/tests/integration/test_catalog.py
@@ -2,18 +2,19 @@
 
 from chsdi.tests.integration import TestsBase
 
+
 class TestCatalogService(TestsBase):
 
     def test_catalog_no_params(self):
         resp = self.testapp.get('/rest/services/blw/CatalogServer', status=200)
         self.failUnless(resp.content_type == 'application/json')
-        self.failUnless(resp.json['results'].has_key('root'))
-        self.failUnless(resp.json['results']['root'].has_key('children'))
-        self.failUnless(resp.json['results']['root'].has_key('selectedOpen'))
-        self.failUnless(resp.json['results']['root'].has_key('category'))
+        self.failUnless('root' in resp.json['results'])
+        self.failUnless('children' in resp.json['results']['root'])
+        self.failUnless('selectedOpen' in resp.json['results']['root'])
+        self.failUnless('category' in resp.json['results']['root'])
 
     def test_catalog_with_callback(self):
-        resp = self.testapp.get('/rest/services/blw/CatalogServer', params={'callback':'cb'}, status=200)
+        resp = self.testapp.get('/rest/services/blw/CatalogServer', params={'callback': 'cb'}, status=200)
         self.failUnless(resp.content_type == 'application/javascript')
 
     def test_catalog_wrong_map(self):
