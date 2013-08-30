1f9532e8bf1cb2726210636c162a4adf8e9fcad5
Gilbert Jeiziner <gjn@space.ch>
2013-08-30 11:20:50 +0200

    Unselect layer in catalogtree when removed in layer manager

--- src/components/catalogtree/CatalogitemDirective.js
+++ src/components/catalogtree/CatalogitemDirective.js
@@ -85,8 +85,8 @@
           var map = this.map;
           var layer = getMapLayer(map, item.idBod);
           if (angular.isDefined(layer) && layer.preview) {
-            layer.preview = false;
             map.removeLayer(layer);
+            layer.preview = false;
           }
         }
 
