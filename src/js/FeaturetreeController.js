0dcfbc448fb11578375df96e97413740bf1c8a9e
Gilbert Jeiziner <gjn@space.ch>
2013-09-05 11:21:35 +0200

    Adding collaps handling that's not working

--- src/js/FeaturetreeController.js
+++ src/js/FeaturetreeController.js
@@ -10,8 +10,24 @@
         var baseUrl = gaGlobalOptions.serviceUrl + '/rest/services';
 
         $scope.options = {
-          identifyUrlTemplate: baseUrl + '/{Topic}/MapServer/identify'
+          identifyUrlTemplate: baseUrl + '/{Topic}/MapServer/identify',
+          active: false
         };
+
+        $('#featuretree').on('show', function() {
+          //console.log('This is never called!');
+          $scope.options.active = true;
+        });
+
+        $('#featuretree').on('hide', function() {
+          //console.log('This is never called!');
+          $scope.options.active = true;
+        });
+        /*
+        $('#featuretree-link').on('click', function() {
+          //console.log('This is called');
+        });
+        */
       }
   ]);
 })();
