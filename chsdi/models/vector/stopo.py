7bbe520e0f259c0cd6e1f851f9e152e76ca05c30
daguer <daguerdat@gmail.com>
2013-09-09 15:45:48 +0200

    add layer ch.swisstopo.geologie-geophysik-inklination with tooltip inklination.mako

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -943,19 +943,21 @@ class Geologischer_Deklination(Base, Vector):
 
 register('ch.swisstopo.geologie-geophysik-deklination',Geologischer_Deklination)
 
-# class Geologischer_Inklination(Base, Vector):
-#         # view in a schema
-#         __tablename__ = 'geophysik_inklination'
-#         __table_args__ = ({'schema': 'geol', 'autoload': False})
-#         __template__ = 'templates/htmlpopup/inklination.mako'
-#         __esriId__ = 1000
-#         __bodId__ = 'ch.swisstopo.geologie-geophysik-inklination'
-#         __displayFieldName__ = ''
-#         id = Column('gid', Integer, primary_key=True)
-#         the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
-# 
-# register('ch.swisstopo.geologie-geophysik-inklination',Geologischer_Inklination)
-# 
+class Geologischer_Inklination(Base, Vector):
+# view in a schema
+        __tablename__ = 'geophysik_inklination'
+        __table_args__ = ({'schema': 'geol', 'autoload': False})
+        __template__ = 'templates/htmlpopup/inklination.mako'
+        __esriId__ = 1000
+        __bodId__ = 'ch.swisstopo.geologie-geophysik-inklination'
+        __displayFieldName__ = 'bgdi_id'
+        id = Column('gid', Integer, primary_key=True)
+        contour = Column('contour', Numeric)
+        bgdi_id = Column('bgdi_id', Integer)
+        the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
+
+register('ch.swisstopo.geologie-geophysik-inklination',Geologischer_Inklination)
+
 # class Geologischer_Aeromagnetik_Jura(Base, Vector):
 #         # view in a schema
 #         __tablename__ = 'gravimetrie_aeromagnetik_jura'
