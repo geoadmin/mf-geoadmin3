57ac5feada21be6a38517d5396aadf8f8def76f9
alcapat <patrick.alcaraz.ch@gmail.com>
2013-09-10 07:48:10 +0200

    Add mako sgt_facilities.mak sgt_planning.mako

--- chsdi/models/vector/uvek.py
+++ chsdi/models/vector/uvek.py
@@ -379,7 +379,6 @@ class sgt_facilities(Base, Vector):
     __bodId__ = 'ch.bfe.sachplan-geologie-tiefenlager'
     __displayFieldName__ = 'facname_de'
     id = Column('stabil_id', Integer, primary_key=True)
-#    id = Column('bgdi_id', Integer, primary_key=True) #OK : http://localhost:9007/rest/services/ech/MapServer/ch.bfe.sachplan-geologie-tiefenlager/13/htmlpopup
     facname_de = Column('facname_de', Text)
     facname_fr = Column('facname_fr', Text)
     facname_it = Column('facname_it', Text)
@@ -401,3 +400,37 @@ class sgt_facilities(Base, Vector):
     the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
 
 register('ch.bfe.sachplan-geologie-tiefenlager', sgt_facilities)
+
+class sgt_planning(Base, Vector):
+    __tablename__ = 'geologische_tiefenlager'
+    __table_args__ = ({'schema': 'bfe', 'autoload': False})
+    __template__ = 'templates/htmlpopup/sgt_planning.mako'
+    __esriId__ = 4015
+    __bodId__ = 'ch.bfe.sachplan-geologie-tiefenlager'
+    __displayFieldName__ = 'facname_fr' # if use facname_de -> error message ??
+    id = Column('stabil_id', Text, primary_key=True)
+    the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
+    facname_fr = Column('facname_fr', Text)
+    facname_it = Column('facname_it', Text)
+    facname_de = Column('facname_de', Text)
+    measurename_de = Column('measurename_de', Text)
+    measurename_fr = Column('measurename_fr', Text)
+    measurename_it = Column('measurename_it', Text)
+    measuretype_text_de = Column('measuretype_text_de', Text)
+    measuretype_text_fr = Column('measuretype_text_fr', Text)
+    measuretype_text_it = Column('measuretype_text_it', Text)
+    coordinationlevel_text_de = Column('coordinationlevel_text_de', Text)
+    coordinationlevel_text_fr = Column('coordinationlevel_text_fr', Text)
+    coordinationlevel_text_it = Column('coordinationlevel_text_it', Text)
+    planningstatus_text_de = Column('planningstatus_text_de', Text)
+    planningstatus_text_fr = Column('planningstatus_text_fr', Text)
+    planningstatus_text_it = Column('planningstatus_text_it', Text)
+    validfrom = Column('validfrom', Text)
+    validuntil = Column('validuntil', Text)
+    description = Column('description', Text)
+    web = Column('web', Text)
+    bgdi_created = Column('bgdi_created', Text)
+    __minscale__ = 50005
+    __maxscale__ = 1000005
+
+register('ch.bfe.sachplan-geologie-tiefenlager', sgt_planning)
---
+++ chsdi/templates/htmlpopup/sgt_facilities.mako
@@ -0,0 +1,53 @@
+<%inherit file="base.mako"/>
+
+#<%def name="preview()">
+#	% if lang == 'de' or lang == 'rm' or lang == 'en':
+#	${c['attributes']['facname_de'] or '-'}
+#	% elif lang == 'fr':
+#	${c['attributes']['facname_fr'] or '-'}
+#	% elif lang == 'it':
+#	${c['attributes']['facname_it'] or '-'}
+#	% endif
+#</%def>
+
+<%def name="table_body(c, lang)">
+<% c[stable_id] = True %>
+    <tr><td width="150">${_('tt_sachplan_facility_name')}</td>    <td>${c['value']}</td></tr>
+    <tr><td width="150">${_('tt_sachplan_facility_anlageart')}</td>
+      % if lang == 'de' or lang == 'rm' or lang == 'en':
+           <td>${c['attributes']['fackind_text_de'] or '-'}</td>
+      % elif lang == 'fr':
+           <td>${c['attributes']['fackind_text_fr'] or '-'}</td>
+	  % elif lang == 'it':
+           <td>${c['attributes']['fackind_text_it'] or '-'}</td>   
+      % endif
+    </tr>
+	<tr><td width="150">${_('tt_sachplan_facility_anlagestatus')}</td>
+      % if lang == 'de' or lang == 'rm' or lang == 'en':
+           <td>${c['attributes']['facstatus_text_de'] or '-'}</td>
+      % elif lang == 'fr':
+           <td>${c['attributes']['facstatus_text_fr'] or '-'}</td>
+	  % elif lang == 'it':
+           <td>${c['attributes']['facstatus_text_it'] or '-'}</td>   
+      % endif
+    </tr>
+	<tr><td width="150">${_('tt_sachplan_facility_beschlussdatum')}</td> <td>${c['attributes']['validfrom'] or '-'}</td></tr>
+	<tr><td width="150">${_('tt_sachplan_beschreibung')}</td> <td>${c['attributes']['description'] or '-'}</td></tr>
+    </tr>
+        <tr><td width="150">${_('tt_sachplan_weitereinfo')}</td>
+      % if c['attributes']['web'] is None:
+           <td> - </td>
+      % else:
+           <td><a href="${c['attributes']['web'] or '-'}" target="_blank">${_('tt_sachplan_objektblatt')}</a></td>
+      %endif
+    </tr>
+	<tr><td width="150">${_('tt_sachplan_facility_uberobjekt')}</td>
+      % if lang == 'de' or lang == 'rm' or lang == 'en':
+           <td>${c['attributes']['objname_text_de'] or '-'}</td>
+      % elif lang == 'fr':
+           <td>${c['attributes']['objname_text_fr'] or '-'}</td>
+	  % elif lang == 'it':
+           <td>${c['attributes']['objname_text_it'] or '-'}</td>   
+      % endif
+    </tr>      
+</%def>
