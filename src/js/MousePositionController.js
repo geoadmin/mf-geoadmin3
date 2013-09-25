e5848dfa120da0f6d25e700b0cbe0b7f656f1aae
Frederic Junod <frederic.junod@camptocamp.com>
2013-09-25 11:54:02 +0200

    Add coordinates prefix to the mouse position

--- src/js/MousePositionController.js
+++ src/js/MousePositionController.js
@@ -1,19 +1,25 @@
 (function() {
   goog.provide('ga_mouseposition_controller');
 
-  var module = angular.module('ga_mouseposition_controller', []);
+  var module = angular.module('ga_mouseposition_controller', [
+    'pascalprecht.translate'
+  ]);
 
   module.controller('GaMousePositionController',
-      function($scope) {
+      function($scope, $translate) {
+        var coordinatesFormat = function(coordinates) {
+          return $translate('coordinates_label') + ': ' +
+              ol.coordinate.toStringXY(coordinates, 0);
+        };
 
         $scope.mousePositionProjections = [{
           value: 'EPSG:21781',
           label: 'CH1903 / LV03',
-          format: ol.coordinate.createStringXY(0)
+          format: coordinatesFormat
         }, {
           value: 'EPSG:2056',
           label: 'CH1903+ / LV95',
-          format: ol.coordinate.createStringXY(0)
+          format: coordinatesFormat
         }, {
           value: 'EPSG:4326',
           label: 'WGS 84 (long/lat)',
