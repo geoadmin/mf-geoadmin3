2e8485dd89dc3b1bc55eb60054a18acebe8cb9b7
Éric Lemoine <eric.lemoine@gmail.com>
2013-09-26 10:14:24 +0200

    Use gaCatalogtreeMapUtils in catalogitem directive

--- src/components/catalogtree/CatalogitemDirective.js
+++ src/components/catalogtree/CatalogitemDirective.js
@@ -1,10 +1,12 @@
 (function() {
   goog.provide('ga_catalogitem_directive');
 
+  goog.require('ga_catalogtree_service');
   goog.require('ga_layer_metadata_popup_service');
   goog.require('ga_map_service');
 
   var module = angular.module('ga_catalogitem_directive', [
+    'ga_catalogtree_service',
     'ga_layer_metadata_popup_service',
     'ga_map_service'
   ]);
@@ -13,7 +15,8 @@
    * See examples on how it can be used
    */
   module.directive('gaCatalogitem',
-      function($compile, gaLayers, gaLayerMetadataPopup) {
+      function($compile, gaCatalogtreeMapUtils, gaLayers,
+          gaLayerMetadataPopup) {
         return {
           restrict: 'A',
           replace: true,
@@ -43,41 +46,11 @@
           }
         };
 
-        // Utility function that look up a layer by its id from the map.
-        function getMapLayer(map, id) {
-          var layer;
-          map.getLayers().forEach(function(l) {
-            if (l.get('id') == id) {
-              layer = l;
-            }
-          });
-          return layer;
-        }
-
-        function addLayer(map, item) {
-          // FIXME: we are super cautious here and display error messages
-          // when either the layer identified by item.idBod doesn't exist
-          // in the gaLayers service, or gaLayers cannot construct an ol
-          // layer object for that layer.
-          var error = true;
-          if (angular.isDefined(gaLayers.getLayer(item.idBod))) {
-            var layer = gaLayers.getOlLayerById(item.idBod);
-            if (angular.isDefined(layer)) {
-              error = false;
-              map.addLayer(layer);
-            }
-          }
-          if (error) {
-            alert('Layer not supported by gaLayers (' + item.idBod + ').');
-            item.errorLoading = true;
-          }
-        }
-
         function addPreviewLayer() {
           // "this" is the scope
           var item = this.item;
           var map = this.map;
-          var layer = getMapLayer(map, item.idBod);
+          var layer = gaCatalogtreeMapUtils.getMapLayer(map, item.idBod);
           if (!angular.isDefined(layer)) {
             // FIXME: we are super cautious here and display error messages
             // when either the layer identified by item.idBod doesn't exist
@@ -100,7 +73,7 @@
           // "this" is the scope
           var item = this.item;
           var map = this.map;
-          var layer = getMapLayer(map, item.idBod);
+          var layer = gaCatalogtreeMapUtils.getMapLayer(map, item.idBod);
           if (angular.isDefined(layer) && layer.preview) {
             map.removeLayer(layer);
             layer.preview = false;
@@ -111,7 +84,7 @@
           // "this" is the scope
           var item = this.item;
           var map = this.map;
-          var layer = getMapLayer(map, item.idBod);
+          var layer = gaCatalogtreeMapUtils.getMapLayer(map, item.idBod);
           return angular.isDefined(layer) && layer.preview;
         }
 
@@ -119,9 +92,9 @@
           // "this" is the scope
           var item = this.item;
           var map = this.map;
-          var layer = getMapLayer(map, item.idBod);
+          var layer = gaCatalogtreeMapUtils.getMapLayer(map, item.idBod);
           if (!angular.isDefined(layer)) {
-            addLayer(map, item);
+            gaCatalogtreeMapUtils.addLayer(map, item);
           } else {
             if (!layer.preview) {
               map.removeLayer(layer);
