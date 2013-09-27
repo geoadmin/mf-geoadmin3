f4ca618b051d50a1a971d65d0f1952a18aa99bde
loicgasser <loicgasser4@gmail.com>
2013-09-27 18:56:18 +0200

    Migrate kgs template

--- chsdi/models/vector/vbs.py
+++ chsdi/models/vector/vbs.py
@@ -8,8 +8,25 @@ from chsdi.models.vector import Vector
 
 Base = bases['vbs']
 
+class Kulturgueter(Base, Vector):
+    __tablename__ = 'kgs'
+    __table_args__ = ({'schema': 'babs', 'autoload': False})
+    __template__ = 'templates/htmlpopup/kgs.mako'
+    __queryable_attributes__ = ['zkob']
+    __esriId__ = 3003
+    __bodId__ = 'ch.babs.kulturgueter'
+    id = Column('kgs_nr', Integer, primary_key=True)
+    the_geom = GeometryColumn(Geometry(dimension=2, srid=21781))
+    zkob = Column('zkob', Text)
+    x = Column('x', Numeric)
+    y = Column('y', Numeric)
+    gemeinde = Column('gemeinde', Text)
+    kt_kz = Column('kt_kz', Text)
+
+register('ch.babs.kulturgueter', Kulturgueter)
+    
+
 class TERRITORIALREGIONEN(Base, Vector):
-    # view in a schema
     __tablename__ = 'territorialregionen'
     __table_args__ = ({'autoload': False})
     __template__ = 'templates/htmlpopup/territorialregionen.mako'
