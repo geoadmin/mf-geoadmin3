da02bf93b73fd99c45498ee24fbba17651cbf8c0
Gilbert Jeiziner <gjn@space.ch>
2013-10-23 14:02:19 +0200

    Add comment for understanding and change variable name

--- src/components/CatalogcollapsibleDirective.js
+++ src/components/CatalogcollapsibleDirective.js
@@ -14,9 +14,15 @@
     return {
       restrict: 'A',
       link: function(scope, element) {
-        scope.$on('gaCatalogState', function(event, state) {
-          if ((state !== false && element.hasClass('collapsed')) ||
-              (state === false && !element.hasClass('collapsed'))) {
+        scope.$on('gaCatalogState', function(event, showCatalog) {
+          // It's not possible to directly show or hide a collapsible,
+          // but we can only toggle the state by emulating a user click.
+          // To assure the showCatalog state is aplied correclt, we have
+          // to check he current state of the collapsible by looking
+          // for the collapsed class, because the user can change this
+          // at will at any time.
+          if ((showCatalog !== false && element.hasClass('collapsed')) ||
+              (showCatalog === false && !element.hasClass('collapsed'))) {
             element.trigger('click');
           }
         });
