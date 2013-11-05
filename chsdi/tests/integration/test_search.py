110129ce0da0f08588bef841f48cb8d489e65582
loicgasser <loicgasser4@gmail.com>
2013-11-05 14:01:26 +0100

    fix feature search

--- chsdi/tests/integration/test_search.py
+++ chsdi/tests/integration/test_search.py
@@ -88,3 +88,9 @@ class TestSearchServiceView(TestsBase):
         self.failUnless(resp.content_type == 'application/json')
         self.failUnless(resp.json['results'][0]['attrs']['detail'] == '880819 chemin isabelle-de-montolieu 1 1010 lausanne 5586 lausanne ch vd')
         self.failUnless(resp.json['results'][0]['attrs']['num'] == 1)
+
+    def test_features_bbox(self):
+        resp = self.testapp.get('/rest/services/ech/SearchServer', params={'features': 'ch.astra.ivs-reg_loc', 'type': 'features', 'bbox': '551306.5625,167918.328125,551754.125,168514.625'}, status=200)
+        self.failUnless(resp.content_type == 'application/json')
+        self.failUnless(resp.json['results'][0]['attrs']['origin'] == 'feature')
+        self.failUnless(resp.json['results'][0]['attrs']['id'] == 43543)
