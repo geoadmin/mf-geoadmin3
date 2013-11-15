36e751b790957024f9c683f33b6aab0a2f96a2d0
loicgasser <loicgasser4@gmail.com>
2013-11-15 17:47:46 +0100

    Remove profile module for now

--- src/js/GaModule.js
+++ src/js/GaModule.js
@@ -23,7 +23,6 @@
   goog.require('ga_tooltip');
   goog.require('ga_swipe');
   goog.require('ga_featuretree');
-  goog.require('ga_profile');
   goog.require('ga_modal_directive');
   goog.require('ga_draggable_directive');
   goog.require('ga_placeholder_directive');
@@ -66,7 +65,6 @@
     'ga_tooltip',
     'ga_swipe',
     'ga_featuretree',
-    'ga_profile',
     'ga_modal_directive',
     'ga_draggable_directive',
     'ga_placeholder_directive',
