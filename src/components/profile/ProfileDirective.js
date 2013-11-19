30898080a8ff5da4f2009f884d6b5846c907161a
loicgasser <loicgasser4@gmail.com>
2013-11-19 16:01:18 +0100

    Move gaPopup in directive, reorganize profile service, add axis

--- src/components/profile/ProfileDirective.js
+++ src/components/profile/ProfileDirective.js
@@ -1,29 +1,37 @@
 (function() {
   goog.provide('ga_profile_directive');
 
+  goog.require('ga_popup');
   goog.require('ga_profile_service');
 
   var module = angular.module('ga_profile_directive', [
-    'ga_profile_service'
+    'ga_profile_service',
+    'ga_popup',
+    'pascalprecht.translate'
   ]);
 
   module.directive('gaProfile',
-      function(gaProfileService) {
+      function($translate, gaProfileService, gaPopup) {
         return {
           restrict: 'A',
           templateUrl: 'components/profile/partials/profile.html',
           scope: {
             options: '=gaProfileOptions'
           },
-          replace: true,
-          transclude: true,
           link: function(scope, element, attrs) {
             var options = scope.options;
-
-            scope.updateProfile = gaProfileService;
+            var profile = gaProfileService(options);
 
             scope.$on('gaProfileDataLoaded', function(ev, data) {
-              scope.updateProfile(data, options);
+              var element = profile.create(data);
+              var popup = gaPopup.create({
+                title: $translate('profile_title'),
+                className: 'profile-popup',
+                content: angular.element(element).context.outerHTML,
+                x: 0,
+                y: 10
+              });
+              popup.open();
             });
           }
         };
--- src/components/profile/ProfileService.js
+++ src/components/profile/ProfileService.js
@@ -12,74 +12,110 @@
 
   module.provider('gaProfileService', function() {
 
-    function createGraph(element, data, options) {
+    function ProfileChart(options) {
       var marginHoriz = options.margin.left + options.margin.right;
       var marginVert = options.margin.top + options.margin.bottom;
 
       var width = options.width - marginHoriz;
       var height = options.height - marginVert;
 
-      var x = d3.scale.linear().range([0, width]);
-      var y = d3.scale.linear().range([height, 0]);
+      var createArea = function(domain, interpolationMethod) {
+        var x = domain.X;
+        var y = domain.Y;
+        var area = d3.svg.area()
+            .interpolate(interpolationMethod)
+            .x(function(d) { return x(d.dist); })
+            .y0(height)
+            .y1(function(d) { return y(d.alts.DTM25); });
+        return area;
+      };
 
-      var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient('bottom');
-      var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient('left');
+      var createAxis = function(domain) {
+        var xAxis = d3.svg.axis()
+            .scale(domain.X)
+            .orient('bottom');
+        var yAxis = d3.svg.axis()
+            .scale(domain.Y)
+            .orient('left');
+        return {
+          X: xAxis,
+          Y: yAxis
+        };
+      };
 
-      var area = d3.svg.area()
-          .x(function(d) { return x(d.dist); })
-          .y0(height)
-          .y1(function(d) { return y(d.alts.DTM25); });
+      this.x = d3.scale.linear().range([0, width]);
+      this.y = d3.scale.linear().range([height, 0]);
 
-      var svg = d3.select(element).append('svg')
-          .attr('width', width + marginHoriz)
-          .attr('height', height + marginVert)
-        .append('g')
-          .attr('transform', 'translate(' + options.margin.left +
-              ' ,' + options.margin.top + ')');
+      this.getXYDomains = function(data) {
+        this.x.domain(d3.extent(data, function(d) { return d.dist; }));
+        this.y.domain([0, d3.max(data, function(d) { return d.alts.DTM25; })]);
+        return {
+          X: this.x,
+          Y: this.y
+        };
+      };
 
-      x.domain(d3.extent(data, function(d) { return d.dist; }));
-      y.domain([0, d3.max(data, function(d) { return d.alts.DTM25; })]);
-
-      svg.append('path')
-          .datum(data)
-          .attr('class', 'area')
-          .attr('d', area);
-
-      svg.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', 'translate(0, ' + height + ')')
-          .call(xAxis);
-
-      svg.append('g')
-          .attr('class', 'y axis')
-          .call(yAxis)
-        .append('text')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end')
-          .text('Elevation');
-
-      return element;
-    }
-
-    this.$get = function($translate, gaPopup) {
-      return function(data, options) {
+      this.create = function(data) {
+        var that = this;
+        var domain = this.getXYDomains(data);
+        var axis = createAxis(domain);
         var element = document.createElement('DIV');
         element.className = 'profile-inner';
-        var template = createGraph(element, data, options);
-        template = angular.element(template).context.outerHTML;
-        var popup = gaPopup.create({
-          title: $translate('profile_title'),
-          content: template,
-          x: 10,
-          y: 10
-        });
-        popup.open();
+
+        this.svg = d3.select(element).append('svg')
+            .attr('width', width + marginHoriz)
+            .attr('height', height + marginVert)
+            .attr('class', 'profile-svg');
+
+        this.area = createArea(domain, 'cardinal');
+
+        this.svg.append('path')
+            .datum(data)
+            .attr('class', 'profile-area')
+            .attr('d', that.area)
+            .style('opacity', 0.9);
+
+        this.svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0, ' + height + ')')
+            .call(axis.X);
+
+        this.svg.append('g')
+            .attr('class', 'y axis')
+            .call(axis.Y)
+          .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end');
+
+        this.svg.append('text')
+            .attr('x', width / 2)
+            .attr('y', height + options.margin.bottom)
+            .style('text-anchor', 'middle')
+            .text('Distance');
+
+        this.svg.append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 0 - options.margin.left)
+            .attr('x', 0 - height / 2)
+            .attr('dy', '1em')
+            .text('Elevation');
+
+         return element;
+      };
+
+      this.remove = function() {
+        if (this.svg) {
+          this.svg.remove();
+        }
+      };
+    }
+
+    this.$get = function($translate) {
+      return function(options) {
+        var chart = new ProfileChart(options);
+        return chart;
       };
     };
   });
