9dc7d1b6fdf2c359b6e526ab16c3d35347c56887
loicgasser <loicgasser4@gmail.com>
2013-12-04 11:14:17 +0100

    Loader js is language dependent, default to de

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -1,4 +1,8 @@
-<% mode = request.params.get('mode') %>
+<%
+mode = request.params.get('mode')
+lang = request.lang
+layersconfig = 'chsdi:static/js/ol3/build/layersconfig.' + lang + '.js'
+%>
 
 <script type="text/javascript">
 (function() {
@@ -8,7 +12,7 @@ document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/resources/layout.css'))}" + '" />');
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/resources/bootstrap/css/bootstrap-responsive.min.css'))}" + '" />');
 // Load js
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/build/layersconfig.de.js'))}" + '"></scr' + 'ipt>')
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url(layersconfig))}" + '"></scr' + 'ipt>')
 % if mode == 'debug':
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/build/ga-whitespace.js'))}" + '"></scr' + 'ipt>');
 % else:
