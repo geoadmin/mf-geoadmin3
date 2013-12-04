a5f7ccfd7a5b5a76b5606da8d0ada3b09fefa41d
Cédric Moullet <cedric.moullet@gmail.com>
2013-12-04 09:37:08 +0100

    Add a first example

---
+++ chsdi/static/doc/examples/geoadmin_simple.js
@@ -0,0 +1,24 @@
+// Create a GeoAdmin Map
+var map = new ga.Map({
+  // Define the div where the map is placed
+  target: 'map',
+  // Create a 2D view
+  view: new ol.View2D({
+    // Define the default resolution
+    // 10 means that one pixel is 10m width and height
+    // List of resolution of the WMTS layers:
+    // 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1, 0.5, 0.25, 0.1
+    resolution: 10,
+    // Define a coordinate CH1903 for the center of the view
+    center: [561666.5, 185569.5]
+  })
+});
+
+// Create a backgrounc layer
+var lyr1 = ga.layer.create('ch.swisstopo.pixelkarte-farbe');
+// Create an overlay layer
+var lyr2 = ga.layer.create('ch.swisstopo.fixpunkte-agnes');
+
+// Add the layers in the map
+map.addLayer(lyr1);
+map.addLayer(lyr2);
