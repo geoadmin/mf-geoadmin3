1213084ad4bb349231ebb6546ba19d021c9adcdf
loicgasser <loicgasser4@gmail.com>
2013-12-05 15:43:51 +0100

    remove bootstrap, correct path enable caching

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -1,22 +1,19 @@
 <%
 mode = request.params.get('mode')
 lang = request.lang
-layersconfig = 'chsdi:static/js/ol3/build/layersconfig.' + lang + '.js'
+layersconfig = 'chsdi:static/js/layersconfig.' + lang + '.js'
 %>
 
 <script type="text/javascript">
 (function() {
 // Load css
-document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/css/ga.css'))}" + '" />');
-document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/resources/bootstrap/css/bootstrap.min.css'))}" +'" />');
-document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/resources/layout.css'))}" + '" />');
-document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/resources/bootstrap/css/bootstrap-responsive.min.css'))}" + '" />');
+document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
 // Load js
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url(layersconfig))}" + '"></scr' + 'ipt>')
 % if mode == 'debug':
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/build/ga-whitespace.js'))}" + '"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga-whitespace.js'))}" + '"></scr' + 'ipt>');
 % else:
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ol3/build/ga.js'))}" + '"></scr' + 'ipt>')
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga.js'))}" + '"></scr' + 'ipt>')
 % endif
 })();
 </script>
