689f0119e0109199778591e32005b1710d247a02
Cédric Moullet <cedric.moullet@gmail.com>
2013-12-06 13:10:25 +0100

    Add proj4js

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -10,6 +10,9 @@ layersconfig = 'chsdi:static/js/layersconfig.' + lang + '.js'
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
 // Load js
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url(layersconfig))}" + '"></scr' + 'ipt>')
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG21781.js'))}" + '"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG2056.js'))}" + '"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/proj4js-compressed.js'))}" + '"></scr' + 'ipt>');
 % if mode == 'debug':
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga-whitespace.js'))}" + '"></scr' + 'ipt>');
 % else:
