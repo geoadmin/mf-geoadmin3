f8aeca2b3eff7145cbb661935ccb4fe7ad2c8de6
loicgasser <loicgasser4@gmail.com>
2013-12-12 10:28:16 +0100

    Load layers config within loaderjs

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -1,14 +1,19 @@
+# -*- coding: utf-8 -*-
+
 <%
 mode = request.params.get('mode')
 lang = request.lang
-layersconfig = 'chsdi:static/js/layersconfig.' + lang + '.js'
+layersconfig = 'http://api3.geo.admin.ch/rest/services/all/MapServer/layersconfig?lang=' + lang
+import urllib2, json
+f = urllib2.urlopen(layersconfig)
+conf = """function getConfig(){ return %s } """ %json.dumps(json.loads(f.read())['layers'])
 %>
 
 (function() {
 // Load css
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
 // Load js
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url(layersconfig))}" + '"></scr' + 'ipt>')
+document.write('<scr' + 'ipt type="text/javascript">' + ${conf|n} + '</scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/proj4js-compressed.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG21781.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG2056.js'))}" + '"></scr' + 'ipt>');
