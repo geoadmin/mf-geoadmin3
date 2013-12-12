118032295ae418776bfddc2dff1dab768a51a094
Marc Monnerat <procrastinatio@gmail.com>
2013-12-12 14:17:33 +0100

    trying to usewq the accept_language

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -2,8 +2,15 @@
 
 <%
 mode = request.params.get('mode')
+<<<<<<< HEAD
 lang = request.lang
 # Because of elastic load balancer application url is always http
+lang = request.params.get('lang', None)
+if lang is None and request.accept_language:
+    AVAILABLE_LANGUAGES = ('de','fr','it','rm','en')
+    lang = request.accept_language.best_match(AVAILABLE_LANGUAGES, default_match='de')
+else:
+    lang = 'de'
 appUrl = request.application_url.replace('http', request.scheme)
 layersconfig = appUrl + '/rest/services/all/MapServer/layersconfig?lang=' + lang
 import urllib2, json
