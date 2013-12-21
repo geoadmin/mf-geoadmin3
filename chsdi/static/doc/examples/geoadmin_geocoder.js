70cb7cc3656e0c8814dc410bb8a824b1245c5b9d
Cédric Moullet <cedric.moullet@gmail.com>
2013-12-21 12:12:27 +0100

    Add geocoder example

---
+++ chsdi/static/doc/examples/geoadmin_geocoder.js
@@ -0,0 +1,28 @@
+// Create a GeoAdmin Map
+var map = new ga.Map({
+  
+  // Define the div where the map is placed
+  target: 'map',
+  
+  // Create a 2D view
+  view: new ol.View2D({
+    
+    // Define the default resolution
+    // 10 means that one pixel is 10m width and height
+    // List of resolution of the WMTS layers:
+    // 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1, 0.5, 0.25, 0.1
+    resolution: 10,
+    
+    // Define a coordinate CH1903 (EPSG:21781) for the center of the view
+    center: [561666.5, 185569.5]
+  })
+});
+
+// Create a background layer
+var lyr1 = ga.layer.create('ch.swisstopo.pixelkarte-farbe');
+
+// Add the layers in the map
+map.addLayer(lyr1);
+
+// Geocode a location
+map.geocode('Payerne');
