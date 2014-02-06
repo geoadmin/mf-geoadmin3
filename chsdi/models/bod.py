4bea2fc748b593ea5929dcbe401eab0ac3e76e58
loicgasser <loicgasser4@gmail.com>
2014-02-06 09:05:16 +0100

    Remove unused scheme declaration, since chsdi is now agnostic

--- chsdi/models/bod.py
+++ chsdi/models/bod.py
@@ -79,7 +79,6 @@ class LayersConfig(Base):
     def getLayerConfig(self, request):
         config = {}
         translate = request.translate
-        scheme = request.scheme
         for k in self.__dict__.keys():
             if not k.startswith("_") and \
                 self.__dict__[k] is not None and \
@@ -104,10 +103,9 @@ class LayersConfig(Base):
                 config['wmsUrl'] = config['wmsUrl'].replace('wms.geo.admin.ch', 'wms-bgdi0t.bgdi.admin.ch')
             if staging == 'integration':
                 config['wmsUrl'] = config['wmsUrl'].replace('wms.geo.admin.ch', 'wms-bgdi0i.bgdi.admin.ch')
-            config['wmsUrl'] = config['wmsUrl'].replace('http', scheme)
         # sublayers don't have attributions
         if 'attribution' in config:
-            config['attributionUrl'] = translate(self.__dict__['attribution'] + '.url').replace('http', scheme)
+            config['attributionUrl'] = translate(self.__dict__['attribution'] + '.url')
 
         return {self.idBod: config}
 
