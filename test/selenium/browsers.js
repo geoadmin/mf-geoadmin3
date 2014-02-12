b355b739e769cd02f83a6544cb755c2af2aab167
Oskar Truffer <oskar.truffer@gmail.com>
2014-02-12 08:16:17 +0100

    added different browsers.

--- test/selenium/browsers.js
+++ test/selenium/browsers.js
@@ -18,7 +18,47 @@ var capabilities = [{
     'browserstack.user' : process.env.BROWSERSTACK_USER,
     'browserstack.key' : process.env.BROWSERSTACK_KEY,
     'browserstack.debug' : 'true'
-  }
+  },
+  {
+    'browser' : 'IE',
+    'browser_version' : '10.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1280x1024',
+    'browserstack.user' : process.env.BROWSERSTACK_USER,
+    'browserstack.key' : process.env.BROWSERSTACK_KEY,
+    'browserstack.debug' : 'true'
+  },
+  {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1280x1024',
+    'browserstack.user' : process.env.BROWSERSTACK_USER,
+    'browserstack.key' : process.env.BROWSERSTACK_KEY,
+    'browserstack.debug' : 'true'
+  },
+  {
+    'browser' : 'Firefox',
+    'browser_version' : '27.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1280x1024',
+    'browserstack.user' : process.env.BROWSERSTACK_USER,
+    'browserstack.key' : process.env.BROWSERSTACK_KEY,
+    'browserstack.debug' : 'true'
+  },
+  {
+    'browser' : 'Safari',
+    'browser_version' : '5.1',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1280x1024',
+    'browserstack.user' : process.env.BROWSERSTACK_USER,
+    'browserstack.key' : process.env.BROWSERSTACK_KEY,
+    'browserstack.debug' : 'true'
+  } 
 ]
 
 module.exports.capabilities = capabilities;
