2ae7de40c5b1e8e7430d833d40827ee2e735d471
Gilbert Jeiziner <gjn@space.ch>
2014-02-18 13:28:36 +0100

    Adding seo service

---
+++ src/components/seo/SeoService.js
@@ -0,0 +1,42 @@
+(function() {
+  goog.provide('ga_seo_service');
+
+  goog.require('ga_permalink_service');
+
+  var module = angular.module('ga_seo_service', [
+    'ga_permalink_service'
+  ]);
+
+  /**
+   * The seo service.
+   *
+   * The serice takes care to capture the application state
+   * relevant to seo  at initialisation.
+   *
+   * We can't put this in the directive because of how
+   * the initialisation of the application works
+   *
+   */
+  module.provider('gaSeoService', function() {
+    this.$get = function(gaPermalink) {
+
+      var layersAtStart = gaPermalink.getParams().layers ?
+                          gaPermalink.getParams().layers.split(',') : [];
+
+      var isSnapshot = gaPermalink.getParams().snapshot == 'true';
+
+      var SeoService = function() {
+        this.isSnapshot = function() {
+          return isSnapshot;
+        };
+
+        this.getLayers = function() {
+          return layersAtStart;
+        };
+      };
+
+      return new SeoService();
+    };
+  });
+
+})();
