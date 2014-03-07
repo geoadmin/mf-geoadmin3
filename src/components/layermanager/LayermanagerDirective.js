449ffe13a8ccd5500ca6f0e05d5516cfd564cd6d
Marc Monnerat <procrastinatio@gmail.com>
2014-03-07 14:37:30 +0100

    Not displaying dummy timestamp for lubis layers.
    For LUBIS layer we use a 'dummy' timestamp to display all available
    year. As this timestamp has not real meaning, it must not be displayed.

--- src/components/layermanager/LayermanagerDirective.js
+++ src/components/layermanager/LayermanagerDirective.js
@@ -20,6 +20,23 @@
       return items.slice().reverse();
     };
   });
+  /**
+   * Filter for discarding unplausible timestamp year. A layer with
+   * timestamp of '99991231' is used to combine all layers.
+   * This is a temporay solution, and has to be reverted once we
+   * switch back from WMTS to WMS source.
+   */
+  module.filter('gaPlausibleYear', function() {
+    return function(input) {
+      // input is the timestamp applied to a layer, it can be a '1978' or
+      // '19783112' or undefined or ''
+      if (!input || input.length < 4) {
+        return '';
+      }
+      var yearStr = input.substring(0, 4);
+      return (parseInt(yearStr) <= (new Date()).getFullYear()) ? yearStr : '';
+    }
+  });
 
   module.directive('gaLayermanager',
       function(gaLayers, gaLayerMetadataPopup, gaBrowserSniffer,
