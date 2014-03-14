6837bc78297b81ee65f4cad6d1df74997d4dae8b
Marc Monnerat <procrastinatio@gmail.com>
2014-03-14 20:45:20 +0100

    splitting externals

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -7,23 +7,24 @@ appUrl = request.application_url.replace('http:', request.scheme + ':')
 layersconfig = appUrl + '/rest/services/all/MapServer/layersConfig?lang=' + lang
 import urllib2
 f = urllib2.urlopen(layersconfig)
-conf = """function getConfig(){ return %s } """ %f.read()
 defaultLang = """function getDefaultLang() { return "%s" } """ % request.lang
 %>
 
 (function() {
 var load = function() {
-  window.GeoAdmin = {};
-  window.GeoAdmin.lang = "${lang}";
+window.GeoAdmin = window.GeoAdmin || {};
+window.GeoAdmin.lang = "${lang}";
 }
 window.addEventListener ? 
 window.addEventListener("load", load, false) :
 window.attachEvent && window.attachEvent("onload", load);
 
+
 // Load css
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
 // Load js
 document.write('<scr' + 'ipt type="text/javascript">' + ${defaultLang|n} + '</scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/serverconfig.js'))}" +  '"></scr' + 'ipt>');
 % if lang == 'en':
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/layersconfig.en.js'))}" +  '"></scr' + 'ipt>');
 % elif lang == 'fr':
@@ -44,3 +45,4 @@ document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(requ
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga.js'))}" + '"></scr' + 'ipt>')
 % endif
 })();
+
