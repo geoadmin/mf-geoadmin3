289c5cd919a8426d386c0dff3165945933255445
Marc Monnerat <procrastinatio@gmail.com>
2014-03-18 14:46:54 +0100

    Putting the layersconfig in the loader as it previously was.
    Note: this is a quick fix, as the loader is never cached.

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -7,35 +7,25 @@ appUrl = request.application_url.replace('http:', request.scheme + ':')
 layersconfig = appUrl + '/rest/services/all/MapServer/layersConfig?lang=' + lang
 import urllib2
 f = urllib2.urlopen(layersconfig)
+layersconfig = """if (typeof window['GeoAdmin'] == 'undefined') window['GeoAdmin'] =  {}; window.GeoAdmin.getConfig  = function(){ return %s } """ %f.read()
 defaultLang = """function getDefaultLang() { return "%s" } """ % request.lang
 %>
 
 (function() {
 var load = function() {
-window.GeoAdmin = window.GeoAdmin || {};
+if (typeof window['GeoAdmin'] == 'undefined') window.GeoAdmin = {};
 window.GeoAdmin.lang = "${lang}";
 }
 window.addEventListener ? 
 window.addEventListener("load", load, false) :
 window.attachEvent && window.attachEvent("onload", load);
 
-
+${layersconfig|n}
 // Load css
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
 // Load js
 document.write('<scr' + 'ipt type="text/javascript">' + ${defaultLang|n} + '</scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/serverconfig.js'))}" +  '"></scr' + 'ipt>');
-% if lang == 'en':
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/layersconfig.en.js'))}" +  '"></scr' + 'ipt>');
-% elif lang == 'fr':
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/layersconfig.fr.js'))}" + '"></scr' + 'ipt>');
-% elif lang == 'it':
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/layersconfig.it.js'))}" + '"></scr' + 'ipt>');
-% elif lang == 'rm':
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/layersconfig.rm.js'))}" + '"></scr' + 'ipt>');
-% else:
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/layersconfig.de.js'))}" + '"></scr' + 'ipt>');
-% endif
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/proj4js-compressed.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG21781.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG2056.js'))}" + '"></scr' + 'ipt>');
