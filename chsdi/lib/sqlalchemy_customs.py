f4e9796f5eeb8c9090a54aee0c99a981ba53b61f
loicgasser <loicgasser4@gmail.com>
2014-03-25 11:40:36 +0100

    Add support for remove_accents in sqlalchemy

---
+++ chsdi/lib/sqlalchemy_customs.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql.expression import FunctionElement
+
+
+class remove_accents(FunctionElement):
+    name = "remove_accents"
+
+
+@compiles(remove_accents)
+def compile(element, compiler, **kw):
+    return "remove_accents(%s)" % compiler.process(element.clauses)
