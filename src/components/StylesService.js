f8dd6dd31b7a8318602b662e2750b5f4930e364e
loicgasser <loicgasser4@gmail.com>
2014-05-08 10:15:14 +0200

    Move geolocation styles in styles service

--- src/components/StylesService.js
+++ src/components/StylesService.js
@@ -50,10 +50,31 @@
       })
     });
 
+    var geolocationStyle = new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: [255, 0, 0, 0.1]
+      }),
+      stroke: new ol.style.Stroke({
+        color: [255, 0, 0, 0.9],
+        width: 3
+      }),
+      image: new ol.style.Circle({
+        radius: 5,
+        fill: new ol.style.Fill({
+          color: [255, 0, 0, 0.9]
+        }),
+        stroke: new ol.style.Stroke({
+          color: [255, 0, 0, 1],
+          width: 3
+        })
+      })
+    });
+
     var styles = {
       'select': selectStyle,
       'highlight': hlStyle,
-      'selectrectangle': srStyle
+      'selectrectangle': srStyle,
+      'geolocation': geolocationStyle
     };
 
     this.$get = function() {
