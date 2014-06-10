71b58daba1e91850ba82379e64a65494578c9cd8
loicgasser <loicgasser4@gmail.com>
2014-06-10 14:01:33 +0200

    Add example

--- chsdi/tests/integration/test_qrcode.py
+++ chsdi/tests/integration/test_qrcode.py
@@ -6,8 +6,8 @@ from chsdi.tests.integration import TestsBase
 class TestQRCodeView(TestsBase):
 
     def test_qrcode(self):
-        resp = self.testapp.get('/qrcode', params={'url': 'http://s.geo.admin.ch/e83c57af1'}, status=200)
+        resp = self.testapp.get('/qrcodegenerator', params={'url': 'http://s.geo.admin.ch/e83c57af1'}, status=200)
         self.failUnless(resp.content_type == 'image/png')
 
     def test_qrcode_badurl(self):
-        resp = self.testapp.get('/qrcode', params={'url': 'http://dummy.com'}, status=400)
+        resp = self.testapp.get('/qrcodegenerator', params={'url': 'http://dummy.com'}, status=400)
