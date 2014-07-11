adcda2de173cded32f2f1ccef3d3585d707a5bf7
Gilbert Jeiziner <gjn@space.ch>
2014-07-11 09:56:15 +0200

    Add possibility to run nosetest in any staging

---
+++ nose_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Adapt your ~/.pgpass file for this to work
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+conf_to_use="buildout_nose_dev.cfg"
+
+while getopts "ip" opt; do
+  case "$opt" in
+  i) conf_to_use="buildout_nose_int.cfg"
+     ;;
+  p) conf_to_use="buildout_nose_prod.cfg"
+     ;;
+  esac
+done
+
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
+mv -f production.ini production.ini.bup
+mv -f development.ini development.ini.bup
+
+buildout/bin/buildout -c $conf_to_use install nosetest-ini && buildout/bin/nosetests
+
+rc=$?
+
+mv -f production.ini.bup production.ini
+mv -f development.ini.bup development.ini
+
+exit $rc
+
