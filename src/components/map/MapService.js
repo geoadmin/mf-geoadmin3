b908bf54c2d36dec3f4b0bbb59cad1f85309bbcb
loicgasser <loicgasser4@gmail.com>
2014-09-23 12:37:20 +0200

    Add missing dependency in map service

--- src/components/map/MapService.js
+++ src/components/map/MapService.js
@@ -1,6 +1,7 @@
 (function() {
   goog.provide('ga_map_service');
 
+  goog.require('ga_networkstatus_service');
   goog.require('ga_offline_service');
   goog.require('ga_storage_service');
   goog.require('ga_styles_service');
@@ -8,6 +9,7 @@
 
   var module = angular.module('ga_map_service', [
     'pascalprecht.translate',
+    'ga_networkstatus_service',
     'ga_offline_service',
     'ga_storage_service',
     'ga_styles_service',
