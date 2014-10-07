eb31e217ac051865a7773ca38c54481f60a6e327
Marc Monnerat <procrastinatio@gmail.com>
2014-10-07 09:49:13 +0200

    correcting the UTM grid zone for CH

--- src/js/MousePositionController.js
+++ src/js/MousePositionController.js
@@ -39,13 +39,13 @@
           label: 'UTM',
           format: function(coordinates) {
             if (coordinates[0] < 6 && coordinates [0] >= 0) {
-              var utm_31n = ol.proj.transform(coordinates,
+              var utm_31t = ol.proj.transform(coordinates,
                 'EPSG:4326', 'EPSG:32631');
-              return coordinatesFormatUTM(utm_31n, '(zone 31N)');
+              return coordinatesFormatUTM(utm_31t, '(zone 31T)');
             } else if (coordinates[0] < 12 && coordinates [0] >= 6) {
-              var utm_32n = ol.proj.transform(coordinates,
+              var utm_32t = ol.proj.transform(coordinates,
                 'EPSG:4326', 'EPSG:32632');
-              return coordinatesFormatUTM(utm_32n, '(zone 32N)');
+              return coordinatesFormatUTM(utm_32t, '(zone 32T)');
             } else {
               return '-';
             }
