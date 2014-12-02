32a838c2ebd3954c1a073ad381543b5262a26220
Marc Monnerat <procrastinatio@gmail.com>
2014-12-02 20:57:04 +0100

    revoming the decorator for mapproxy

--- mapproxy/createWsgi.py
+++ mapproxy/createWsgi.py
@@ -22,23 +22,6 @@ for path in reversed(syspaths):
 from paste.deploy import loadapp
 
 
-class Auth():
-
-    def __init__(self, app):
-        self._app = app
-
-    def __call__(self, environ, start_response):
-        if environ.get('HTTP_X_SEARCHSERVER_AUTHORIZED') == 'true':
-            return self._app(environ, start_response)
-        else:
-            return self._forbidden(environ, start_response)
-
-    def _forbidden(self, environ, start_response):
-        start_response('403 Forbidden',
-            [('Content-Type', 'text/plain')])
-        return [b'You must have a registered referer to use this service.']
-
-
 # If you want to debug, uncomment the following lines
 #from paste.script.util.logging_config import fileConfig
 #fileConfig(r'%(log_config)s', {'here': os.path.dirname(__file__)})
@@ -46,7 +29,7 @@ class Auth():
 
 from mapproxy.wsgiapp import make_wsgi_app
 configfile="%(config)s"
-application = Auth(make_wsgi_app(configfile))
+application = make_wsgi_app(configfile)
 
 """
 
