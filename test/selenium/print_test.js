e77267d043b6abc4060d0c8751237c7924f3a2d0
Gilbert Jeiziner <gjn@space.ch>
2015-02-17 10:46:26 +0100

    Take animation into account for print tests

--- test/selenium/print_test.js
+++ test/selenium/print_test.js
@@ -5,8 +5,8 @@ var webdriver = require('browserstack-webdriver');
 var runTest = function(cap, driver, target) {
   // Click on "Drucken"
   driver.findElement(webdriver.By.xpath("//a[@id='printHeading']")).click();
-  // Wait 1 sec for the menu to pull down
-  driver.findElement(webdriver.By.xpath("//a[@id='printHeading' and @aria-expanded='true']"));
+  // Wait until print is opened and animation is finished
+  driver.findElement(webdriver.By.xpath("//div[@id='print' and contains(@class, 'collapse in')]"));
   // Try Print
   driver.findElement(webdriver.By.xpath("//button[contains(text(), 'Drucken')]")).click();
   // Is it success?
