a365e15b5d9c745bac92e436f83cd72d3ff2c5b3
oterral <olivier.terral@swisstopo.ch>
2015-02-23 15:13:35 +0100

    Display an alert message when popup has not been created

--- src/components/PrintService.js
+++ src/components/PrintService.js
@@ -1,6 +1,9 @@
 (function() {
   goog.provide('ga_print_service');
-  var module = angular.module('ga_print_service', []);
+
+  var module = angular.module('ga_print_service', [
+    'pascalprecht.translate'
+  ]);
 
   module.provider('gaPrintService', function() {
     var windowPrint;
@@ -21,7 +24,7 @@
       return '<link href="' + cssLink + '" rel="stylesheet" type="text/css">';
     };
 
-    this.$get = function($window) {
+    this.$get = function($window, $translate) {
       var Print = function() {
         this.htmlPrintout = function(body, head, onLoad) {
           $window.printOnLoad = onLoad || function(windowPrint) {
@@ -31,6 +34,10 @@
             windowPrint.close();
           }
           windowPrint = $window.open('', 'printout', 'height=400, width=600');
+          if (!windowPrint) {
+            alert($translate.instant('popup_blocked'));
+            return;
+          }
           windowPrint.document.write(buildHtml(body, head));
           windowPrint.document.close();
         };
