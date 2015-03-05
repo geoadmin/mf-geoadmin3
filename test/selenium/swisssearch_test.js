192276fbd0b9384a06ce02566f82f86e9e33049f
ponceta <arnaud.poncet@swisstopo.ch>
2015-03-05 08:31:27 +0100

    check if page is loaded before testing content

--- test/selenium/swisssearch_test.js
+++ test/selenium/swisssearch_test.js
@@ -8,6 +8,8 @@ var QUERYSTRING_MOOS = "X=128630.00&Y=627650.00";
 var runTest = function(cap, driver, target){
   //swissearch parameter with multiple results
   driver.get(target + '/?swisssearch=raron&lang=de');
+  //wait until topics related stuff is loaded. We know this when catalog is there
+  driver.findElement(webdriver.By.xpath("//a[contains(text(), 'Grundlagen und Planung')]"));
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Raron')]"));
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Flugfeld')]")).click();
   driver.findElement(webdriver.By.xpath("//a[contains(@href, '" + QUERYSTRING_OF_RARON + "')]"));
@@ -18,6 +20,8 @@ var runTest = function(cap, driver, target){
 
   //swisssearch parameter with multiple results (locations and layers), reset selection
   driver.get(target + '/?swisssearch=wasser&lang=de');
+  //wait until topics related stuff is loaded. We know this when catalog is there
+  driver.findElement(webdriver.By.xpath("//a[contains(text(), 'Grundlagen und Planung')]"));
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Standorte')]"));
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Karten')]"));
   driver.findElement(webdriver.By.xpath("//i[@ng-click='clearInput()']")).click();
@@ -29,6 +33,8 @@ var runTest = function(cap, driver, target){
   //swissearch parameter with 1 result (direct selection doesn't work in safari 5.1)
   if (!(cap.browser == 'Safari' && cap.browser_version == '5.1')) {
     driver.get(target + '/?swisssearch=brückenmoostrasse 11 raron&lang=de');
+    //wait until topics related stuff is loaded. We know this when catalog is there
+    driver.findElement(webdriver.By.xpath("//a[contains(text(), 'Grundlagen und Planung')]"));
     driver.findElement(webdriver.By.xpath("//a[contains(@href, '" + QUERYSTRING_MOOS + "')]"));
     //parameter stays after initial automatic selection
     driver.findElement(webdriver.By.xpath("//input[@ng-model='permalinkValue']")).getAttribute("value").then(function(val) {
