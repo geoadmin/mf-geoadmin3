ea7cd3f69b4457145065b28c54552d5fffe1bdf4
Marc Monnerat <procrastinatio@gmail.com>
2015-04-22 14:11:22 +0200

    Service to store user content in a bucket.
    With each file is an fileId/adminId associate to allow user to edit its
    file. The adminId/fileid association is stored in a dynamoDB table. The
    files are in an AWS bucket.
    
    - File are checked for Content-Type, size and XML well-formedness
    - Only request from map.geo.admin.ch are consiered
    - Method POST, GET, OPTIONS and DELETE
    - Integration tests for services
    - Varnish tests for service "files"

--- chsdi/models/clientdata_dynamodb.py
+++ chsdi/models/clientdata_dynamodb.py
@@ -42,11 +42,11 @@ table.delete()
 # http://boto.readthedocs.org/en/latest/boto_config_tut.html
 
 
-def get_table():
+def get_table(table_name='shorturl'):
 
     # url_short is the pkey
     try:
         conn = connect_to_region(region_name='eu-west-1')
-        return conn.get_table('shorturl')
+        return conn.get_table(table_name)
     except Exception as e:
         raise exc.HTTPBadRequest('Error during connection %s' % e)
