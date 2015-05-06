642df6ea9baf9b463c287ad07fe76c3699eb9aeb
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-06 10:29:48 +0200

    Change script to generate translations
    This script is shorter and easier to understand than the previous one.
    It currently only works with CSV file and has no support of
    gspreadsheet.

---
+++ scripts/translation2json.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+translation2js.py
+This script is used to create the {lang}.json files from the csv.
+
+Usage: ./env/bin/python translation2js.py [CSV_FILE [JSON_FOLDER]]
+default: CSV_FILE: src/locales/translations.csv, JSON_FOLDER: src/locales/
+"""
+
+import sys
+import csv
+import json
+
+from os.path import join
+
+
+def main():
+    if len(sys.argv) > 1:
+        csv_filename = sys.argv[1]
+        if len(sys.argv) > 2:
+            json_folder = sys.argv[2]
+    else:
+        csv_filename = 'src/locales/translations.csv'
+        json_folder = 'src/locales/'
+
+    with open(csv_filename, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
+        translations = {lang: {} for lang in csv_reader.fieldnames if lang != 'key'}
+        for row in csv_reader:
+            key = row['key']
+            for lang, traduction in [(key, value) for key, value in row.items() if key != 'key']:
+                translations[lang][key] = traduction
+    for lang in translations:
+        with open(join(json_folder, lang + '.json'), 'w') as json_file:
+            json.dump(translations[lang], json_file, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
