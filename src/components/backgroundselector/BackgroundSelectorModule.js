4342f29640d78f5796aecc77cd84a05fa6fdf872
Gilbert Jeiziner <gjn@space.ch>
2015-05-15 08:16:12 +0200

    Rename layerselector to background selector

---
+++ src/components/backgroundselector/BackgroundSelectorModule.js
@@ -0,0 +1,9 @@
+(function() {
+  goog.provide('ga_backgroundselector');
+
+  goog.require('ga_backgroundselector_directive');
+
+  angular.module('ga_backgroundselector', [
+    'ga_backgroundselector_directive'
+  ]);
+})();
--- src/components/layerselector/LayerSelectorModule.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  goog.provide('ga_layerselector');
-
-  goog.require('ga_layerselector_directive');
-
-  angular.module('ga_layerselector', [
-    'ga_layerselector_directive'
-  ]);
-})();
