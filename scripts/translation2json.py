8b019d1ec17982f5100d1cd104aa8b4ad22d0abf
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-27 16:43:49 +0200

    Allow translation2json to fetch translation from gspread
    - Compatible with python 2 and 3

--- scripts/translation2json.py
+++ scripts/translation2json.py
@@ -7,35 +7,99 @@ Usage: ./env/bin/python translation2js.py [CSV_FILE [JSON_FOLDER]]
 default: CSV_FILE: src/locales/translations.csv, JSON_FOLDER: src/locales/
 """
 
-import sys
+import argparse
 import csv
 import json
+import gspread
+import sys
 
 from os.path import join
+from oauth2client.client import SignedJwtAssertionCredentials
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
-def main():
-    if len(sys.argv) > 1:
-        csv_filename = sys.argv[1]
-        if len(sys.argv) > 2:
-            json_folder = sys.argv[2]
+def main(args, delimiter=',', quotechar='"'):
+    translations = {}
+    if args.gspreads is not None:
+        for key in args.gspreads:
+            csv_reader = _get_csv_reader_from_gspread(
+                key,
+                args.key_file[0],
+                delimiter=delimiter,
+                quotechar=quotechar
+            )
+            translations = _process_csv_file(csv_reader, translations)
+
+    if args.files is not None:
+        for filename in args.files:
+            with open(filename, 'r') as csv_file:
+                csv_reader = csv.DictReader(csv_file, delimiter=delimiter, quotechar=quotechar)
+                translations = _process_csv_file(csv_reader, translations)
+
+    _save_translations(translations, args.output_folder)
+
+
+def _get_csv_reader_from_gspread(key, key_file, delimiter=',', quotechar='"', **kwargs):
+    json_key = json.load(open(key_file))
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = SignedJwtAssertionCredentials(
+        json_key['client_email'],
+        json_key['private_key'].encode('utf-8'),
+        scope
+    )
+    gc = gspread.authorize(credentials)
+    wks = gc.open_by_key(key).sheet1
+    resp = wks.export()
+    if resp.status == 200:
+        if sys.version_info[0] == 2:
+            content = resp.read()
+        else:
+            content = resp.read().decode('utf-8')
+        csv_content = StringIO(content)
+        return csv.DictReader(csv_content, delimiter=delimiter, quotechar=quotechar, **kwargs)
     else:
-        csv_filename = 'src/locales/translations.csv'
-        json_folder = 'src/locales/'
+        print('Unable to open gspread: {}.'.format(key))
 
-    with open(csv_filename, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
-        translations = {lang: {} for lang in csv_reader.fieldnames if lang != 'key'}
-        for row in csv_reader:
-            json_key = row['key']
-            for lang, traduction in [(key, value) for key, value in row.items() if key != 'key']:
-                translations[lang][json_key] = traduction
+
+def _process_csv_file(csv_reader, translations):
+    if not translations:
+        translations = _init_translations(csv_reader.fieldnames)
+    for row in csv_reader:
+        json_key = row['key']
+        for lang, traduction in [(key, value) for key, value in row.items() if key != 'key']:
+            translations[lang][json_key] = traduction
+
+    return translations
+
+
+def _init_translations(fieldnames):
+    return {lang: {} for lang in fieldnames if lang != 'key'}
+
+
+def _save_translations(translations, output_folder):
     for lang in translations:
-        with open(join(json_folder, lang + '.json'), 'w') as json_file:
+        with open(join(output_folder, lang.lower() + '.json'), 'w') as json_file:
             # "separators=(',', ': ')" is required to avoid a trailing whitespace in python < 3.4
             json.dump(translations[lang], json_file, sort_keys=True, indent=4, ensure_ascii=False,
-                        separators=(',', ': '))
+                      separators=(',', ': '))
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Update translations. Files will be parsed in the '
+                                     'order you pass them. gspread will be parsed before CSV files. '
+                                     'The fist file or spreadsheet will be used to define the '
+                                     'available languages.')
+    parser.add_argument('-g', '--gspread', help='List of gspread to parse', dest='gspreads', nargs='*')
+    parser.add_argument('-f', '--files', help='List of CSV file to parse', dest='files', nargs='*')
+    parser.add_argument('-k', '--key', help='OAuth 2 json key file to use', dest='key_file', nargs=1)
+    parser.add_argument(
+        '-o',
+        '--output-folder',
+        help='Folder in which to save the translations files',
+        dest='output_folder',
+        default='src/locales'
+    )
+    main(parser.parse_args())
