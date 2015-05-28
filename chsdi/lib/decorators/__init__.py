3dc78144d650a974681fb8c12bf56a41cfa208cb
Gilbert Jeiziner <gjn@space.ch>
2015-05-28 11:50:03 +0200

    Assure that files service works with IE9

--- chsdi/lib/decorators/__init__.py
+++ chsdi/lib/decorators/__init__.py
@@ -5,8 +5,11 @@ import xml.parsers.expat
 
 from pyramid.view import view_config
 import pyramid.httpexceptions as exc
-       
 
+import urllib
+
+
+EXPECTED_CONTENT_TYPE = 'application/vnd.google-earth.kml+xml'
 
 def requires_authorization():
     def wrapper(f):
@@ -34,10 +37,15 @@ def validate_kml_input():
 
             MAX_FILE_SIZE = 1024 * 1024 * 2
 
-            if request.content_type != 'application/vnd.google-earth.kml+xml':
+            # IE9 simply doesn't send content_type header. So we set it ourself
+            if request.user_agent is not None and 'MSIE 9.0' in request.user_agent:
+                request.content_type = EXPECTED_CONTENT_TYPE
+
+            if request.content_type != EXPECTED_CONTENT_TYPE:
                 raise exc.HTTPUnsupportedMediaType('Only KML file are accepted')
 
-            data = request.body
+            # IE9 sends data urlencoded
+            data = urllib.unquote_plus(request.body)
             if len(data) > MAX_FILE_SIZE:
                 raise exc.HTTPRequestEntityTooLarge('File size exceed %s bytes' % MAX_FILE_SIZE)
             try:
@@ -45,6 +53,8 @@ def validate_kml_input():
                 p.Parse(data)
             except Exception:
                 raise exc.HTTPUnsupportedMediaType('Only valid KML file are accepted')
+
+            request.body = data
 
             return func(self, *args, **kwargs)
         return wrapper
