d80906289cec010e01d2cbef88b5e931362212eb
Gilbert Jeiziner <gjn@space.ch>
2015-05-28 10:03:08 +0200

    Use reframe service directly

--- src/js/ContextPopupController.js
+++ src/js/ContextPopupController.js
@@ -8,7 +8,7 @@
         $scope.options = {
           heightUrl: gaGlobalOptions.apiUrl + '/rest/services/height',
           qrcodeUrl: gaGlobalOptions.apiUrl + '/qrcodegenerator',
-          lv03tolv95Url: gaGlobalOptions.apiUrl + '/reframe/lv03tolv95'
+          lv03tolv95Url: 'https://geodesy.geo.admin.ch/reframe/lv03tolv95'
         };
 
       });
