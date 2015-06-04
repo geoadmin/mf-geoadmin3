2215035f78cfcd0aa3a8a4e3228ef5703652ed20
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-06-04 14:38:59 +0200

    Display the print rectangle
    - Change when updating the format
    - Change when updating the scale

--- src/components/print/PrintDirective.js
+++ src/components/print/PrintDirective.js
@@ -13,8 +13,12 @@
           $window, $timeout, $q, $http,
           ngeoCreatePrint, ngeoPrintUtils) {
 
+    var canceller;
     var print = ngeoCreatePrint($scope.options.printPath);
     var printConfigLoaded = false;
+    var deregister = [];
+    var POINTS_PER_INCH = 72; //PostScript points 1/72"
+    var MM_PER_INCHES = 25.4;
 
     // Get print config
     var updatePrintConfig = function() {
@@ -23,21 +27,95 @@
         timeout: canceller.promise
       }).success(function(data) {
         $scope.capabilities = data;
+        $scope.layouts = [];
 
-        angular.forEach($scope.capabilities.layouts, function(lay) {
-          lay.stripped = lay.name.substr(2);
+        angular.forEach($scope.capabilities.layouts, function(layout) {
+          var clientInfo = layout.attributes[4].clientInfo;
+          $scope.layouts.push({
+            name: layout.name,
+            scales: clientInfo.scales.reverse(),
+            paperSize: [clientInfo.width, clientInfo.height],
+            dpis: clientInfo.dpiSuggestions
+          });
         });
 
         // default values:
-        $scope.layout = data.layouts[0];
-        $scope.dpis = $scope.layout.attributes[4].clientInfo.dpiSuggestions;
-        $scope.dpi = $scope.dpis[0];
-        $scope.scales = $scope.layout.attributes[4].clientInfo.scales;
-        $scope.scale = $scope.scales[0];
+        $scope.layout = $scope.layouts[0];
+        $scope.dpi = $scope.layout.dpis[0];
+        var mapSize = $scope.map.getSize();
+        var mapResolution = $scope.map.getView().getResolution();
+        $scope.scale = ngeoPrintUtils.getOptimalScale(mapSize, mapResolution,
+                $scope.layout.paperSize, $scope.layout.scales);
         $scope.options.legend = false;
         $scope.options.graticule = false;
       });
     };
+
+    var activate = function() {
+      deregister = [
+        $scope.map.on('postcompose', handlePostCompose)
+      ];
+      var mapSize = $scope.map.getSize();
+      var viewResolution = $scope.map.getView().getResolution();
+      $scope.scale = ngeoPrintUtils.getOptimalScale(mapSize, viewResolution,
+          $scope.layout.paperSize, $scope.layout.scales);
+      refreshComp();
+    };
+
+    var calculatePageBoundsPixels = function(scale) {
+      var s = parseFloat(scale.value);
+      var size = $scope.layout.paperSize; // papersize in dot!
+      var view = $scope.map.getView();
+      var resolution = view.getResolution();
+      var w = size.width / POINTS_PER_INCH * MM_PER_INCHES / 1000.0 *
+          s / resolution * ol.has.DEVICE_PIXEL_RATIO;
+      var h = size.height / POINTS_PER_INCH * MM_PER_INCHES / 1000.0 *
+          s / resolution * ol.has.DEVICE_PIXEL_RATIO;
+      var mapSize = $scope.map.getSize();
+      var center = [mapSize[0] * ol.has.DEVICE_PIXEL_RATIO / 2 ,
+          mapSize[1] * ol.has.DEVICE_PIXEL_RATIO / 2];
+
+      var minx, miny, maxx, maxy;
+
+      minx = center[0] - (w / 2);
+      miny = center[1] - (h / 2);
+      maxx = center[0] + (w / 2);
+      maxy = center[1] + (h / 2);
+      return [minx, miny, maxx, maxy];
+    };
+
+    var deactivate = function() {
+      if (deregister) {
+        for (var i = 0; i < deregister.length; i++) {
+          ol.Observable.unByKey(deregister[i]);
+        }
+      }
+      refreshComp();
+    };
+
+    var refreshComp = function () {
+      if ($scope.options.active) {
+        printRectangle = calculatePageBoundsPixels($scope.scale);
+        $scope.map.render();
+      }
+      $scope.map.render();
+    };
+
+    // Compose events
+    var handlePostCompose = ngeoPrintUtils.createPrintMaskPostcompose(
+      /**
+       * @return {ol.Size} Size in dots of the map to print.
+       */
+      function() {
+        return $scope.layout.paperSize;
+      },
+      /**
+       * @param {olx.FrameState} frameState Frame state.
+       * @return {number} Scale of the map to print.
+       */
+      function() {
+        return $scope.scale;
+      });
 
     $scope.submit = function() {
       var map = $scope.map;
@@ -47,7 +125,7 @@
       $scope.printState = 'Printing...';
 
       var url = $window.location.toString();
-      var spec = print.createSpec(map, $scope.scale, $scope.dpi, $scope.layout.name, {
+      var spec = print.createSpec(map , $scope.scale, $scope.dpi, $scope.layout.name, {
         name: 'Ma super carte',
         qrimage: $scope.options.qrcodeUrl + encodeURIComponent(url),
         url: url,
@@ -98,6 +176,26 @@
       if (!printConfigLoaded) {
         updatePrintConfig();
         printConfigLoaded = true;
+      }
+    });
+        // Listeners
+    $scope.$on('gaLayersChange', function() {
+      refreshComp();
+    });
+    $scope.map.on('change:size', function() {
+      refreshComp();
+    });
+    $scope.$watch('scale', function() {
+      refreshComp();
+    });
+    $scope.$watch('layout', function() {
+      refreshComp();
+    });
+    $scope.$watch('options.active', function(newVal) {
+      if (newVal === true) {
+        activate();
+      } else {
+        deactivate();
       }
     });
   });
