a69e7a7444658f2a9812143e210d408be5b9b237
oterral <olivier.terral@swisstopo.ch>
2015-06-17 09:16:23 +0200

    Add preview mode for the future version of ol

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -16,19 +16,27 @@ window.GeoAdmin.lang = "${lang}";
 ${layersconfig|n}
 // Load js
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/serverconfig.js'))}" +  '"></scr' + 'ipt>');
+% if mode == 'preview' or mode == 'previewdebug':
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
+% else:
+document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/old-ga.css'))}" + '" />');
+% endif
 document.write('<scr' + 'ipt type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/proj4js/2.2.1/proj4.js"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG21781.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG2056.js'))}" + '"></scr' + 'ipt>');
 
-% if mode == 'debug':
+% if mode == 'preview':
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga.js'))}" + '"></scr' + 'ipt>');
+% elif mode == 'previewdebug':
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga-debug.js'))}" + '"></scr' + 'ipt>');
+% elif mode == 'debug':
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/old-ga-debug.js'))}" + '"></scr' + 'ipt>');
 % elif mode == 'waf':
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/old-ga-waf.js'))}" + '"></scr' + 'ipt>');
 % elif mode == 'wafint':
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/old-ga-wafint.js'))}" + '"></scr' + 'ipt>');
 % else:
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga.js'))}" + '"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/old-ga.js'))}" + '"></scr' + 'ipt>');
 % endif
 })();
 
