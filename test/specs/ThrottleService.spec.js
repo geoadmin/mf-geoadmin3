27a80988d0403fd6fa7db9c90af3f9091b998302
Gilbert Jeiziner <gjn@space.ch>
2015-07-07 13:08:08 +0200

    Improve throttle unit test

--- test/specs/ThrottleService.spec.js
+++ test/specs/ThrottleService.spec.js
@@ -1,10 +1,10 @@
 describe('ga_throttle_service', function() {
-  var gaThrottle, now, last, cpt, cptNoTrail;
+  var gaThrottle, now, last, cpt, cptNoTrail, runs;
 
   beforeEach(function() {
     inject(function($injector) {
       gaThrottle = $injector.get('gaThrottle');
-      cpt = 0, cptNoTrail = 0;
+      cpt = 0, cptNoTrail = 0, runs = 0;
       now = +new Date();
       last = now + 1000; 
     });
@@ -13,19 +13,24 @@ describe('ga_throttle_service', function() {
   it('throttles', inject(function($timeout) {
     var funcThrottled = gaThrottle.throttle(function() {
       cpt++;
-    }, 99);
+    }, 100);
     var funcThrottledNoTrail = gaThrottle.throttle(function() {
       cptNoTrail++;
-    }, 99, true);
+    }, 100, true);
     while (now < last) {
+      runs++;
       funcThrottled();
       funcThrottledNoTrail();
       now = +new Date();
     }
     $timeout.flush();
-    // We can't test value of cpt (because it depends of the machine)
-    // but we test if the noTrailing options is used
-    // correctly. 
-    expect(cpt).to.be.eql(cptNoTrail + 1);
+    expect(runs).to.be.above(0);
+    expect(cpt).to.be.above(0);
+    expect(cptNoTrail).to.be.above(0);
+    expect(runs).to.be.greaterThan(cpt);
+    expect(runs).to.be.greaterThan(cptNoTrail);
+    expect(cpt).to.be.lessThan(12);
+    expect(cptNoTrail).to.be.lessThan(12);
+    expect(cpt >= cptNoTrail).to.be(true);
   }));
 });
