d7a12478b2937d0d220b2b67785ef9973442e128
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-07-10 09:59:55 +0200

    Import NgeoPrint from updated version from ngeo

--- src/components/ngeoPrint/NgeoPrint.js
+++ src/components/ngeoPrint/NgeoPrint.js
@@ -54,14 +54,43 @@ goog.require('ngeo');
 ngeo.CreatePrint;
 
 
+/**
+ * @enum {string}
+ */
+ngeo.PrintStyleType = {
+  LINE_STRING: 'LineString',
+  POINT: 'Point',
+  POLYGON: 'Polygon'
+};
+
+
+/**
+ * @type {Object.<ol.geom.GeometryType, ngeo.PrintStyleType>}
+ * @private
+ */
+ngeo.PrintStyleTypes_ = {};
+
+ngeo.PrintStyleTypes_[ol.geom.GeometryType.LINE_STRING] =
+    ngeo.PrintStyleType.LINE_STRING;
+ngeo.PrintStyleTypes_[ol.geom.GeometryType.POINT] =
+    ngeo.PrintStyleType.POINT;
+ngeo.PrintStyleTypes_[ol.geom.GeometryType.POLYGON] =
+    ngeo.PrintStyleType.POLYGON;
+ngeo.PrintStyleTypes_[ol.geom.GeometryType.MULTI_LINE_STRING] =
+    ngeo.PrintStyleType.LINE_STRING;
+ngeo.PrintStyleTypes_[ol.geom.GeometryType.MULTI_POINT] =
+    ngeo.PrintStyleType.POINT;
+ngeo.PrintStyleTypes_[ol.geom.GeometryType.MULTI_POLYGON] =
+    ngeo.PrintStyleType.POLYGON;
+
+
 
 /**
  * @constructor
  * @param {string} url URL to MapFish print web service.
  * @param {angular.$http} $http Angular $http service.
- * @param {angular.$window} $window Angural $window service
  */
-ngeo.Print = function(url, $http, $window) {
+ngeo.Print = function(url, $http) {
   /**
    * @type {string}
    * @private
@@ -73,12 +102,6 @@ ngeo.Print = function(url, $http, $window) {
    * @private
    */
   this.$http_ = $http;
-
-  /**
-   * @type {angular.$window}
-   * @private
-   */
-  this.$window_ = $window;
 };
 
 
@@ -87,6 +110,21 @@ ngeo.Print = function(url, $http, $window) {
  * @private
  */
 ngeo.Print.FEAT_STYLE_PROP_PREFIX_ = '_ngeo_style_';
+
+
+/**
+ * Cancel a report.
+ * @param {string} ref Print report reference.
+ * @param {angular.$http.Config=} opt_httpConfig $http config object.
+ * @return {angular.$http.HttpPromise} HTTP promise.
+ */
+ngeo.Print.prototype.cancel = function(ref, opt_httpConfig) {
+  var httpConfig = goog.isDef(opt_httpConfig) ? opt_httpConfig :
+      /** @type {angular.$http.Config} */ ({});
+  var url = this.url_ + '/cancel/' + ref;
+  // "delete" is a reserved word, so use ['delete']
+  return this.$http_['delete'](url, httpConfig);
+};
 
 
 /**
@@ -155,7 +193,7 @@ ngeo.Print.prototype.encodeMap_ = function(map, scale, object) {
        */
       function(layer, idx, layers) {
         goog.asserts.assert(goog.isDef(viewResolution));
-        this.encodeLayer_(object.layers, layer, viewResolution);
+        this.encodeLayer(object.layers, layer, viewResolution);
       }, this);
 };
 
@@ -164,9 +202,8 @@ ngeo.Print.prototype.encodeMap_ = function(map, scale, object) {
  * @param {Array.<MapFishPrintLayer>} arr Array.
  * @param {ol.layer.Base} layer Layer.
  * @param {number} resolution Resolution.
- * @private
  */
-ngeo.Print.prototype.encodeLayer_ = function(arr, layer, resolution) {
+ngeo.Print.prototype.encodeLayer = function(arr, layer, resolution) {
   if (layer instanceof ol.layer.Image) {
     this.encodeImageLayer_(arr, layer);
   } else if (layer instanceof ol.layer.Tile) {
@@ -197,10 +234,25 @@ ngeo.Print.prototype.encodeImageLayer_ = function(arr, layer) {
  * @private
  */
 ngeo.Print.prototype.encodeImageWmsLayer_ = function(arr, layer) {
-  var url = this.getUrlImageWms_(layer);
   var source = layer.getSource();
-  var opacity = layer.getOpacity();
+
+  goog.asserts.assertInstanceof(layer, ol.layer.Image);
+  goog.asserts.assertInstanceof(source, ol.source.ImageWMS);
+
+  var url = source.getUrl();
   var params = source.getParams();
+  this.encodeWmsLayer_(arr, layer.getOpacity(), url, params);
+};
+
+
+/**
+ * @param {Array.<MapFishPrintLayer>} arr Array.
+ * @param {number} opacity Opacity of the layer.
+ * @param {string|undefined} url Url of the WMS server.
+ * @param {Object} params Url parameters
+ * @private
+ */
+ngeo.Print.prototype.encodeWmsLayer_ = function(arr, opacity, url, params) {
   var customParams = {'TRANSPARENT': true};
   goog.object.extend(customParams, params);
 
@@ -219,29 +271,6 @@ ngeo.Print.prototype.encodeImageWmsLayer_ = function(arr, layer) {
 };
 
 
-
-/**
- * @param {ol.source.Tile|ol.source.Image} layer
- * @return {string} url
- * @private
- */
-ngeo.Print.prototype.getUrlImageWms_ = function(layer) {
-  var source = layer.getSource();
-  var url;
-  if (source instanceof ol.source.ImageWMS) {
-    goog.asserts.assertInstanceof(layer, ol.layer.Image);
-    goog.asserts.assertInstanceof(source, ol.source.ImageWMS);
-    url = source.getUrl();
-  } else if (source instanceof ol.source.TileWMS) {
-    goog.asserts.assertInstanceof(layer, ol.layer.Tile);
-    goog.asserts.assertInstanceof(source, ol.source.TileWMS);
-    var url = this.$window_.location.protocol + source.getUrls()[0];
-  }
-
-  return url;
-};
-
-
 /**
  * @param {Array.<MapFishPrintLayer>} arr Array.
  * @param {ol.layer.Tile} layer Layer.
@@ -253,7 +282,7 @@ ngeo.Print.prototype.encodeTileLayer_ = function(arr, layer) {
   if (source instanceof ol.source.WMTS) {
     this.encodeTileWmtsLayer_(arr, layer);
   } else if (source instanceof ol.source.TileWMS) {
-    this.encodeImageWmsLayer_(arr, layer);
+    this.encodeTileWmsLayer_(arr, layer);
   }
 };
 
@@ -314,6 +343,23 @@ ngeo.Print.prototype.encodeTileWmtsLayer_ = function(arr, layer) {
 
 /**
  * @param {Array.<MapFishPrintLayer>} arr Array.
+ * @param {ol.layer.Tile} layer Layer.
+ * @private
+ */
+ngeo.Print.prototype.encodeTileWmsLayer_ = function(arr, layer) {
+  var source = layer.getSource();
+
+  goog.asserts.assertInstanceof(layer, ol.layer.Tile);
+  goog.asserts.assertInstanceof(source, ol.source.TileWMS);
+
+  var url = source.getUrls()[0];
+  var params = source.getParams();
+  this.encodeWmsLayer_(arr, layer.getOpacity(), url, params);
+};
+
+
+/**
+ * @param {Array.<MapFishPrintLayer>} arr Array.
  * @param {ol.layer.Vector} layer Layer.
  * @param {number} resolution Resolution.
  * @private
@@ -333,9 +379,18 @@ ngeo.Print.prototype.encodeVectorLayer_ = function(arr, layer, resolution) {
 
   for (var i = 0, ii = features.length; i < ii; ++i) {
     var feature = features[i];
+    var geometry = feature.getGeometry();
+
+    // no need to encode features with no geometry
+    if (!goog.isDefAndNotNull(geometry)) {
+      continue;
+    }
+
+    var geometryType = geometry.getType();
     var geojsonFeature = geojsonFormat.writeFeatureObject(feature);
 
     // Remove style property to avoid problem when converting to JSON
+    // This should be fixed in OpenLayers 3.7, see: https://github.com/openlayers/ol3/issues/3832
     if (geojsonFeature.hasOwnProperty('properties') &&
             geojsonFeature.properties &&
             geojsonFeature.properties.hasOwnProperty('Style')) {
@@ -362,36 +417,47 @@ ngeo.Print.prototype.encodeVectorLayer_ = function(arr, layer, resolution) {
         var styleId = goog.getUid(style).toString();
         var featureStyleProp = ngeo.Print.FEAT_STYLE_PROP_PREFIX_ + j;
         this.encodeVectorStyle_(
-            mapfishStyleObject, style, styleId, featureStyleProp);
+            mapfishStyleObject, geometryType, style, styleId, featureStyleProp);
         geojsonFeature.properties[featureStyleProp] = styleId;
       }
     }
   }
 
-  var geojsonFeatureCollection = /** @type {GeoJSONFeatureCollection} */ ({
-    type: 'FeatureCollection',
-    features: geojsonFeatures
-  });
+  // MapFish Print fails if there are no style rules, even if there are no
+  // features either. To work around this we just ignore the layer if the
+  // array of GeoJSON features is empty.
+  // See https://github.com/mapfish/mapfish-print/issues/279
 
-  var object = /** @type {MapFishPrintVectorLayer} */ ({
-    geoJson: geojsonFeatureCollection,
-    style: mapfishStyleObject,
-    type: 'geojson'
-  });
-
-  arr.push(object);
+  if (geojsonFeatures.length > 0) {
+    var geojsonFeatureCollection = /** @type {GeoJSONFeatureCollection} */ ({
+      type: 'FeatureCollection',
+      features: geojsonFeatures
+    });
+    var object = /** @type {MapFishPrintVectorLayer} */ ({
+      geoJson: geojsonFeatureCollection,
+      style: mapfishStyleObject,
+      type: 'geojson'
+    });
+    arr.push(object);
+  }
 };
 
 
 /**
  * @param {MapFishPrintVectorStyle} object MapFish style object.
+ * @param {ol.geom.GeometryType} geometryType Type of the GeoJSON geometry
  * @param {ol.style.Style} style Style.
  * @param {string} styleId Style id.
  * @param {string} featureStyleProp Feature style property name.
  * @private
  */
 ngeo.Print.prototype.encodeVectorStyle_ =
-    function(object, style, styleId, featureStyleProp) {
+    function(object, geometryType, style, styleId, featureStyleProp) {
+  if (!(geometryType in ngeo.PrintStyleTypes_)) {
+    // unsupported geometry type
+    return;
+  }
+  var styleType = ngeo.PrintStyleTypes_[geometryType];
   var key = '[' + featureStyleProp + ' = \'' + styleId + '\']';
   if (key in object) {
     // do nothing if we already have a style object for this CQL rule
@@ -403,18 +469,23 @@ ngeo.Print.prototype.encodeVectorStyle_ =
   object[key] = styleObject;
   var fillStyle = style.getFill();
   var imageStyle = style.getImage();
-  var textStyle = style.getText();
   var strokeStyle = style.getStroke();
-  if (!goog.isNull(fillStyle)) {
-    this.encodeVectorStylePolygon_(
-        styleObject.symbolizers, fillStyle, strokeStyle);
-  } else if (!goog.isNull(strokeStyle)) {
-    this.encodeVectorStyleLine_(styleObject.symbolizers, strokeStyle);
-  } else if (!goog.isNull(imageStyle)) {
-    this.encodeVectorStylePoint_(styleObject.symbolizers, imageStyle);
+  var textStyle = style.getText();
+  if (styleType == ngeo.PrintStyleType.POLYGON) {
+    if (!goog.isNull(fillStyle)) {
+      this.encodeVectorStylePolygon_(
+          styleObject.symbolizers, fillStyle, strokeStyle);
+    }
+  } else if (styleType == ngeo.PrintStyleType.LINE_STRING) {
+    if (!goog.isNull(strokeStyle)) {
+      this.encodeVectorStyleLine_(styleObject.symbolizers, strokeStyle);
+    }
+  } else if (styleType == ngeo.PrintStyleType.POINT) {
+    if (!goog.isNull(imageStyle)) {
+      this.encodeVectorStylePoint_(styleObject.symbolizers, imageStyle);
+    }
   }
-
-  if (!goog.isNull(textStyle) && textStyle.getText()) {
+  if (!goog.isNull(textStyle)) {
     this.encodeTextStyle_(styleObject.symbolizers, textStyle);
   }
 };
@@ -537,44 +608,61 @@ ngeo.Print.prototype.encodeVectorStyleStroke_ =
  * @private
  */
 ngeo.Print.prototype.encodeTextStyle_ = function(symbolizers, textStyle) {
-  var symbolizer = {
-    type: 'text'
-  };
-  var font = textStyle.getFont().split(' ');
-  var labelAlign = textStyle.getTextAlign();
-  if (labelAlign) {
-    symbolizer.labelAlign = labelAlign;
-  }
-  var labelRotation = textStyle.getRotation();
-  if (labelRotation) {
-    symbolizer.labelRotation = labelRotation * 180 / Math.PI;
-  }
-  symbolizer.fontWeight = font[0];
-  symbolizer.fontSize = font[1];
-  symbolizer.fontFamily = font.splice(2).join(' ');
-  symbolizer.label = textStyle.getText();
-  symbolizer.XOffset = textStyle.getOffsetX();
-  symbolizer.YOffset = textStyle.getOffsetY();
+  var symbolizer = /** @type {MapFishPrintSymbolizerText} */ ({
+    type: 'Text'
+  });
+  var label = textStyle.getText();
+  if (goog.isDef(label)) {
+    symbolizer.label = label;
 
-  var strokeStyle = textStyle.getStroke();
-  var fillStyle = textStyle.getFill();
-  if (!goog.isNull(strokeStyle)) {
-    var strokeColorRgba = ol.color.asArray(strokeStyle.getColor());
-    symbolizer.haloColor = goog.color.rgbArrayToHex(strokeColorRgba);
-    symbolizer.haloOpacity = strokeColorRgba[3];
-    symbolizer.haloRadius = strokeStyle.getWidth();
-  }
-  if (!goog.isNull(fillStyle)) {
-    var fillColorRgba = ol.color.asArray(fillStyle.getColor());
-    symbolizer.fontColor = goog.color.rgbArrayToHex(fillColorRgba);
-  }
+    var labelAlign = textStyle.getTextAlign();
+    if (goog.isDef(labelAlign)) {
+      symbolizer.labelAlign = labelAlign;
+    }
 
-  symbolizers.push(symbolizer);
+    var labelRotation = textStyle.getRotation();
+    if (goog.isDef(labelRotation)) {
+      // Mapfish print expects a string, not a number to rotate text
+      symbolizer.labelRotation = (labelRotation * 180 / Math.PI).toString();
+    }
+
+    var fontStyle = textStyle.getFont();
+    if (goog.isDef(fontStyle)) {
+      var font = fontStyle.split(' ');
+      if (font.length >= 3) {
+        symbolizer.fontWeight = font[0];
+        symbolizer.fontSize = font[1];
+        symbolizer.fontFamily = font.splice(2).join(' ');
+      }
+    }
+
+    var strokeStyle = textStyle.getStroke();
+    if (!goog.isNull(strokeStyle)) {
+      var strokeColorRgba = ol.color.asArray(strokeStyle.getColor());
+      symbolizer.haloColor = goog.color.rgbArrayToHex(strokeColorRgba);
+      symbolizer.haloOpacity = strokeColorRgba[3];
+      var width = strokeStyle.getWidth();
+      if (goog.isDef(width)) {
+        symbolizer.haloRadius = width;
+      }
+    }
+
+    var fillStyle = textStyle.getFill();
+    if (!goog.isNull(fillStyle)) {
+      var fillColorRgba = ol.color.asArray(fillStyle.getColor());
+      symbolizer.fontColor = goog.color.rgbArrayToHex(fillColorRgba);
+    }
+
+    symbolizer.XOffset = textStyle.getOffsetX();
+    symbolizer.YOffset = textStyle.getOffsetY();
+
+    symbolizers.push(symbolizer);
+  }
 };
 
 
 /**
- * Return the WMTS URL to use in the print spec.
+ * Return the WMTS URL to use in the print spec.
  * @param {ol.source.WMTS} source The WMTS source.
  * @return {string} URL.
  * @private
@@ -627,7 +715,7 @@ ngeo.Print.prototype.getStatus = function(ref, opt_httpConfig) {
 
 
 /**
- * Get the URL of a report.
+ * Get the URL of a report.
  * @param {string} ref Print report reference.
  * @return {string} The report URL for this ref.
  */
@@ -637,9 +725,9 @@ ngeo.Print.prototype.getReportUrl = function(ref) {
 
 
 /**
- * Get the capabilities of the report
- * @param {angular.$http.Config=} opt_httpConfig $http config object
- * @return {Object}
+ * Get the print capabilities from MapFish Print.
+ * @param {angular.$http.Config=} opt_httpConfig $http config object.
+ * @return {angular.$http.HttpPromise} HTTP promise.
  */
 ngeo.Print.prototype.getCapabilities = function(opt_httpConfig) {
   var httpConfig = goog.isDef(opt_httpConfig) ? opt_httpConfig :
@@ -651,20 +739,18 @@ ngeo.Print.prototype.getCapabilities = function(opt_httpConfig) {
 
 /**
  * @param {angular.$http} $http Angular $http service.
- * @param {angular.$window} $window Angular $window service.
  * @return {ngeo.CreatePrint} The function to create a print service.
  * @ngInject
  */
-ngeo.createPrintServiceFactory = function($http, $window) {
+ngeo.createPrintServiceFactory = function($http) {
   return (
       /**
        * @param {string} url URL to MapFish print service.
        */
       function(url) {
-        return new ngeo.Print(url, $http, $window);
+        return new ngeo.Print(url, $http);
       });
 };
 
 
-ngeoModule
-        .factory('ngeoCreatePrint', ngeo.createPrintServiceFactory);
+ngeoModule.factory('ngeoCreatePrint', ngeo.createPrintServiceFactory);
