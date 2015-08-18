79fb1e189c77b465bcead3580c5794152fce60d0
Gilbert Jeiziner <gjn@space.ch>
2015-08-18 10:39:50 +0200

    Refactor e2e nosetests

--- chsdi/tests/mapproxy/test_wmtsgettile.py
+++ chsdi/tests/mapproxy/test_wmtsgettile.py
@@ -1,9 +1,10 @@
 #-*- coding: utf-8 -*-
 
-from chsdi.tests.mapproxy import MapProxyTestsBase
-
 import requests
 import random
+from nose.tools import with_setup
+
+from chsdi.tests.mapproxy import MapProxyTestsBase
 
 # Official URLS we support
 WMTS_URLS = [
@@ -33,7 +34,7 @@ HEADER_RESULTS = [{
     'Header': {'User-Agent': 'WMTS Unit Tester v0.0.1', 'Referer': 'http://unittest.geo.admin.ch'}
 }, {
     'Results': [403],
-    'Header': {'User-Agent': 'WMTS Unit Tester v0.0.1'}
+    'Header': {'User-Agent': 'WMTS Unit Tester v0.0.1', 'Referer': None}
 }, {
     'Results': [403],
     'Header': {'User-Agent': 'WMTS Unit Tester v0.0.1', 'Referer': 'http://foonogood.ch'}
@@ -45,10 +46,10 @@ def get_header():
     return HEADER_RESULTS[random.randint(0, len(HEADER_RESULTS) - 1)]
 
 
-class TestWmtsGetTile(MapProxyTestsBase):
+class TileChecker(MapProxyTestsBase):
 
     def __init__(self):
-        super(TestWmtsGetTile, self).setUp()
+        super(TileChecker, self).setUp()
         self.session = requests.Session()
         self.session.mount("http://", requests.adapters.HTTPAdapter(max_retries=5))
 
@@ -62,30 +63,9 @@ class TestWmtsGetTile(MapProxyTestsBase):
         self.session.headers.update(h['Header'])
         resp = self.session.get(url)
         checkcode = resp.status_code in h['Results']
+        assert checkcode, url
 
-        assert(checkcode)
-
-    def test_epsg21781(self):
-        for params in self.get_tiles():
-            yield self.check_status_code, params[0]
-
-    def test_epsg3857(self):
-        for params in self.get_tiles(epsg=3857):
-            yield self.check_status_code, params[0]
-
-    def test_epsg2056(self):
-        for params in self.get_tiles(epsg=2056):
-            yield self.check_status_code, params[0]
-
-    def test_epsg4326(self):
-        for params in self.get_tiles(epsg=4326):
-            yield self.check_status_code, params[0]
-
-    def test_epsg4258(self):
-        for params in self.get_tiles(epsg=4258):
-            yield self.check_status_code, params[0]
-
-    def get_tiles(self, epsg=21781):
+    def itiles(self, epsg=21781):
         from urlparse import urlparse, urlunparse
         import xml.etree.ElementTree as etree
 
@@ -96,7 +76,6 @@ class TestWmtsGetTile(MapProxyTestsBase):
                  4326: [(15, 2, 2)],
                  4258: [(15, 2, 2)]
                  }
-        param_list = []
 
         if epsg in tiles.keys():
             capabilities_name = "WMTSCapabilities.EPSG.%d.xml" % epsg if epsg != 21781 else "WMTSCapabilities.xml"
@@ -125,7 +104,46 @@ class TestWmtsGetTile(MapProxyTestsBase):
                                 pth2 = pth.replace('{TileCol}', str(col)).replace('{TileRow}', str(row)).replace('{TileMatrix}', str(zoom)).replace('{Time}', str(t))
                             except:
                                 print 'Cannot replace in template %s' % pth
-                            url = urlunparse((tpl_parsed.scheme, tpl_parsed.netloc, pth2, '', '', ''))
-                            param_list.append((url,))
+                            yield urlunparse((tpl_parsed.scheme, tpl_parsed.netloc, pth2, '', '', ''))
 
-        return param_list
+
+tc = None
+
+
+def setup():
+    global tc
+    tc = TileChecker()
+
+
+def teardown():
+    tc.tearDown()
+
+
+@with_setup(setup, teardown)
+def test_epsg21781():
+    for tile in tc.itiles():
+        yield tc.check_status_code, tile
+
+
+@with_setup(setup, teardown)
+def test_epsg3857():
+    for tile in tc.itiles(epsg=3857):
+        yield tc.check_status_code, tile
+
+
+@with_setup(setup, teardown)
+def test_epsg2056():
+    for tile in tc.itiles(epsg=2056):
+        yield tc.check_status_code, tile
+
+
+@with_setup(setup, teardown)
+def test_epsg4326():
+    for tile in tc.itiles(epsg=4326):
+        yield tc.check_status_code, tile
+
+
+@with_setup(setup, teardown)
+def test_epsg4258():
+    for tile in tc.itiles(epsg=4258):
+        yield tc.check_status_code, tile
