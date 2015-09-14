380708ec210147d59c2e21400ee7bbe481b33a30
alcapat <patrick.alcaraz.ch@gmail.com>
2015-09-14 15:07:44 +0200

    test on going

--- test/saucelabs/tests_ltalp.py
+++ test/saucelabs/tests_ltalp.py
@@ -1,6 +1,9 @@
 import sys
 import os
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -21,9 +24,17 @@ try:
 except KeyError:
     print "Please set the environment variable SAUCELABS_USER and SAUCELABS_KEY"
     sys.exit(2)
-print "user: " + saucelabs_user
-print "key: " + saucelabs_key
+###Debug
+#print "user: " + saucelabs_user
+#print "key: " + saucelabs_key
 
+print "On est sur tests_ltalp.py"
+
+## 1) Locally test
+## Create a new instance of the PhantomJS driver
+driver = webdriver.PhantomJS()
+
+## 2) Or use a specific browser supported by SauceLabs (remote access)
 # This is the only code you need to edit in your existing scripts. 
 # The command_executor tells the test to run on Sauce, while the desired_capabilties 
 # parameter tells us which browsers and OS to spin up.
@@ -32,52 +43,67 @@ desired_cap = {
                 'browserName': "chrome",
                     'version': "31",
 }
-# Locally test
-browser = webdriver.PhantomJS()
-
 #driver = webdriver.Remote(
 #   command_executor='http://YOUR_USERNAME:YOUR_ACCESS_KEY@ondemand.saucelabs.com:80/wd/hub',
 #      desired_capabilities=desired_cap)
 
-## okay we will start the script!
-print ("Starting SauceLabs script!");
+
+### Set webdriver config for timeout
 
 # This is your test logic. You can add multiple tests here.
-browser.implicitly_wait(1)
-browser.get(url)
+driver.implicitly_wait(1)
+driver.get(url)
 ## Set the timeout to x ms
-browser.set_page_load_timeout(3);
+driver.set_page_load_timeout(3);
+
+#### First exemple
+#
+## go to the google home page
+#driver.get("http://www.google.com")
+#
+## the page is ajaxy so the title is originally this:
+#print "Title of the google page : " + driver.title
+#
+## find the element that's name attribute is q (the google search box)
+#inputElement = driver.find_element_by_name("q")
+#
+## type in the search
+#inputElement.send_keys("cheese!")
+#
+## Make a screen shot
+#driver.get_screenshot_as_file('/home/ltalp/mf-geoadmin3/google_cheese.png')
+#
+## submit the form (although google automatically searches now without submitting)
+#inputElement.submit()
+#
+#try:
+#    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+#    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+#
+#    # You should see "cheese! - Google Search"
+#    print "Nouvelle page pour cheese : " + driver.title
+#
+#finally:
+#    print "End Test google"
 
 ## Go to the deployed site.
 ##print "Passage en allemand Ok !"
-##browser.get(url + '/?lang=de');
+##driver.get(url + '/?lang=de');
 
-print "URL : " + url
-print "Step 1 : swisssearch raron, expect QUERYSTRING_OF_RARON" + QUERYSTRING_OF_RARON
-browser.get(url + '/?swisssearch=raron&lang=de');
-## wait until topics related stuff is loaded. We know this when catalog is there
-print "Step 2 : wait end of search"
-browser.find_element_by_xpath("//a[contains(text(), 'Grundlagen und Planung')]");
-print "Step 3 : Ok ici"
-##driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Raron')]"));
-##driver.findElement(webdriver.By.xpath("//*[contains(text(), ', Flugplatz')]")).click();
-##driver.findElement(webdriver.By.xpath("//a[contains(@href, '" + QUERYSTRING_OF_RARON + "')]"));
-##//parameter should disappear when selection is done
-##driver.findElement(webdriver.By.xpath("//*[@id='toptools']//a[contains(@href,'http')]")).getAttribute("href").then(function(val) {
-##  assert.ok(val.indexOf('swisssearch') == -1);
-##});
+### End exemple
 
-## Wait until topics related stuff is loaded. We know this when catalog is there
-##browser.findElement(browser.By.xpath("//a[contains(text(), 'Grundlagen und Planung')]"));
-##if not "Google" in driver.title:
-##            raise Exception("Unable to load google page!")
-##            elem = driver.find_element_by_name("q")
-##            elem.send_keys("Sauce Labs")
-##            elem.submit()
-##            print driver.title
-##else:
-##        print "Test Ok"
+print "Test pour map.geo.admin : " + url
+driver.get(url + '/?lang=de')
+
+driver.implicitly_wait(1)
+
+text_area = driver.find_element_by_id('search-container')
+text_area.send_keys("print 'Salavaux'")
+##driver.find_element_by_xpath("//a[contains(text(), 'Grundlagen und Planung')]")[0];
+
+# Make a screenshot
+driver.get_screenshot_as_file('/home/ltalp/mf-geoadmin3/map.geo.admin_search_salavaux.png')
+
+#driver.find_element_by_xpath("//*[@type='search']")).sendKeys('Bern');
 print "Test End"
-# This is where you tell Sauce Labs to stop running tests on your behalf.  
-# It's important so that you aren't billed after your test finishes.
 driver.quit()
