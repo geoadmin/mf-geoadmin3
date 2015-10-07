548feb04aa866cd315a9246287d8f18f0fc3b838
alcapat <patrick.alcaraz.ch@gmail.com>
2015-10-07 10:35:21 +0200

    Increase wait time

--- test/saucelabs/print_test.py
+++ test/saucelabs/print_test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-DEFAULT_WAIT=6
+DEFAULT_WAIT=10
 
 def wait_printSucessTagExist(driver, timeout=DEFAULT_WAIT):
     UrlHasChanged = False
