69b31e7511a5d014437c50384ac9c91a98070d3e
alcapat <patrick.alcaraz.ch@gmail.com>
2015-10-20 07:30:39 +0200

    Typo

--- test/saucelabs/search_test.py
+++ test/saucelabs/search_test.py
@@ -9,27 +9,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # Search testing using browserstack
-print 'Search_test starts!'
 QUERYSTRING_OF_BERN = "X=200393.28&Y=596671.16";
 
 def search_test(cap, driver, url):
-  driver.get(url)
-  driver.maximize_window()
-  driver.get(url + '/?lang=de')
+    print 'Search_test starts!'
+    driver.get(url)
+    driver.maximize_window()
+    driver.get(url + '/?lang=de')
     
-#  Send "Bern" to the searchbar
-  driver.find_element_by_xpath("//*[@type='search']").send_keys("Bern")
+    #  Send "Bern" to the searchbar
+    driver.find_element_by_xpath("//*[@type='search']").send_keys("Bern")
 
-#  Click on the field "Bern (BE)"
-  driver.find_element_by_xpath("//*[contains(text(), 'Bern')]").click()
+    #  Click on the field "Bern (BE)"
+    driver.find_element_by_xpath("//*[contains(text(), 'Bern')]").click()
 
-#  Specifically search if href of links is updated
-  driver.find_element_by_xpath("//*[@id='toptools']//a[contains(@href,'" + QUERYSTRING_OF_BERN + "')]")
+    #  Specifically search if href of links is updated
+    driver.find_element_by_xpath("//*[@id='toptools']//a[contains(@href,'" + QUERYSTRING_OF_BERN + "')]")
 
-#  Was the URL in the address bar adapted?
-  for item in cap: 
-    if(not(item['browserName'] == "IE" and item['version'] == "9.0")):
-      cUrl = driver.current_url
-      assert (cUrl.index(QUERYSTRING_OF_BERN) > -1)
+    #  Was the URL in the address bar adapted?
+    for item in cap: 
+        if(not(item['browserName'] == "IE" and item['version'] == "9.0")):
+            cUrl = driver.current_url
+        assert (cUrl.index(QUERYSTRING_OF_BERN) > -1)
 
-print 'Search_test completed'
+    print 'Search_test completed'
