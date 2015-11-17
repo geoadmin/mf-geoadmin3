4f3d3cf1e3d343d3272b577088191a67958042b4
alcapat <patrick.alcaraz.ch@gmail.com>
2015-11-17 15:28:22 +0100

    Add wms tests + active all test for chrome 45

--- test/saucelabs/test.py
+++ test/saucelabs/test.py
@@ -7,12 +7,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from start_test import runStart_test
-from kml_test import runKml_test
-from search_test import runSearch_test
+from start_test import runStartTest
+from kml_test import runKmlTest
+from search_test import runSearchTest
 from swisssearch_test import runSwissSearchTest
 from print_test import runPrintTest
-from mobile_test import runMobile_test
+from mobile_test import runMobileTest
+from wms_test import runWmsTest
 
 if __name__ == '__main__':
     
@@ -40,13 +41,13 @@ if __name__ == '__main__':
     desired_cap_list = [
 #        {'platform': "Windows 7", 'browserName': "chrome", 'version': "43.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 7", 'browserName': "chrome", 'version': "44.0", 'screenResolution': "1280x1024" },
-#        {'platform': "Windows 7", 'browserName': "chrome", 'version': "45.0", 'screenResolution': "1280x1024" },
+        {'platform': "Windows 7", 'browserName': "chrome", 'version': "45.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 8.1", 'browserName': "chrome", 'version': "45.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 7", 'browserName': "firefox", 'version': "38.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 7", 'browserName': "firefox", 'version': "39.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 7", 'browserName': "firefox", 'version': "40.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 8.1", 'browserName': "firefox", 'version': "40.0", 'screenResolution': "1280x1024"}
-        {'platform': "Windows 7", 'browserName': "internet explorer", 'version': "9.0", 'screenResolution': "1280x1024" },
+#        {'platform': "Windows 7", 'browserName': "internet explorer", 'version': "9.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 7", 'browserName': "internet explorer", 'version': "10.0", 'screenResolution': "1280x1024" },
 #        {'platform': "Windows 7", 'browserName': "internet explorer", 'version': "11.0", 'screenResolution': "1280x1024" },
         ]
@@ -61,31 +62,31 @@ if __name__ == '__main__':
         print "+--> Start test with " + current_desired_cap['platform'] + " " + current_desired_cap['browserName'] + " (" + current_desired_cap['version'] + ")"
 
         bDoStartTest = 1
-        bDoMobileTest = 0
+        bDoMobileTest = 1
         bDoSearchTest = 1
-        bDoKmlTest = 0
-        bDoSwissSearchTest = 0
-        bDoPrintTest = 0
-        bDoWmsTest = 0
+        bDoKmlTest = 1
+        bDoSwissSearchTest = 1
+        bDoPrintTest = 1
+        bDoWmsTest = 1
 
         driver = webdriver.Remote(
             command_executor='http://' + saucelabs_user + ':' + saucelabs_key + '@ondemand.saucelabs.com:80/wd/hub', desired_capabilities=current_desired_cap)
         try: 
             if bDoStartTest:
                 tpartial = time.time()
-                runStart_test(driver, url)                         ## Ok with Chrome and FF
+                runStartTest(driver, url)                         ## Ok with Chrome and FF
                 print("Temp partiel Start test : %.2f secondes" % (time.time() - tpartial))
             if bDoMobileTest:
                 tpartial = time.time()
-                runMobile_test(current_desired_cap, driver, url)   ## Ok with Chrome and FF
+                runMobileTest(current_desired_cap, driver, url)   ## Ok with Chrome and FF
                 print("Temp partiel Mobile : %.2f secondes" % (time.time() - tpartial))
             if bDoSearchTest:
                 tpartial = time.time()
-                runSearch_test(desired_cap_list, driver, url)      ## Ok with Chrome and FF
+                runSearchTest(desired_cap_list, driver, url)      ## Ok with Chrome and FF
                 print("Temp partiel Search : %.2f secondes" % (time.time() - tpartial))
             if bDoKmlTest:
                 tpartial = time.time()
-                runKml_test(desired_cap_list, driver, url)         ## Ok with Chrome and FF  
+                runKmlTest(desired_cap_list, driver, url)         ## Ok with Chrome and FF  
                 print("Temp partiel Kml : %.2f secondes" % (time.time() - tpartial))
             if bDoSwissSearchTest:
                 tpartial = time.time()
@@ -95,6 +96,11 @@ if __name__ == '__main__':
                 tpartial = time.time()
                 runPrintTest(driver, url)                          ## Ok with Chrome and FF
                 print("Temp partiel Print : %.2f secondes" % (time.time() - tpartial))
+            if bDoWmsTest:
+                tpartial = time.time()
+                runWmsTest(driver, url)                            ## Ok with Chrome and FF
+                print("Temp partiel Wms : %.2f secondes" % (time.time() - tpartial))
+
         finally:
             driver.quit()
         print "--- end test for this browser"
