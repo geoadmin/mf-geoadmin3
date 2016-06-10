c6a33ffb86cbbad3128fdcb8753811f706c7de31
loicgasser <loicgasser4@gmail.com>
2016-06-10 17:55:28 +0200

    Hide timestamp wh real-time layer invisible

--- src/components/timestampcontrol/TimestampControlDirective.js
+++ src/components/timestampcontrol/TimestampControlDirective.js
@@ -1,9 +1,12 @@
 goog.provide('ga_timestamp_control_directive');
+
+goog.require('ga_map_service');
 (function() {
 
-  var module = angular.module('ga_timestamp_control_directive', []);
+  var module = angular.module('ga_timestamp_control_directive',
+      ['ga_map_service']);
 
-  module.directive('gaTimestampControl', function($rootScope) {
+  module.directive('gaTimestampControl', function($rootScope, gaLayerFilters) {
     return {
       restrict: 'A',
       scope: {
@@ -11,9 +14,25 @@ goog.provide('ga_timestamp_control_directive');
       },
       templateUrl: 'components/timestampcontrol/partials/timestampcontrol.html',
       link: function(scope) {
-        scope.timestamp = '';
+        var time = '';
+        scope.timestamp = time;
         $rootScope.$on('gaNewLayerTimestamp', function(e, timestamp) {
           scope.timestamp = timestamp;
+          time = timestamp;
+        });
+        scope.layers = scope.map.getLayers().getArray();
+        scope.selectedAndVisible = gaLayerFilters.selectedAndVisible;
+        scope.realtime = gaLayerFilters.realtime;
+        scope.$watchCollection(
+            'layers | filter:selectedAndVisible | filter:realtime',
+            function(layers) {
+              if (layers.length > 0) {
+                // Only one timestamp is displayed at a time.
+                scope.timestamp = time;
+              } else {
+                time = scope.timestamp;
+                scope.timestamp = '';
+              }
         });
       }
     };
