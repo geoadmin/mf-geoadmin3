c615cf6e09ead27a205b4ee6d069fbd2b2cf4c22
loicgasser <loicgasser4@gmail.com>
2016-07-07 18:31:00 +0200

    Add tests for labels

--- test/specs/StylesFromLiterals.spec.js
+++ test/specs/StylesFromLiterals.spec.js
@@ -78,6 +78,188 @@ describe('ga_stylesfromliterals_service', function() {
     expect(olStyle.getFill().getColor()).to.equal('#FF2FF2');
   });
 
+  it('supports label for single type style assignment', function() {
+    var singleTypeStyle = {
+      type: 'single',
+      geomType: 'point',
+      vectorOptions: {
+        type: 'circle',
+        radius: 8,
+        fill: {
+          color: '#FF1FF1'
+        },
+        stroke: {
+          color: '#FFFFFF',
+          width: 3
+        },
+        label: {
+          property: 'foo',
+          text: {
+            textAlign: 'center',
+            textBaseline: 'middle',
+            font: 'bold 10px Helvetica',
+            scale: 1.5,
+            offsetX: 0,
+            offsetY: 0,
+            stroke: {
+              color: 'rgba(255, 255, 255, 0.7)',
+              width: 4
+            },
+            fill: {
+              color: 'rgba(52, 52, 52, 0.95)',
+            }
+          }
+        }
+      }
+    };
+    var geoJsonFormat = new ol.format.GeoJSON();
+    var olFeature = geoJsonFormat.readFeature(
+      '{"type": "Feature",' +
+        '"geometry": {' +
+          '"coordinates": [' +
+            '10000,' +
+            '20000' +
+          '],' +
+          '"type": "Point"' +
+        '},' +
+        '"properties": {' +
+          '"foo": "bar"' +
+        '}}'
+    );
+    var gaStyle = gaStylesFromLiterals(singleTypeStyle);
+    var olStyle = gaStyle.getFeatureStyle(olFeature);
+    var olImage = olStyle.getImage();
+    var olText = olStyle.getText();
+    expect(olStyle).to.be.an(ol.style.Style);
+    expect(olImage).to.be.an(ol.style.Image);
+    expect(olImage).to.be.an(ol.style.Circle);
+    expect(olText).to.be.an(ol.style.Text);
+    expect(olImage.getStroke()).to.be.an(ol.style.Stroke);
+    expect(olImage.getStroke().getColor()).to.equal('#FFFFFF');
+    expect(olImage.getStroke().getWidth()).to.equal(3);
+    expect(olImage.getFill()).to.be.an(ol.style.Fill);
+    expect(olImage.getFill().getColor()).to.equal('#FF1FF1');
+    expect(olText.getText()).to.equal('bar');
+    expect(olText.getFill()).to.be.an(ol.style.Fill);
+    expect(olText.getStroke()).to.an(ol.style.Stroke);
+    expect(olText.getScale()).to.equal(1.5);
+    expect(olText.getOffsetX()).to.equal(0);
+    expect(olText.getOffsetY()).to.equal(0);
+    expect(olText.getTextAlign()).to.equal('center');
+    expect(olText.getTextBaseline()).to.equal('middle');
+  });
+
+  it('supports label for unique type style assignment', function() {
+    var uniqueTypeStyle = {
+      type: 'unique',
+      property: 'foo',
+      values: [
+        {
+          geomType: 'line',
+          value: 'bar',
+          vectorOptions: {
+            stroke: {
+              color: '#FFFFFF',
+              width: 3
+            },
+            label: {
+              property: 'oraison',
+              text: {
+                textAlign: 'center',
+                textBaseline: 'middle',
+                font: 'bold 10px Helvetica',
+                scale: 1.1,
+                offsetX: 1,
+                offsetY: 2,
+                stroke: {
+                  color: 'rgba(22, 22, 22, 0.4)',
+                  width: 4
+                },
+                fill: {
+                  color: 'rgba(52, 52, 52, 0.3)',
+                }
+              }
+            }
+          }
+        }, {
+          geomType: 'point',
+          value: 'toto',
+          vectorOptions: {
+            type: 'circle',
+            radius: 2,
+            fill: {
+              color: '#FF2222'
+            },
+            stroke: {
+              color: '#F55555',
+              width: 3
+            }
+          }
+        }
+      ]
+    };
+    var geoJsonFormat = new ol.format.GeoJSON();
+    var olFeature = geoJsonFormat.readFeature(
+      '{"type": "Feature",' +
+        '"geometry": {' +
+          '"coordinates": [' +
+            '10000,' +
+            '20000' +
+          '],' +
+          '"type": "Point"' +
+        '},' +
+        '"properties": {' +
+          '"foo": "toto",' +
+          '"oraison": "mylord"' +
+        '}}'
+    );
+    var gaStyle = gaStylesFromLiterals(uniqueTypeStyle);
+    var olStyle = gaStyle.getFeatureStyle(olFeature);
+    var olImage = olStyle.getImage();
+    var olText = olStyle.getText();
+    expect(olStyle).to.be.an(ol.style.Style);
+    expect(olImage).to.be.an(ol.style.Image);
+    expect(olImage).to.be.an(ol.style.Circle);
+    expect(olText).to.equal(null);
+    expect(olImage.getStroke()).to.be.an(ol.style.Stroke);
+    expect(olImage.getFill()).to.be.an(ol.style.Fill);
+    expect(olImage.getStroke().getColor()).to.equal('#F55555');
+    expect(olImage.getFill().getColor()).to.equal('#FF2222');
+
+    olFeature = geoJsonFormat.readFeature(
+      '{"type": "Feature",' +
+        '"geometry": {' +
+          '"coordinates": [' +
+            '[10000,' +
+             '20000],' +
+            '[12000,' +
+             '21000]' +
+          '],' +
+          '"type": "LineString"' +
+        '},' +
+        '"properties": {' +
+          '"foo": "bar",' +
+          '"oraison": "mylady"' +
+        '}}'
+    );
+    olStyle = gaStyle.getFeatureStyle(olFeature);
+    olImage = olStyle.getImage();
+    olText = olStyle.getText();
+    expect(olStyle).to.be.an(ol.style.Style);
+    expect(olImage).not.to.be.an(ol.style.Image);
+    expect(olText).to.be.an(ol.style.Text);
+    expect(olStyle.getStroke()).to.be.an(ol.style.Stroke);
+    expect(olStyle.getStroke().getColor()).to.equal('#FFFFFF');
+    expect(olStyle.getStroke().getWidth()).to.equal(3);
+    expect(olText.getText()).to.equal('mylady');
+    expect(olText.getStroke()).to.an(ol.style.Stroke);
+    expect(olText.getScale()).to.equal(1.1);
+    expect(olText.getOffsetX()).to.equal(1);
+    expect(olText.getOffsetY()).to.equal(2);
+    expect(olText.getTextAlign()).to.equal('center');
+    expect(olText.getTextBaseline()).to.equal('middle');
+  });
+
   it('supports simple unique type style assignment', function() {
     var uniqueTypeStyle = {
       type: 'unique',
