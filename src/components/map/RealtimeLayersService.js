5f3f61e9f4c6257dd5a0efc583854e63ef491ef8
oterral <olivier.terral@swisstopo.ch>
2016-07-28 10:41:32 +0200

    Add tests for gaRealtimeLayersManager service

--- src/components/map/RealtimeLayersService.js
+++ src/components/map/RealtimeLayersService.js
@@ -9,8 +9,8 @@ goog.require('ga_map_service');
   ]);
 
   module.provider('gaRealtimeLayersManager', function() {
-    this.$get = function($rootScope, $http, $timeout,
-        gaLayerFilters, gaMapUtils, gaGlobalOptions) {
+    this.$get = function($rootScope, $http, $timeout, gaLayerFilters,
+        gaMapUtils, gaGlobalOptions) {
 
       var timers = [];
       var realTimeLayersId = [];
@@ -19,23 +19,22 @@ goog.require('ga_map_service');
       function setLayerSource(layer) {
         var fullUrl = gaGlobalOptions.ogcproxyUrl + layer.geojsonUrl;
         var olSource = layer.getSource();
-        $http.get(fullUrl, {
-          cache: false
-        }).success(function(data) {
+        $http.get(fullUrl).then(function(response) {
+          var data = response.data;
           olSource.clear();
           olSource.addFeatures(
             geojsonFormat.readFeatures(data)
           );
-          var layerIdIndex = realTimeLayersId.indexOf(layer.bodId);
-          if (layerIdIndex != -1 && !layer.preview) {
-            $timeout.cancel(timers.splice(layerIdIndex, 1));
-            timers.push(setLayerUpdateInterval(layer));
+          if (!layer.preview) {
+            var layerIdIndex = realTimeLayersId.indexOf(layer.bodId);
+            timers[layerIdIndex] = setLayerUpdateInterval(layer);
             if (data.timestamp) {
               $rootScope.$broadcast('gaNewLayerTimestamp', data.timestamp);
             }
           }
         });
       }
+
       // updateDeplay should be higher than the time
       // needed to upload the geojson file
       function setLayerUpdateInterval(layer) {
@@ -51,42 +50,41 @@ goog.require('ga_map_service');
 
         scope.$watchCollection('layers | filter:layerFilter',
             function(newLayers, oldLayers) {
+
           // Layer Removed
-          for (var i = 0; i < oldLayers.length; i++) {
-            var bodId = oldLayers[i].bodId;
+          oldLayers.forEach(function(oldLayer) {
+            var bodId = oldLayer.bodId;
             var oldLayerIdIndex = realTimeLayersId.indexOf(bodId);
             if (oldLayerIdIndex != -1 &&
                 !gaMapUtils.getMapLayerForBodId(map, bodId)) {
               realTimeLayersId.splice(oldLayerIdIndex, 1);
-              $timeout.cancel(timers.splice(oldLayerIdIndex, 1));
+              $timeout.cancel(timers.splice(oldLayerIdIndex, 1)[0]);
               if (realTimeLayersId.length == 0) {
                 $rootScope.$broadcast('gaNewLayerTimestamp', '');
               }
             }
-          }
+          });
+
           // Layer Added
-          for (var i = 0; i < newLayers.length; i++) {
-            var bodId = newLayers[i].bodId;
-            var newLayerIdIndex = realTimeLayersId.indexOf(bodId);
-            if (newLayerIdIndex == -1) {
-              if (!newLayers[i].preview) {
+          newLayers.forEach(function(newLayer) {
+            var bodId = newLayer.bodId;
+            if (realTimeLayersId.indexOf(bodId) == -1) {
+              if (!newLayer.preview) {
                 realTimeLayersId.push(bodId);
               }
-              setLayerSource(newLayers[i]);
+              setLayerSource(newLayer);
             }
-          }
+          });
         });
 
         // Update geojson source on language change
         $rootScope.$on('$translateChangeEnd', function(evt) {
-          for (var i = 0; i < realTimeLayersId.length; i++) {
-            var bodId = realTimeLayersId[i];
+
+          realTimeLayersId.forEach(function(bodId, i) {
             var olLayer = gaMapUtils.getMapLayerForBodId(map, bodId);
-            var olSource = olLayer.getSource();
-            var indexLayerId = realTimeLayersId.indexOf(bodId);
-            $timeout.cancel(timers.splice(indexLayerId, 1));
+            $timeout.cancel(timers[i]);
             setLayerSource(olLayer);
-          }
+          });
         });
       };
     };
