b40dd59508590e7cc82dcfb3fd72751f777635ef
oterral <olivier.terral@swisstopo.ch>
2016-09-30 09:28:43 +0200

    Do nothing if the layer does not exist

--- src/components/draw/DrawDirective.js
+++ src/components/draw/DrawDirective.js
@@ -700,7 +700,11 @@ goog.require('ga_styles_service');
         // create/update the file on s3
         ////////////////////////////////////
         var save = function(evt) {
-          if (layer && layer.getSource().getFeatures().length == 0) {
+          if (!layer {
+            // Do nothing if the layer does not exist
+            return;
+          }
+          if (layer.getSource().getFeatures().length == 0) {
             //if no features to save, delete the file
             if (layer.adminId) {
               scope.statusMsgId = '';
