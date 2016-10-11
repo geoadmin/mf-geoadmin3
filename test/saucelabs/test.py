1f206522f9dea000ad45f93c331ac6dc14dffeb8
oterral <olivier.terral@swisstopo.ch>
2016-10-11 14:07:47 +0200

    Remove tests of chsdi services from saucelabs

--- test/saucelabs/test.py
+++ test/saucelabs/test.py
@@ -11,7 +11,6 @@ from search_test import runSearchTest
 from print_test import runPrintTest
 from mobile_test import runMobileTest
 from wms_test import runWmsTest
-from checker_test import runCheckerTest
 from tooltip_test import runTooltipTest
 
 DEFAULT_WAIT_FOUND = 5
@@ -93,12 +92,12 @@ if __name__ == '__main__':
     ]
 
     config_test_list = {
-        "firefox": ['start', 'mobile', 'search', 'checker', 'wms', 'tooltip'],
-        "chrome": ['start', 'mobile', 'search', 'checker', 'wms', 'tooltip'],
+        "firefox": ['start', 'mobile', 'search', 'wms', 'tooltip'],
+        "chrome": ['start', 'mobile', 'search', 'wms', 'tooltip'],
         "internet explorer": ['start'],
         "opera": ['start'],
         "safari": ['start'],
-        "MicrosoftEdge": ['start', 'search', 'checker']
+        "MicrosoftEdge": ['start', 'search']
     }
 
     # okay we will start the script!
@@ -114,7 +113,6 @@ if __name__ == '__main__':
         'search': runSearchTest,
         'kml': runKmlTest,
         'print': runPrintTest,
-        'checker': runCheckerTest,
         'tooltip': runTooltipTest
     }
 
