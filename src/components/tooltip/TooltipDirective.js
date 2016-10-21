fc596bf6a123a670a8663fa8bca55336bc57d3c3
oterral <olivier.terral@swisstopo.ch>
2016-10-21 14:28:01 +0200

    Fix #3471: Display 'Show profile' link on mobile

--- src/components/tooltip/TooltipDirective.js
+++ src/components/tooltip/TooltipDirective.js
@@ -699,8 +699,7 @@ goog.require('ga_topic_service');
                 showVectorInfos: (value instanceof ol.Feature),
                 clickGeometry: new ol.geom.Point(scope.clickCoordinate),
                 snippet: $sce.trustAsHtml(html),
-                showProfile: !gaBrowserSniffer.mobile &&
-                    value instanceof ol.Feature && value.getGeometry()
+                showProfile: value instanceof ol.Feature && value.getGeometry()
               });
             };
           }
