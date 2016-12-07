1a078cb14facec1fec003a553020efca094f50ed
oterral <olivier.terral@swisstopo.ch>
2016-12-07 09:20:58 +0100

    Change timestamp

--- src/components/map/MapService.js
+++ src/components/map/MapService.js
@@ -568,7 +568,7 @@ goog.require('ga_urlutils_service');
                 'ch.swisstopo.swissnames3d.3d'
               ];
               var tilesetTs = [
-                '20160909',
+                '20161121',
                 '20160909'
               ];
 
