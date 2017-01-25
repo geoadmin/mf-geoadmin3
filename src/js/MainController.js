12c16072c67524bdee1cf411be629cef7e2c3651
oterral <olivier.terral@swisstopo.ch>
2017-01-25 09:41:44 +0100

    Fix init loading of tilesets

--- src/js/MainController.js
+++ src/js/MainController.js
@@ -114,7 +114,7 @@ goog.require('ga_topic_service');
 
       var cesium = new GaCesium($scope.map, gaPermalink, gaLayers,
                                 gaGlobalOptions, gaBrowserSniffer, $q,
-                                $translate, $rootScope);
+                                $translate, $rootScope, gaBackground);
       cesium.loaded().then(function(ol3d) {
         $scope.ol3d = ol3d;
         if (!$scope.ol3d) {
