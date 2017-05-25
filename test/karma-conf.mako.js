1292a4b71063c3a47fe056110139bad4cc93e3a0
Julien Enselme <julien.enselme@ioda-net.ch>
2017-05-25 16:45:17 +0200

    Display output of console.log in stdout

--- test/karma-conf.mako.js
+++ test/karma-conf.mako.js
@@ -145,7 +145,12 @@ module.exports = function(config) {
 
   // Continuous Integration mode
   // if true, it capture browsers, run tests and exit
-  singleRun: false
+  singleRun: false,
+  browserConsoleLogOptions: {
+    level: 'log',
+    format: '%b %T: %m',
+    terminal: true
+  }
 
   });
 };
