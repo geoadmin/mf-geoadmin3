5d9b4b26175f1f340940648ab1bd79e95e7a8cd7
Marc Monnerat <procrastinatio@gmail.com>
2017-07-19 14:08:41 +0200

    new print directive + tests

--- src/components/print/PrintModule.js
+++ src/components/print/PrintModule.js
@@ -2,11 +2,12 @@ goog.provide('ga_print');
 
 goog.require('ga_print_directive');
 goog.require('ga_printstyle_service');
+
 (function() {
 
   angular.module('ga_print', [
     'ga_print_directive',
-    'ga_printstyle_service'
+    'ga_printlayer_service',
   ]);
 })();
 
