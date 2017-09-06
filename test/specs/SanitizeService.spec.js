fdbc1ded9533fc0e495bf8602a671237acdf80e4
Gilbert Jeiziner <gjn@space.ch>
2017-09-06 13:01:53 +0200

    A littler more strict sanitation of kml content

--- test/specs/SanitizeService.spec.js
+++ test/specs/SanitizeService.spec.js
@@ -2,8 +2,10 @@ describe('ga_sanitize_service', function() {
 
   describe('gaSanitize', function() {
     var sanitize;
-    var html = '<div><p></p></div>';
-    var htmlEncoded = '&lt;div&gt;&lt;p&gt;&lt;/p&gt;&lt;/div&gt;';
+    var html = '<div ><p></p></div>';
+    var html2 = '<div ><p ></p></div>';
+    var html3 = '<div> <p></p></div>';
+    var htmlEncoded = '&lt;div&gt; &lt;p&gt;&lt;/p&gt;&lt;/div&gt;';
     var htmlWithEvts = '<div onmousedown="alert(document.cookie)"><p ' +
         'onpointerdown="alert(document.cookie)"></p></div>';
     var htmlWithEvts2 = '<div onmousedown=\'alert(document.cookie)\'><p ' +
@@ -26,12 +28,12 @@ describe('ga_sanitize_service', function() {
       });
 
       it('removes all onXXX attributes from html tags', function() {
-        expect(gaSanitize.html(htmlWithEvts)).to.be(html);
-        expect(gaSanitize.html(htmlWithEvts2)).to.be(html);
+        expect(gaSanitize.html(htmlWithEvts)).to.be(html2);
+        expect(gaSanitize.html(htmlWithEvts2)).to.be(html2);
       });
 
       it('removes script tags', function() {
-        expect(gaSanitize.html(htmlWithScriptTags)).to.be(html);
+        expect(gaSanitize.html(htmlWithScriptTags)).to.be(html3);
         expect(gaSanitize.html(htmlEncodedWithScriptTags)).to.be(htmlEncoded);
       });
     });
