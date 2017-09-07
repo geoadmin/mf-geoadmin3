0ae3e06a3390116c4c9424f7965036faff48a8f9
Gilbert Jeiziner <gilbert.jeiziner@gjn.ch>
2017-09-07 14:45:05 +0200

    A littler more strict sanitation of kml content (#3942)

--- src/components/SanitizeService.js
+++ src/components/SanitizeService.js
@@ -8,14 +8,15 @@ goog.provide('ga_sanitize_service');
 
       // https://regex101.com/r/81tipL/1/
       var attrRegex = /(\s+on\w*=("[^"]+"|'[^']+'))/img;
-      // https://regex101.com/r/U5ccHi/2
-      var tagRegex = /(<|&lt;)script[^(>|&gt;)]*?(>|&gt;)(.|\s)*?(<|&lt;)\/script(>|&gt;)/img;
+      // https://regex101.com/r/U5ccHi/4
+      var tagRegex = /(<|&lt;)script\s*\S*[^(>|&gt;)]*?(>|&gt;)(.|\s)*?(<|&lt;)\/script(>|&gt;)/img;
 
       var Sanitize = function() {
 
-        // Remove all js events and script tags
+        // Remove all js events and script tags.
+        // We replace it with spaces
         this.html = function(str) {
-          return str.replace(attrRegex, '').replace(tagRegex, '');
+          return str.replace(tagRegex, ' ').replace(attrRegex, ' ');
         };
       };
       return new Sanitize();
