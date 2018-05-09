7bc709284b8158fe4e5064151ca15724343f8e08
oterral <olivier.terral@swisstopo.ch>
2018-05-09 12:57:49 +0200

    Add basic tests for TimeSelector directives

--- src/components/timeselector/TimeSelectorDirective.js
+++ src/components/timeselector/TimeSelectorDirective.js
@@ -1,6 +1,5 @@
 goog.provide('ga_timeselector_directive');
 
-goog.require('ga_browsersniffer_service');
 goog.require('ga_debounce_service');
 goog.require('ga_layerfilters_service');
 goog.require('ga_layers_service');
@@ -11,7 +10,6 @@ goog.require('ga_time_service');
 (function() {
 
   var module = angular.module('ga_timeselector_directive', [
-    'ga_browsersniffer_service',
     'ga_debounce_service',
     'ga_layerfilters_service',
     'ga_layers_service',
@@ -29,8 +27,7 @@ goog.require('ga_time_service');
       }
   );
 
-  module.directive('gaTimeSelectorBt', function($rootScope, $translate,
-      gaBrowserSniffer, gaTime) {
+  module.directive('gaTimeSelectorBt', function($rootScope, gaTime) {
     return {
       restrict: 'A',
       templateUrl: 'components/timeselector/partials/timeselector-bt.html',
@@ -83,203 +80,202 @@ goog.require('ga_time_service');
     };
   });
 
-  module.directive('gaTimeSelector',
-      function(gaDebounce, gaLayers, gaTime, gaMapUtils, $timeout) {
+  module.directive('gaTimeSelector', function(gaDebounce, gaTime,
+      $timeout) {
 
-      // Magnetize a year to the closest available year
-        var magnetize = function(currentYear, availableYears) {
-          var minGap = null;
-          for (var i = 0, length = availableYears.length; i < length;
-            i++) {
-            var elt = availableYears[i];
-            var gap = elt.value - currentYear;
-            minGap = (!minGap || (Math.abs(gap) < Math.abs(minGap))) ?
-              gap : minGap;
-          }
-          if (minGap) {
-            currentYear += minGap;
-          }
-          return currentYear;
-        };
+    // Magnetize a year to the closest available year
+    var magnetize = function(currentYear, availableYears) {
+      var minGap = null;
+      for (var i = 0, length = availableYears.length; i < length;
+        i++) {
+        var elt = availableYears[i];
+        var gap = elt.value - currentYear;
+        minGap = (!minGap || (Math.abs(gap) < Math.abs(minGap))) ?
+          gap : minGap;
+      }
+      if (minGap) {
+        currentYear += minGap;
+      }
+      return currentYear;
+    };
 
-        return {
-          restrict: 'A',
-          templateUrl: 'components/timeselector/partials/timeselector.html',
-          scope: {
-            map: '=gaTimeSelectorMap',
-            ol3d: '=gaTimeSelectorOl3d',
-            options: '=gaTimeSelectorOptions'
-          },
-          controller: 'GaTimeSelectorDirectiveController',
-          link: function(scope, elt, attrs, controller) {
-            scope.years = [];
-            var promise;
+    return {
+      restrict: 'A',
+      templateUrl: 'components/timeselector/partials/timeselector.html',
+      scope: {
+        map: '=gaTimeSelectorMap',
+        ol3d: '=gaTimeSelectorOl3d',
+        options: '=gaTimeSelectorOptions'
+      },
+      controller: 'GaTimeSelectorDirectiveController',
+      link: function(scope, elt, attrs, controller) {
+        scope.years = [];
+        var promise;
 
-            // Copy from slider
-            var nextValue = function(value, list) {
-              if (list && list.length > 0) {
-                for (var i = list.length - 1; i >= 0; i--) {
-                  var elt = list[i];
-                  if (elt.value === value) {
-                    value = magnetize(value, list.slice(i + 1, list.length));
+        // Copy from slider
+        var nextValue = function(value, list) {
+          if (list && list.length > 0) {
+            for (var i = list.length - 1; i >= 0; i--) {
+              var elt = list[i];
+              if (elt.value === value) {
+                value = magnetize(value, list.slice(i + 1, list.length));
 
-                    // if we have reached the end of the list restart from the
-                    // beginning
-                    if (value === elt.value && list.length > 1) {
-                      value = list[0].value;
-                    }
-                    break;
-                  }
+                // if we have reached the end of the list restart from the
+                // beginning
+                if (value === elt.value && list.length > 1) {
+                  value = list[0].value;
                 }
+                break;
               }
-              return value;
-            };
-
-            var applyNextYear = function() {
-              var nextYear = nextValue(scope.currentYear, scope.availableYears);
-              if (nextYear !== scope.currentYear) {
-                scope.currentYear = nextYear;
-                promise = $timeout(applyNextYear, 1000);
-              } else {
-                scope.stop();
-              }
-            };
-
-            scope.play = function() {
-              scope.isPlaying = true;
-              applyNextYear();
-            };
-
-            scope.stop = function() {
-              scope.isPlaying = false;
-              if (promise) {
-                $timeout.cancel(promise);
-                promise = undefined;
-              }
-            };
-
-            // Update the status of the directive
-            var timeEnabledLayers = [];
-            var updateStatus = function() {
-              if (!timeEnabledLayers.length) {
-                scope.isActive = false;
-                return;
-              }
-              scope.isActive = !!(gaTime.get());
-            };
-
-            // Events to force the state of the component from another directive
-            scope.$on('gaTimeSelectorToggle', function(evt, active) {
-              scope.isActive = active;
-            });
-
-            // Activate/deactivate automatically the time selector
-            scope.$on('gaTimeChange', function(evt, time) {
-              if (angular.isDefined(time) &&
-                  scope.currentYear !== parseFloat(time)) {
-                scope.currentYear = time;
-              }
-              updateStatus();
-              if (!scope.isActive) {
-                scope.stop();
-              }
-            });
-
-            // Update then magnetize the current year
-            scope.$watchCollection('layers | filter:layerFilter',
-                function(olLayers) {
-                  timeEnabledLayers = olLayers;
-                  // We update the list of dates available then
-                  // we magnetize the current year value
-                  // to the closest available year if needed
-                  if (updateDatesAvailable(olLayers)) {
-                    scope.currentYear = magnetize(scope.currentYear,
-                        scope.availableYears);
-                  }
-                  updateStatus();
-                });
-
-            // Watch if 3d is active
-            scope.$watch(function() {
-              return scope.ol3d && scope.ol3d.getEnabled();
-            }, function(active) {
-              scope.stop();
-              scope.is3dActive = active;
-              elt.toggle(scope.isActive);
-            });
-
-            // Activate/deactivate the component
-            scope.$watch('isActive', function(active, old) {
-              // On the first call old and active are false both but we don't
-              // want to apply the year
-              if (active !== old) {
-                scope.years = active ? scope.options.years : [];
-                applyNewYear((active ? scope.currentYear : undefined));
-                elt.toggle(active);
-              }
-            });
-
-            // currentYear is always an integer
-            scope.$watch('currentYear', function(year) {
-              if (scope.isActive) {
-                applyNewYearDebounced(year);
-              }
-            });
-
-            /** Utils **/
-
-            // Apply the year selected
-            var applyNewYear = function(year) {
-              var newYear = (year) ? '' + year : year;
-
-              // Only valid values are allowed: undefined, null or
-              // minYear <= newYear <= maxYear
-              if ((!newYear || (scope.options.minYear <= newYear &&
-                newYear <= scope.options.maxYear))) {
-                gaTime.set(newYear);
-              }
-            };
-            var applyNewYearDebounced = gaDebounce.debounce(applyNewYear, 200,
-                false);
-
-            // Update the list of years available
-            scope.availableYears = [];
-            var updateDatesAvailable = function(olLayers) {
-              var magnetizeCurrentYear = true;
-              scope.availableYears = [];
-              for (var i = 0, length = scope.options.years.length; i < length;
-                i++) {
-                var year = scope.options.years[i];
-                year.available = false;
-                olLayers.forEach(function(olLayer, opt) {
-                  if (year.available || !olLayer.timeEnabled) {
-                    return;
-                  }
-                  var timestamps = olLayer.timestamps || [];
-                  for (var i = 0, length = timestamps.length; i < length; i++) {
-                    var yearTimestamp = gaTime.getYearFromTimestamp(
-                        timestamps[i]);
-                    if (year.value === yearTimestamp) {
-                      year.available = true;
-                      scope.availableYears.push(year);
-                      if (year.value === scope.currentYear) {
-                        magnetizeCurrentYear = false;
-                      }
-                      break;
-                    }
-                  }
-                });
-              }
-              return magnetizeCurrentYear;
-            };
-
-            // Initialize the state of the component
-            scope.currentYear = gaTime.get() || scope.options.maxYear;
-            if (gaTime.get()) {
-              scope.isActive = true;
             }
           }
+          return value;
         };
+
+        var applyNextYear = function() {
+          var nextYear = nextValue(scope.currentYear, scope.availableYears);
+          if (nextYear !== scope.currentYear) {
+            scope.currentYear = nextYear;
+            promise = $timeout(applyNextYear, 1000);
+          } else {
+            scope.stop();
+          }
+        };
+
+        scope.play = function() {
+          scope.isPlaying = true;
+          applyNextYear();
+        };
+
+        scope.stop = function() {
+          scope.isPlaying = false;
+          if (promise) {
+            $timeout.cancel(promise);
+            promise = undefined;
+          }
+        };
+
+        // Update the status of the directive
+        var timeEnabledLayers = [];
+        var updateStatus = function() {
+          if (!timeEnabledLayers.length) {
+            scope.isActive = false;
+            return;
+          }
+          scope.isActive = !!(gaTime.get());
+        };
+
+        // Events to force the state of the component from another directive
+        scope.$on('gaTimeSelectorToggle', function(evt, active) {
+          scope.isActive = active;
+        });
+
+        // Activate/deactivate automatically the time selector
+        scope.$on('gaTimeChange', function(evt, time) {
+          if (angular.isDefined(time) &&
+              scope.currentYear !== parseFloat(time)) {
+            scope.currentYear = time;
+          }
+          updateStatus();
+          if (!scope.isActive) {
+            scope.stop();
+          }
+        });
+
+        // Update then magnetize the current year
+        scope.$watchCollection('layers | filter:layerFilter',
+            function(olLayers) {
+              timeEnabledLayers = olLayers;
+              // We update the list of dates available then
+              // we magnetize the current year value
+              // to the closest available year if needed
+              if (updateDatesAvailable(olLayers)) {
+                scope.currentYear = magnetize(scope.currentYear,
+                    scope.availableYears);
+              }
+              updateStatus();
+            });
+
+        // Watch if 3d is active
+        scope.$watch(function() {
+          return scope.ol3d && scope.ol3d.getEnabled();
+        }, function(active) {
+          scope.stop();
+          scope.is3dActive = active;
+          elt.toggle(scope.isActive);
+        });
+
+        // Activate/deactivate the component
+        scope.$watch('isActive', function(active, old) {
+          // On the first call old and active are false both but we don't
+          // want to apply the year
+          if (active !== old) {
+            scope.years = active ? scope.options.years : [];
+            applyNewYear((active ? scope.currentYear : undefined));
+            elt.toggle(active);
+          }
+        });
+
+        // currentYear is always an integer
+        scope.$watch('currentYear', function(year) {
+          if (scope.isActive) {
+            applyNewYearDebounced(year);
+          }
+        });
+
+        /** Utils **/
+
+        // Apply the year selected
+        var applyNewYear = function(year) {
+          var newYear = (year) ? '' + year : year;
+
+          // Only valid values are allowed: undefined, null or
+          // minYear <= newYear <= maxYear
+          if ((!newYear || (scope.options.minYear <= newYear &&
+            newYear <= scope.options.maxYear))) {
+            gaTime.set(newYear);
+          }
+        };
+        var applyNewYearDebounced = gaDebounce.debounce(applyNewYear, 200,
+            false);
+
+        // Update the list of years available
+        scope.availableYears = [];
+        var updateDatesAvailable = function(olLayers) {
+          var magnetizeCurrentYear = true;
+          scope.availableYears = [];
+          for (var i = 0, length = scope.options.years.length; i < length;
+            i++) {
+            var year = scope.options.years[i];
+            year.available = false;
+            olLayers.forEach(function(olLayer, opt) {
+              if (year.available || !olLayer.timeEnabled) {
+                return;
+              }
+              var timestamps = olLayer.timestamps || [];
+              for (var i = 0, length = timestamps.length; i < length; i++) {
+                var yearTimestamp = gaTime.getYearFromTimestamp(
+                    timestamps[i]);
+                if (year.value === yearTimestamp) {
+                  year.available = true;
+                  scope.availableYears.push(year);
+                  if (year.value === scope.currentYear) {
+                    magnetizeCurrentYear = false;
+                  }
+                  break;
+                }
+              }
+            });
+          }
+          return magnetizeCurrentYear;
+        };
+
+        // Initialize the state of the component
+        scope.currentYear = gaTime.get() || scope.options.maxYear;
+        if (gaTime.get()) {
+          scope.isActive = true;
+        }
       }
-  );
+    };
+  });
 })();
