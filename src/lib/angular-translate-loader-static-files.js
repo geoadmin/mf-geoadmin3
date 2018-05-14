ad2ab150f2927258e9ccfb7abdbe5f5158a605b9
oterral <olivier.terral@swisstopo.ch>
2018-05-14 11:54:11 +0200

    Update angular files

--- src/lib/angular-translate-loader-static-files.js
+++ src/lib/angular-translate-loader-static-files.js
@@ -1,7 +1,7 @@
 /*!
- * angular-translate - v2.17.0 - 2017-12-21
+ * angular-translate - v2.17.1 - 2018-04-16
  * 
- * Copyright (c) 2017 The angular-translate team, Pascal Precht; Licensed MIT
+ * Copyright (c) 2018 The angular-translate team, Pascal Precht; Licensed MIT
  */
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
