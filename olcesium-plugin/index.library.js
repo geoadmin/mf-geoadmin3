eb93fca211cb1370e09ff7190f61fc2cc8bdbac5
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-09-11 09:31:24 +0200

    Add index.library.js file

---
+++ olcesium-plugin/index.library.js
@@ -0,0 +1,23 @@
+import OLCesium from './olcs/OLCesium.js';
+
+import AbstractSynchronizer from './olcs/AbstractSynchronizer.js';
+import RasterSynchronizer from './olcs/RasterSynchronizer.js';
+import VectorSynchronizer from './olcs/VectorSynchronizer.js';
+
+import GaKmlSynchronizer from './olcs/GaKmlSynchronizer.js';
+import GaRasterSynchronizer from './olcs/GaRasterSynchronizer.js';
+import GaTileset3dSynchronizer from './olcs/GaTileset3dSynchronizer.js';
+import GaVectorSynchronizer from './olcs/GaVectorSynchronizer.js';
+
+export default OLCesium;
+
+const olcs = window['olcs'] = {};
+olcs.OLCesium = OLCesium;
+olcs.AbstractSynchronizer = AbstractSynchronizer;
+olcs.RasterSynchronizer = RasterSynchronizer;
+olcs.VectorSynchronizer = VectorSynchronizer;
+
+olcs.GaKmlSynchronizer = GaKmlSynchronizer;
+olcs.GaRasterSynchronizer = GaRasterSynchronizer;
+olcs.GaTileset3dSynchronizer = GaTileset3dSynchronizer;
+olcs.GaVectorSynchronizer = GaVectorSynchronizer;
