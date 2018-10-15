7f80bfc93bc2a9cf0f033247a0430a89b6d17db9
loicgasser <loicgasser4@gmail.com>
2018-10-15 20:55:17 +0200

    Add test to vector feedback directive

---
+++ test/specs/vectorfeedback/VectorFeedbackDirective.spec.js
@@ -0,0 +1,55 @@
+/* eslint-disable max-len */
+describe('ga_vector_feedback_directive', function() {
+  var $rootScope, $compile, el, map, options, submit;
+
+  var loadDirective = function(map, options, submit) {
+    var scope = $rootScope.$new();
+    scope.map = map;
+    scope.options = options;
+    scope.submit = submit;
+    var tpl =
+      '<div ga-vector-feedback ' +
+      'ga-vector-feedback-map="::map"' +
+      'ga-vector-feedback-options="options"' +
+      'ga-vector-feedback-submit="::submit">' +
+      '</div>';
+    el = $compile(tpl)(scope);
+    $rootScope.$digest();
+  };
+
+  var createMap = function() {
+    var mp = new ol.Map({});
+    mp.setSize([600, 300]);
+    mp.getView().fit([-20000000, -20000000, 20000000, 20000000]);
+    return mp;
+  };
+
+  var injectServices = function($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $compile = $injector.get('$compile');
+  };
+
+  beforeEach(function() {
+    inject(function($injector) {
+      injectServices($injector);
+    });
+
+    map = createMap();
+    options = {
+      serviceDocUrl: 'https://service-doc.html',
+      comment: '',
+      likeSelect: ''
+    };
+    submit = sinon.spy(function() {});
+  });
+
+  it('creates the vector feedback directive', function() {
+    loadDirective(map, options, submit);
+    var submitBtn = el.find('button[type=submit]');
+    expect(el.find('a').attr('href')).to.be('https://service-doc.html');
+    expect(el.find('textarea').length).to.be(1);
+    expect(submit.callCount).to.be(0);
+    submitBtn.click();
+    expect(submit.callCount).to.be(1);
+  });
+});
