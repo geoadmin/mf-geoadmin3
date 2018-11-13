d8bae4b8283c92129b929d84a825340480a35313
loicgasser <loicgasser4@gmail.com>
2018-11-13 01:05:06 +0100

    Add test

---
+++ test/specs/queryvector/QueryVectorDirective.spec.js
@@ -0,0 +1,46 @@
+/* eslint-disable max-len */
+describe('ga_query_vector_directive', function() {
+  var $rootScope,
+      $compile,
+      el,
+      map;
+
+  var loadDirective = function(map) {
+    scope = $rootScope.$new();
+    scope.map = map;
+    var tpl =
+      '<div ga-query-vector ' +
+      'ga-query-vector-map="map"' +
+      '</div>';
+    el = $compile(tpl)(scope);
+    $rootScope.$digest();
+  };
+
+  var createMap = function() {
+    var mp = new ol.Map({});
+    mp.setSize([600, 300]);
+    mp.getView().fit([-20000000, -20000000, 20000000, 20000000]);
+    return mp;
+  };
+
+  var injectServices = function($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $compile = $injector.get('$compile');
+  };
+
+  beforeEach(function() {
+    inject(function($injector) {
+      injectServices($injector);
+    });
+    map = createMap();
+  });
+
+
+  it('creates the query vector directive and adds an overlay to the map', function() {
+    loadDirective(map);
+    expect(el.find('table').length).to.be(1);
+    expect($(map.getViewport()).find('div.ol-overlay-container').css('display')).to.equal('none');
+    var overlay = map.getOverlays().getArray()[0];
+    expect(overlay).to.be.an(ol.Overlay);
+  });
+});
