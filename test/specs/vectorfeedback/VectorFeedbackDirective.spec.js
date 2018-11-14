7b7c377b67121e3a16e106bd6b1a034ef0d1189a
Olivier Terral <olivier.terral@geops.de>
2018-11-14 17:02:05 +0100

    Fix #4548: Open a new window for the survey on ie

--- test/specs/vectorfeedback/VectorFeedbackDirective.spec.js
+++ test/specs/vectorfeedback/VectorFeedbackDirective.spec.js
@@ -5,6 +5,7 @@ describe('ga_vector_feedback_directive', function() {
     $httpBackend,
     $q,
     scope,
+    scopeElt,
     el,
     map,
     options,
@@ -16,7 +17,8 @@ describe('ga_vector_feedback_directive', function() {
     spyFilter,
     spyEdit,
     spyResetFilters,
-    spyResetEdits;
+    spyResetEdits,
+    gaBrowserSniffer;
 
   var loadDirective = function(map, options) {
     scope = $rootScope.$new();
@@ -29,6 +31,7 @@ describe('ga_vector_feedback_directive', function() {
       '</div>';
     el = $compile(tpl)(scope);
     $rootScope.$digest();
+    scopeElt = el.isolateScope();
   };
 
   var createMap = function() {
@@ -43,6 +46,7 @@ describe('ga_vector_feedback_directive', function() {
     $compile = $injector.get('$compile');
     $httpBackend = $injector.get('$httpBackend');
     $q = $injector.get('$q');
+    gaBrowserSniffer = $injector.get('gaBrowserSniffer');
   };
 
   beforeEach(function() {
@@ -66,6 +70,11 @@ describe('ga_vector_feedback_directive', function() {
       $provide.value('gaBackground', {
         init: function() {},
         setById: spySetById
+      });
+      $provide.value('gaLang', {
+        getNoRm: function() {
+          return 'somelang'
+        }
       });
 
       spyApplyGlStyleToOlLayer = sinon.spy(function() {});
@@ -110,6 +119,7 @@ describe('ga_vector_feedback_directive', function() {
     options = {
       mobile: true,
       serviceDocUrl: 'https://service-doc.html',
+      surveyUrl: 'http://foo.ch/{lang}',
       backgroundLayers: [
         {
           id: 'toto',
@@ -154,6 +164,8 @@ describe('ga_vector_feedback_directive', function() {
     expect(scope.options.activeColor).to.equal(null);
     expect(scope.options.serviceDocUrl).to.equal('https://service-doc.html');
     expect($(el).find('.ga-vector-feeback-service-link')[1].href).to.equal('https://service-doc.html/');
+    expect(el.find('[target="_blank"]').length).to.be(1);
+    expect(el.find('[href="http://foo.ch/somelang"]').length).to.be(0);
   });
 
   it('calls the right services when changing the background layer', function() {
@@ -206,5 +218,20 @@ describe('ga_vector_feedback_directive', function() {
     expect(spyGetLayer.calledOnce).to.be.ok();
     expect(spyEdit.calledOnce).to.be.ok();
     expect(spyApplyGlStyleToOlLayer.calledOnce).to.be.ok();
+  });
+
+  it('get the survey url', function() {
+    expect(scope.getSurveyUrl)
+    loadDirective(map, {
+      surveyUrl: 'http://foo.ch/{lang}'
+    });
+    expect(scopeElt.getSurveyUrl()).to.be('http://foo.ch/somelang');
+  });
+
+  it('open the survey in a new window on IE', function() {
+    gaBrowserSniffer.msie = 56;
+    loadDirective(map, options);
+    expect(el.find('[target="_blank"]').length).to.be(2);
+    expect(el.find('[href="http://foo.ch/somelang"]').length).to.be(1);
   });
 });
