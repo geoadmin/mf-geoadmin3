713822d6e0ca509dc6d11f622df72b4eecf2cd73
Gilbert Jeiziner <gjn@space.ch>
2018-12-06 13:38:47 +0100

    Remove relief

--- src/components/map/LayersService.js
+++ src/components/map/LayersService.js
@@ -334,7 +334,6 @@ goog.require('ga_urlutils_service');
                     ' swisstopo',
                 subLayersIds: [
                   'OpenMapTiles',
-                  'ch.swisstopo.swissalti3d-reliefschattierung',
                   'ch.swisstopo.vektorkarte.vt',
                   'ch.bav.haltestellen-oev.vt',
                   'ch.swisstopo.amtliches-strassenverzeichnis_validiert',
