703b4b20f7765af4b8f5bc1b714597c131ad0e79
Olivier Terral <olivier.terral@geops.de>
2019-01-03 10:04:08 +0100

    Fix js error when timestamps is undefined

--- src/components/offline/OfflineService.js
+++ src/components/offline/OfflineService.js
@@ -230,7 +230,7 @@ goog.require('ga_window_service');
             // changed.
             gaStorage.getItem(layersKey).split(',').forEach(function(id, idx) {
               var layer = gaLayers.getLayer(id);
-              if (layer && !layer.timeEnabled &&
+              if (layer && !layer.timeEnabled && layer.timestamps &&
                   layer.timestamps[0] !== ts[idx]) {
                 isObsolete = true;
               }
