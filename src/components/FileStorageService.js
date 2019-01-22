6cb024f92e1f5acc14c3a176f94bdfe4bc3016a4
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2019-01-22 16:59:50 +0100

    backport refactoring of FileStorage into PublicStorage and FileStorage from mvt to master

--- src/components/FileStorageService.js
+++ src/components/FileStorageService.js
@@ -1,81 +1,48 @@
 goog.provide('ga_filestorage_service');
+
+goog.require('ga_publicstorage_service');
+
 (function() {
 
-  var module = angular.module('ga_filestorage_service', []);
+  var module = angular.module('ga_filestorage_service', [
+    'ga_publicstorage_service'
+  ]);
 
   /**
-   * This service can create/read/write/delete a file on s3.
+   * This service can create/read/write/delete a KML on public s3 bucket.
    */
   module.provider('gaFileStorage', function() {
-    this.$get = function($http, $q) {
-      var fileStorageUrl = this.fileStorageUrl;
-      var publicUrl = this.publicUrl;
-
-      var getServiceUrl = function(id) {
-        return fileStorageUrl + ((id) ? '/' + id : '');
-      };
-
-      var getPublicUrl = function(fileId) {
-        return publicUrl + ((fileId) ? '/' + fileId : '');
-      };
+    this.$get = function($http, $q, gaPublicStorage) {
+      var endPoint = '/files';
+      var publicEndPoint = '';
+      var contentType = 'application/vnd.google-earth.kml+xml';
 
       var FileStorage = function() {
 
         // Get the file from a fileId
         this.get = function(fileId) {
-          return $http.get(getPublicUrl(fileId));
+          return gaPublicStorage.get(publicEndPoint, fileId);
         };
 
         // Get a fileId from a fileUrl
         this.getFileIdFromFileUrl = function(fileUrl) {
-          if (!fileUrl) {
-            return;
-          }
-          return fileUrl.split('/').pop();
+          return gaPublicStorage.getFileIdFromFileUrl(fileUrl);
         };
 
         // Get the accessible url of the file from an adminId
         this.getFileUrlFromAdminId = function(adminId) {
-          var deferred = $q.defer();
-          $http.get(getServiceUrl(adminId)).then(function(response) {
-            var data = response.data;
-            if (data && data.fileId) {
-              var url = getPublicUrl(data.fileId);
-              deferred.resolve(url);
-            } else {
-              deferred.reject();
-            }
-          }, function() {
-            deferred.reject();
-          });
-          return deferred.promise;
+          return gaPublicStorage.getFileUrlFromAdminId(endPoint, publicEndPoint,
+              adminId);
         };
 
-        // Save the content of a file in s3.
-        // If no id defined --> create a new file
-        //     returns new adminId and new file url
-        // If id is an adminId --> update the file
-        //     returns the same adminId and the same file url
-        // if id is an fileId --> fork the file
-        //     returns new adminId and new file url
-        this.save = function(id, content, contentType) {
-          return $http.post(getServiceUrl(id), content, {
-            headers: {
-              'Content-Type': contentType
-            }
-          }).then(function(response) {
-            var data = response.data;
-            return {
-              adminId: data.adminId,
-              fileId: data.fileId,
-              fileUrl: getPublicUrl(data.fileId)
-            };
-          });
+        this.save = function(id, content) {
+          return gaPublicStorage.save(endPoint, publicEndPoint, id, content,
+              contentType);
         };
 
         // Delete the file in s3. Only if an adminId is specified
         this.del = function(adminId) {
-          return $http['delete'](getServiceUrl(adminId));
+          return gaPublicStorage.del(endPoint, adminId);
         };
 
       };
