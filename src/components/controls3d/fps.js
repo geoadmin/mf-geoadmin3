42c6d5b7ff94416f2cc8969f171f0b68ea90473d
Pascal Barth <pak.barth@gmail.com>
2019-02-14 09:34:36 +0100

    Fix #4484 (3d camera terrain collision) (#4675)
    In 3d pegman, check altitude 25m ahead (in camera direction) and raise the camera height if the camera could see through the terrain because of high slope.

--- src/components/controls3d/fps.js
+++ src/components/controls3d/fps.js
@@ -344,17 +344,37 @@ FPS.prototype.getTimeDifference_ = function() {
  * @param {!Cesium.Cartesian3} gpos
  * @param {number} minHeight
  * @param {number=} optMaxHeight
+ * @param {number} cameraHeading
  * @return {!Cesium.Cartesian3}
  * @private
  */
-FPS.prototype.clampAboveTerrain_ = function(gpos, minHeight, optMaxHeight) {
-  var lla = this.ellipsoid_.cartesianToCartographic(gpos);
-  var groundAlt = Cesium.defaultValue(this.scene_.globe.getHeight(lla), 0.0);
+FPS.prototype.clampAboveTerrain_ = function(gpos, minHeight, optMaxHeight, 
+  cameraHeading) {
+  var lla = this.ellipsoid_.cartesianToCartographic(gpos),
+      lla25MetersAhead = this.ellipsoid_.cartesianToCartographic(gpos);
+  // projecting 25m ahead in the camera direction to check altitude
+  // 111.111km is a rough estimate of kilometers per 1 deg of latitude
+  lla25MetersAhead.latitude = lla.latitude 
+                              + 0.025 / 111.111 * Math.PI / 180.0 
+                                * Math.cos(cameraHeading);
+  // we have to take latitude into account for the km/deg estimate on longitude
+  lla25MetersAhead.longitude = lla.longitude
+                              + 0.025 * Math.cos(lla.latitude) / 111.111
+                                * Math.PI / 180.0
+                                * Math.sin(cameraHeading);
+  var groundAlt = Cesium.defaultValue(this.scene_.globe.getHeight(lla), 0.0),
+      groundAlt25MetersAhead = Cesium.defaultValue(this.scene_.globe.getHeight(
+        lla25MetersAhead), 0.0);
   if (lla.height - groundAlt < minHeight) {
     lla.height = groundAlt + minHeight;
   }
   if (optMaxHeight && (lla.height - groundAlt > optMaxHeight)) {
     lla.height = groundAlt + optMaxHeight;
+  }
+  // if height set is below altitude 25m ahead, we raise the camera height 
+  // to avoid terrain collision
+  if (lla.height < groundAlt25MetersAhead) {
+    lla.height = groundAlt25MetersAhead;
   }
   return this.ellipsoid_.cartographicToCartesian(lla);
 };
@@ -397,7 +417,7 @@ FPS.prototype.flyModeTick_ = function(delta) {
 
   this.camera_.moveForward(moveAmount);
 
-  var gpos = this.clampAboveTerrain_(this.camera_.position, 2);
+  var gpos = this.clampAboveTerrain_(this.camera_.position, 2, heading);
 
   this.camera_.setView({
     destination: gpos,
@@ -452,7 +472,7 @@ FPS.prototype.manTick_ = function(delta) {
     this.camera_.moveBackward(moveAmount);
   }
 
-  var gpos = this.clampAboveTerrain_(this.camera_.position, 2, 2);
+  var gpos = this.clampAboveTerrain_(this.camera_.position, 2, 2, heading);
 
   this.camera_.setView({
     destination: gpos,
