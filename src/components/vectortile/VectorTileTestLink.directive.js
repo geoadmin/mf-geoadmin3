3f4ea9ffe0ace6ff0a89623503cf8c217389ff2d
Pascal Barth <pak.barth@gmail.com>
2019-05-02 13:54:32 +0200

    linting

--- src/components/vectortile/VectorTileTestLink.directive.js
+++ src/components/vectortile/VectorTileTestLink.directive.js
@@ -4,35 +4,35 @@ goog.require('ga_translation_service');
 goog.require('ga_permalink_service');
 goog.require('ga_urlutils_service');
 
-(function () {
-  
+(function() {
+
   angular.module('ga_vector_tile_test_link', [
     'ga_translation_service',
     'ga_permalink_service',
     'ga_urlutils_service'
-  ])
+  ]).
 
-  .directive('gaVectorTileTestLink', function ($window, gaLang, gaPermalink,
-    gaUrlUtils) {
+      directive('gaVectorTileTestLink', function($window, gaLang, gaPermalink,
+          gaUrlUtils) {
 
-    function generateTestLinkUrl() {
-      var params = gaUrlUtils.toKeyValue(gaPermalink.getParams()) || '';
-      return "//test.map.geo.admin.ch?lang=" + gaLang.get() + 
+        function generateTestLinkUrl() {
+          var params = gaUrlUtils.toKeyValue(gaPermalink.getParams()) || '';
+          return '//test.map.geo.admin.ch?lang=' + gaLang.get() +
         (params.length > 0 ? '&' + params : '');
-    }
+        }
 
-    return {
-      restrict: 'A',
-      transclude: true,
-      templateUrl: 'components/vectortile/partials/testlink.html',
-      link: function (scope, element, attrs) {
-        scope.url = generateTestLinkUrl();
-        scope.openTestViewerWithSamePermalink = function(e) {
-          var url = generateTestLinkUrl();
-          $window.open(url, '_blank');
-          e.preventDefault();
+        return {
+          restrict: 'A',
+          transclude: true,
+          templateUrl: 'components/vectortile/partials/testlink.html',
+          link: function(scope, element, attrs) {
+            scope.url = generateTestLinkUrl();
+            scope.openTestViewerWithSamePermalink = function(e) {
+              var url = generateTestLinkUrl();
+              $window.open(url, '_blank');
+              e.preventDefault();
+            };
+          }
         };
-      }
-    };
-  })
+      })
 })();
