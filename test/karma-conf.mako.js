5d640a2a0a48165d8b29d5c94374253ce1be1780
Pascal Barth <pak.barth@gmail.com>
2019-05-14 12:19:06 +0200

    olcesium is back in karma conf for release

--- test/karma-conf.mako.js
+++ test/karma-conf.mako.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
     '${basePath}/lib/d3.min.js',
     '${basePath}/lib/Cesium.min.js',
     '${basePath}/lib/ol.js',
+    '${basePath}/lib/olcesium.js',
     '${basePath}/lib/build.js',
   % else:
     '${basePath}/style/app.css',
