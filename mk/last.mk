e462de98291ae046962738de22a26d87439ea0d1
Pascal Barth <pak.barth@gmail.com>
2020-01-13 11:49:31 +0100

    pip instead of pip2

--- mk/last.mk
+++ mk/last.mk
@@ -10,7 +10,7 @@ endef
 ${PYTHON_VENV}: .build-artefacts/last-pypi-url
 	mkdir -p .build-artefacts
 	virtualenv --no-site-packages $@
-	${PIP_CMD} install --index-url ${PYPI_URL} -U pip2 setuptools==43.0.0
+	${PIP_CMD} install --index-url ${PYPI_URL} -U pip setuptools==44.0.0
 	$(MAKE) .build-artefacts/requirements.timestamp
 
 
