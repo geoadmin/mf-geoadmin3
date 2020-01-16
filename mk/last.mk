4a5da98c98449a9e8eb2d9e39cf0b7f9fc9ff4fc
Pascal Barth <pak.barth@gmail.com>
2020-01-16 14:57:35 +0100

    Forcing python2 with virtualenv

--- mk/last.mk
+++ mk/last.mk
@@ -9,7 +9,7 @@ endef
 
 ${PYTHON_VENV}: .build-artefacts/last-pypi-url
 	mkdir -p .build-artefacts
-	virtualenv --no-site-packages $@
+	virtualenv --no-site-packages --python=python2 $@
 	${PIP_CMD} install --index-url ${PYPI_URL} -U pip setuptools==44.0.0
 	$(MAKE) .build-artefacts/requirements.timestamp
 
