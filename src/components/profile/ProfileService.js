40129e8f16a2585b49b1d0bfff47dfd0ad946360
Pascal Barth <pak.barth@gmail.com>
2020-01-23 13:35:26 +0100

    Adding new param, removing smoothing as its not useful anymore as smart filling will not need smoothing

--- src/components/profile/ProfileService.js
+++ src/components/profile/ProfileService.js
@@ -41,7 +41,7 @@ goog.require('ga_urlutils_service');
 
     // Utils functions
     var createArea = function(domain, height, elevationModel) {
-      return d3.area().curve(d3.curveBasis).x(function(d) {
+      return d3.area().x(function(d) {
         return domain.X(d.domainDist);
       }).y0(height).y1(function(d) {
         return domain.Y(d.alts[elevationModel]);
@@ -296,7 +296,8 @@ goog.require('ga_urlutils_service');
           var data = $.param({
             geom: wkt,
             elevation_models: elevationModel,
-            offset: 0
+            offset: 0,
+            smart_filling: true
           });
 
           var config = {
