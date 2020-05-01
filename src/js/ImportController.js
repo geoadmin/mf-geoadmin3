904acd5365206bed64b36270c21801750388d8a4
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2020-05-01 16:37:10 +0200

    bugfixing previous PR

--- src/js/ImportController.js
+++ src/js/ImportController.js
@@ -193,9 +193,9 @@ goog.require('ga_wmts_service');
       'https://geodienste.ch/db/npl_waldgrenzen/deu',
       'https://geodienste.ch/db/npl_waldgrenzen/fra',
       'https://geodienste.ch/db/npl_waldgrenzen/ita',
-      'https://geodienste.ch/db/planungszonen_v1_0_0/deu'
-      'https://geodienste.ch/db/planungszonen_v1_0_0/fra'
-      'https://geodienste.ch/db/planungszonen_v1_0_0/ita'
+      'https://geodienste.ch/db/planungszonen_v1_0_0/deu',
+      'https://geodienste.ch/db/planungszonen_v1_0_0/fra',
+      'https://geodienste.ch/db/planungszonen_v1_0_0/ita',
       'https://geodienste.ch/db/waldreservate/deu',
       'https://geodienste.ch/db/waldreservate/fra',
       'https://geodienste.ch/db/waldreservate/ita',
