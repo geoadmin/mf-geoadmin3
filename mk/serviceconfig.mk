d64b7a47c6d464d7126d2a4b0f4e71209aa10e7e
Marcel Clausen <marcel.clausen@swisstopo.ch>
2020-05-25 20:57:04 +0200

    check if json is valid

--- mk/serviceconfig.mk
+++ mk/serviceconfig.mk
@@ -24,8 +24,8 @@ configs/: .build-artefacts/last-version \
           guard-LAYERSCONFIG_VERSION
 	mkdir -p $@
 	curl -s -q -o configs/services.json http:$(API_URL)/rest/services
-	$(foreach lang, $(LANGS), mkdir -p $@$(lang) && curl -s --retry 3 --fail -o configs/$(lang)/layersConfig.json http:$(API_URL)/rest/services/all/MapServer/layersConfig?lang=$(lang);)
-	$(foreach topic, $(TOPICS), $(foreach lang, $(LANGS),curl -s --retry 3 --fail -o configs/${lang}/catalog.${topic}.json http:$(API_URL)/rest/services/$(topic)/CatalogServer?lang=$(lang); ))
+	$(foreach lang, $(LANGS), mkdir -p $@$(lang) && curl -s --retry 3 --fail -o configs/$(lang)/layersConfig.json http:$(API_URL)/rest/services/all/MapServer/layersConfig?lang=$(lang) && jq . configs/$(lang)/layersConfig.json 1> /dev/null;)
+	$(foreach topic, $(TOPICS), $(foreach lang, $(LANGS),curl -s --retry 3 --fail -o configs/${lang}/catalog.${topic}.json http:$(API_URL)/rest/services/$(topic)/CatalogServer?lang=$(lang) && jq . configs/${lang}/catalog.${topic}.json 1> /dev/null; ))
 
 
 # Variables for the different staging.
