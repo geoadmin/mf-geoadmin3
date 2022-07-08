839a7b5b4b0de928700ba38ee123cf1cbdf3dc02
Pascal Barth <pak.barth@gmail.com>
2022-07-08 14:51:25 +0200

    Increase timeouts for karma testing

--- test/karma-conf.mako.js
+++ test/karma-conf.mako.js
@@ -169,6 +169,9 @@ module.exports = function(config) {
   // If browser does not capture in given timeout [ms], kill it
   captureTimeout: 5000,
 
+  browserDisconnectTimeout: 60000,
+  browserDisconnectTolerance: 3,
+  browserNoActivityTimeout: 60000,
 
   // Continuous Integration mode
   // if true, it capture browsers, run tests and exit
